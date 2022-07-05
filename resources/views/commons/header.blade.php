23e72afed5c1365f5925e335060652677aff984b
naoki kaneko <vs.noo.moo@gmail.com>
2022-07-05 13:29:46 +0900

    #7共通ファイル作成 #8ヘッダー・フッター & トップページのタイトルのみ表示「Topic Posts」

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -1,5 +1,5 @@
 <header class="mb-5">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+    <nav class="navbar navbar-expand-lg bg-info">
         <a class="navbar-brand" href="/">Topic Posts</a>
         <button type="button" class="navbar-toggler" data-toggler="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
@@ -7,8 +7,8 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="" class="nav-link" style="color: #fff;" onMouseOut="this.style.color='#fff';" onMouseOver="this.style.color='#cbd3da';">ログイン</a></li>
-                <li class="nav-item"><a href="" class="nav-link" style="color: #fff;" onMouseOut="this.style.color='#fff';" onMouseOver="this.style.color='#cbd3da';">新規ユーザ登録</a></li>
+                <li class="nav-item"><a href="" class="nav-link">ログイン</a></li>
+                <li class="nav-item"><a href="" class="nav-link">新規ユーザ登録</a></li>
             </ul>
         </div>
     </nav>
