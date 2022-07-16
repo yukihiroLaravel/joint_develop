96c73fc1e374e1f9ab31c3a43d84fe12d712b2f6
EC2 Default User <ec2-user@ip-172-31-17-124.ap-northeast-1.compute.internal>
2022-07-16 15:21:42 +0000

    #9 ユーザ新規登録

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -7,8 +7,8 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
+                <li class="nav-item"><a href="{{ route('signup') }}" class="nav-link">新規ユーザ登録</a></li>
                 <li class="nav-item"><a href="" class="nav-link">ログイン</a></li>
-                <li class="nav-item"><a href="" class="nav-link">新規ユーザ登録</a></li>
             </ul>
         </div>
     </nav>
