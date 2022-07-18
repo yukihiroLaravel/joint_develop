74dd0e1e93fe288e70c5a7a547d19b7abe91ec27
EC2 Default User <ec2-user@ip-172-31-17-124.ap-northeast-1.compute.internal>
2022-07-18 14:14:07 +0000

    #13 ユーザ詳細

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+// ユーザ
+Route::get('/', 'UsersController@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
