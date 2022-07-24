b71a7debb56886fdbaaf6ebc5ef11007353f995e
EC2 Default User <ec2-user@ip-172-31-17-124.ap-northeast-1.compute.internal>
2022-07-24 03:26:24 +0000

    #9ユーザ新規登録

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (env('APP_ENV_CLOUD9') === true) {                       
+            \URL::forceScheme('https');
+        }
+    }
+}
