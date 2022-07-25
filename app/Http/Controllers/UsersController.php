6425f93943e9d95958e6cedfa2edd89b0b4dae90
naoki kaneko <vs.noo.moo@gmail.com>
2022-07-25 23:06:23 +0900

    #14ユーザ編集画面・更新

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,26 +18,13 @@ class UsersController extends Controller
 
     public function update(EditRequest $request, $id)
     {
-        
         $user = User::findOrFail($id);
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
-        
-        //新規パスワードの確認
-        
-        $password = $user->password;
-        $hash = password_hash($password, PASSWORD_DEFAULT);
-        if(password_verify($password, $hash)){
-            echo "パスワードを変更しました。";
-        }
-
-        
+    
         $user->save();
         return back();
-        // return view('users'.{$id})
-        // return redirect ('/');
-        // ->with('success','パスワードの変更が終了しました');
+        
     }
-    
 }
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -34,39 +34,3 @@
     </form>
 
 @endsection
-
-<!-- 
-<h2 class="mt-5 mb-3">ユーザ情報を編集する</h2>
-    <form method="" action="">
-        <input type="hidden" name="id" value="" />
-        <div class="form-group">
-            <label for="name">ユーザ名</label>
-            <input class="form-control" value="" name="name" />
-        </div>
-
-        <div class="form-group">
-            <label for="email">メールアドレス</label>
-            <input class="form-control" value="" name="" />
-        </div>
-
-        <div class="form-group">
-            <label for="password">パスワード</label>
-            <input class="form-control" type="password" name="" />
-        </div>
-
-        <div class="form-group">
-            <label for="password_confirmation">パスワードの確認</label>
-            <input class="form-control" type="password" name="" />
-        </div>
-
-        <div class="d-flex justify-content-between">
-            <a class="btn btn-danger text-light" data-toggle="modal" data-target="#deleteConfirmModal">退会する</a>
-            <button type="submit" class="btn btn-primary">更新する</button>
-        </div>
-    </form>
--->
-
-
-
-
-
