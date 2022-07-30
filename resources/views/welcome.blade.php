c6d7e19f64d6884c28f72578a45ded0643d3f8c0
yuta <prnkmt2947@gmail.com>
2022-07-30 10:51:36 +0900

    #12 トップページに投稿表示＿別タスクのコードを削除（ユウタ）

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,21 +9,6 @@
         
     </div>
     <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
-    @if(Auth::check())
-    <div class="w-75 m-auto">エラーメッセージが入る場所</div>
-        <div class="text-center mb-3">
-            <form method="" action="" class="d-inline-block w-75">
-                <div class="form-group">
-                    <textarea class="form-control" name="" rows=""></textarea>
-                    <div class="text-left mt-3">
-                        <button type="submit" class="btn btn-primary">投稿する</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    @endif
-
-    
     @foreach ($posts as $post)
         <ul class="list-unstyled">
             <li class="mb-3 text-center">
