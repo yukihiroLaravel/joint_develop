acf5256a1576f0f6283402ce1e6e774621b0e0f0
yuta <prnkmt2947@gmail.com>
2022-08-01 15:46:32 +0900

    #15 新規等作成（ユウタ

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,10 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Post;
+use App\User;
+use App\Http\Requests\MovieRequest;
 
 class PostsController extends Controller
 {
-   public function index()
+   public function store()
+   {
+       $user = \Auth::user();
+       $data = [
+           'user' => $user,
+           'posts' =>$posts,
+       ];
+    }
+    public function index()
    {
        $posts = Post::orderBy('id', 'desc')->paginate(10);
        
