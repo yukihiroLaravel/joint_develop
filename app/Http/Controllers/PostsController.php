b05480b59a40520a6eef727e490f9b1805e7aedd
yuta <prnkmt2947@gmail.com>
2022-08-01 15:56:04 +0900

    #12 トップページの投稿表示（ユウタ）プルリク修正

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,26 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Post;
-use App\User;
-use App\Http\Requests\MovieRequest;
 
 class PostsController extends Controller
 {
-   public function store()
-   {
-       $user = \Auth::user();
-       $data = [
-           'user' => $user,
-           'posts' =>$posts,
-       ];
-    }
     public function index()
    {
        $posts = Post::orderBy('id', 'desc')->paginate(10);
        
        return view('welcome', [ 
-            'posts' => $posts,
-            
+            'posts' => $posts,    
        ]);
    }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,9 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public function user () 
+    public function user() 
     {
         return $this->belongsTo(User::class);
-        
     }
 }
