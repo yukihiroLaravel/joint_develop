0a69bebd6efb5c76beddda23311307b1e27888fb
EC2 Default User <ec2-user@ip-172-31-17-124.ap-northeast-1.compute.internal>
2022-08-04 14:44:49 +0000

    #13 ユーザ詳細

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,8 +1,35 @@
 @extends('layouts.app')
 @section('content')
-    <h1>{{ $user->name }}</h1>
-    <ul class="nav nav-tabs nav-justified mt-5 mb-2">
-        <li class="nav-item nav-link {{ Request::is('users/'. $user->id) ? 'active' : '' }}"><a href="{{ route('user.show', $user->id) }}"><投 稿<br><div class="badge badge-secondary"></div></a></li>
-        <li class="nav-item nav-link><a href="">お気に入り<br><div class="badge badge-secondary"></div></a></li>
-    </ul>
+    <div class="row">
+        <aside class="col-sm-4 mb-5">
+            <div class="card bg-info">
+                <div class="card-header">
+                    <h3 class="card-title text-light">{{ $user->name }}</h3>
+                </div>
+                <div class="card-body">
+                    <img class="rounded-circle img-fluid" src="" alt="">
+                        <div class="mt-3">
+                            
+                            @auth
+                                <a class="nav-link" href=""><i class="mr-1"></i>ユーザ情報の編集</a></li>
+                            @endauth
+    
+                            @guest 
+                                <li class="nav-item">
+                                <a class="nav-link" href=""></a></li>
+                            @endguest 
+                        </div>
+                </div>
+            </div>
+        </aside>
+        <div class="col-sm-8">
+            <ul class="nav nav-tabs nav-justified mb-3">
+                <li class="nav-item"><a href="" class="nav-link {{ Request::is() ? 'active' : '' }}">タイムライン</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">フォロー中</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">フォロワー</a></li>
+            </ul>
+                    @include('posts.post', ['posts' => $posts])
+        </div>
+    </div>
+    
 @endsection
