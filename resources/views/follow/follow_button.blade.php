f664af6f1b5fcaa21f94fc6c48cde787c5b8af5e
naoki kaneko <vs.noo.moo@gmail.com>
2022-08-08 14:49:37 +0900

    #19フォロー機能

--- resources/views/follow/follow_button.blade.php
+++ resources/views/follow/follow_button.blade.php
@@ -1,14 +1,17 @@
 @if (Auth::check())
-  @if (Auth::id() != $user->id)
-    @if (Auth::user()->is_following($suer->id))
-      {!! Form::open(['route' => ['unfollow', $user->id], 'method' => 'delete']) !!}
-        {!! Form:submit('フォローを外す', ['class' => "button btn btn-danger mt-1"]) !!}
-      {!! Form::close() !!}
-    @else
-      {!! Form::open(['route' => ['follow', $user->id]]) !!}
-        {!! submit('フォローする', ['class' => "button btn btn-primary mt-1"]) !!}
-      {!! Form::close() !!}
+    @if (Auth::id() != $user->id)
+        @if (Auth::user()->is_following($user->id))
+            <form action="{{ route('unfollow', $user->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">フォローを外す</button>
+            </form>
+        @else
+            <form action="{{ route('follow', $user->id) }}" method="POST">
+                @csrf
+                <button type="submit" class="btn btn-success">フォロー</button>
+            </form>
+        @endif
     @endif
-  @endif
 @endif
 
