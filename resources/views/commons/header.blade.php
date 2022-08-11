280fed8da194ae50a4f431d4c37f1736b4387351
EC2 Default User <ec2-user@ip-172-31-17-124.ap-northeast-1.compute.internal>
2022-08-11 13:27:14 +0000

    #13 ユーザ詳細

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -10,7 +10,6 @@
                 @if (Auth::check())
                     <li class="nav-item"><a href="{{ route('user.show', Auth::id()) }}" class="nav-link">{{ Auth::user()->name}}</a></li>
                     <li class="nav-item"><a href="{{ route('logout') }}" class="nav-link">ログアウト</a></li>
-                    
                 @else
                     <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">ログイン</a></li>
                     <li class="nav-item"><a href="{{ route('signup') }}" class="nav-link">新規ユーザ登録</a></li>
