a2a194ffc5016a670cc8915d7430c171297a151f
yuta <prnkmt2947@gmail.com>
2022-08-11 09:34:55 +0900

    #18 ユーザ退会（ユウタ）

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ Route::post('signup','Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
-
 Route::get('/', 'PostsController@index');
 Route::group(['middleware' => 'auth'], function () {
     Route::prefix('users')->group(function () {
