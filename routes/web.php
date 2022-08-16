678df8659e0d8f922061663b5ff873c8fd794078
naoki kaneko <vs.noo.moo@gmail.com>
2022-08-17 00:11:05 +0900

    #14ユーザ編集画面・更新

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::prefix('users')->group(function () {
-    Route::get('{id}', 'UsersController@show')->name('user.show');
-    Route::get('{id}/edit', 'UsersController@edit')->name('users.edit');
-    Route::put('{id}', 'UsersController@update')->name('users.update');
-});
+
 Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup','Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
@@ -29,6 +25,7 @@ Route::group(['middleware' => 'auth'], function () {
 });
 
 Route::get('/', 'PostsController@index');
+Route::get('users/{id}', 'UsersController@show')->name('user.show');
 Route::group(['middleware' => 'auth'], function () {
     Route::prefix('users')->group(function () {
         Route::get('{id}/edit', 'UsersController@edit')->name('users.edit');
