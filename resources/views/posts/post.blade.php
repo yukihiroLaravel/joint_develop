123bcb20c11e4d711827ab01e2e9003831274f2d
EC2 Default User <ec2-user@ip-172-31-17-124.ap-northeast-1.compute.internal>
2022-08-25 13:28:28 +0000

    #16 投稿削除

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -12,12 +12,12 @@
                 @if (Auth::id() === $post->user_id)
                     <div class="d-flex justify-content-between w-75 pb-3 m-auto">
                         <form method="POST" action="{{ route('post.delete', $post->id) }}">
-                        @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">削除</button></form>
-            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary">編集する</a>
-        </div>
-@endif
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">削除</button></form>
+                        <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary">編集する</a>
+                    </div>
+                @endif
         </div>
     </li>
 @endforeach
