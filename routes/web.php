6e75bccd898daebf1383c0286ef2b30c47afad66
naoki kaneko <vs.noo.moo@gmail.com>
2022-09-20 12:06:23 +0900

    search

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('followers', 'UsersController@followers')->name('followers');
     });
 });
-
+Route::get('search', 'PostsController@search')->name('post.search');
 Route::get('/', 'PostsController@index');
 
 Route::group(['prefix' => 'user/{id}'],function(){
