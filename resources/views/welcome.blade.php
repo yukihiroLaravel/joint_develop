35c54da8252402ef20b7532704e0b57448dbc97d
naoki kaneko <vs.noo.moo@gmail.com>
2022-09-22 11:09:25 +0900

    #56アニメーション追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,13 @@
 
 @extends('layouts.app')
 @section('content')
-    <div class="center jumbotron jumbotron-extend bg-info" data-aos="flip-down" data-aos-duration="1000">
+    <div class="center jumbotron jumbotron-extend bg-info" data-aos="zoom-out" data-aos-duration="1000">
         <div class="text-center text-white mt-2 pt-1">
-            <h1><i class="fas fa-baseball-ball fa-lg pr-3"></i>Base Talk</h1>
+            <h1 data-aos="zoom-out" data-aos-duration="3000"><i class="fas fa-baseball-ball fa-lg pr-3"></i>Base Talk</h1>
             @include('searchs.search')
         </div>
     </div>
-    <h5 class="text-center mb-3">プロ野球チームについて140字以内で会話しよう！</h5> 
+    <h5 class="text-center mb-3" data-aos="zoom-out" data-aos-duration="1000">プロ野球チームについて140字以内で会話しよう！</h5> 
     <div class="w-75 m-auto">@include('commons.flash_message')</div>
     @if(Auth::check())
         @include('posts.new_post')
