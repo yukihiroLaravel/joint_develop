139c1833ee132bb26af2f5ef750035436d69c1bb
naoki kaneko <vs.noo.moo@gmail.com>
2022-09-24 10:01:30 +0900

    #56アニメーション追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,6 @@
 
         <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-        <script src="{{ asset('/js/script.js') }}"></script>    
         <script>AOS.init();</script>
     </body>
 </html>
