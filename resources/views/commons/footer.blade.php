b814299d4018d8011b0c83e98fc2b40b6c07adcc
daisuke takebuchi <bucchi_orange@yahoo.co.jp>
2022-10-02 18:23:45 +0900

    #59共通ファイル、ヘッダー・フッター & トップページのタイトルの作成

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="mt-5">
+    <nav class="navbar navbar-dark bg-primary justify-content-center">
+        <span class="navbar-brand">@Gut Familie,All rights reserved.</span>
+    </nav>
+</footer>
---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,15 @@
+<header class="mb-5">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+        <a class="navbar-brand" href="/">Topic Posts</a>
+        <button type="navbar-brand" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="bavbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-time"><a href="" class="nav-link">ログイン</a></li>
+                <li class="nav-item"><a href="" class="nav-link">新規ユーザー登録</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
