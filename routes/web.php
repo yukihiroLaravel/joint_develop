3b9d8e1e8c707bf8c663119f7cc18ecdb139933f
daisuke takebuchi <bucchi_orange@yahoo.co.jp>
2022-10-25 21:28:21 +0900

    #66 ユーザー詳細画面(bladeのみ)

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/', 'UsersController@index');
+
+//ユーザー詳細
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');});
