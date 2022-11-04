e61a8313a27fc6db88e5cab74707f4f6ab38dc58
徳重舜也 <tokushigeshunya@macBook.local>
2022-11-04 13:36:48 +0900

    ユーザ編集画面、更新

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use App\User;
+use App\Http\Requests\UserEditRequest;
 
 class UsersController extends Controller
 {
@@ -20,4 +21,30 @@ class UsersController extends Controller
       
       return view('users.show',$data);
    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        if(\Auth::id() === $user->id) {
+            return view('users.edit', [
+                'user' => $user
+            ]);
+        }
+        abort(404);
+    }
+
+    public function update(UserEditRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+        if(\Auth::id() === $user->id) {
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->save();
+            return redirect('/users/'. $user->id);
+        }
+    }
 }
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,14 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 Route::prefix('users')->group(function () {
     Route::get('{id}', 'UsersController@show')->name('user.show');
 });
+
+// ログイン後
+Route::group(['middleware' => 'auth'], function () {
+    // ユーザー編集
+    Route::prefix('users')->group(function () {
+        Route::get('{id}/edit', 'UsersController@edit')->name('users.edit');
+        Route::put('{id}', 'UsersController@update')->name('users.update');
+    });
+});
+
+
