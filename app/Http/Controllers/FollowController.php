3334f8c7f4be96d4e00469f1ddb76cb0dddf31e0
daisuke takebuchi <bucchi_orange@yahoo.co.jp>
2022-11-23 14:51:50 +0900

    #71 不要なコードを削除

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -6,12 +6,22 @@ use Illuminate\Http\Request;
 
 class FollowController extends Controller
 {
+    /**
+     *フォロー実行
+     *@param int $id
+     *@return back
+     */
     public function store($id)
     {
         \Auth::user()->follow($id);
         return back();
     }
 
+    /**
+     *フォロー解除
+     *@param int $id
+     *@return view
+     */
     public function destroy($id)
     {
         \Auth::user()->unfollow($id);
