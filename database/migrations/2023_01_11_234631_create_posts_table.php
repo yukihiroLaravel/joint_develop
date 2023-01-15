3b0ec0c67b7c62470345a078211196be1f4c6aaa
mao-fukushi <zhenyangfushi@gmail.com>
2023-01-15 20:35:54 +0900

    posts（投稿）テーブルのマイグレーションとシーダー作成の修正_2

--- database/migrations/2023_01_11_234631_create_posts_table.php
+++ database/migrations/2023_01_11_234631_create_posts_table.php
@@ -18,7 +18,7 @@ class CreatePostsTable extends Migration
             $table->bigInteger('user_id')->unsigned()->index();
             $table->text('content');
             $table->timestamps();
-            $table->softDeletes();
+            // $table->softDeletes();
             // 外部キー制約
             // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
