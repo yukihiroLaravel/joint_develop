25440da971d5eeca6c59fffb7f22d5360f58115a
ryoma fujioka <doragon_5525@yahoo.co.jp>
2023-01-16 22:43:13 +0900

    ユーザログイン・ログアウト(ふじおか)

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+// ログイン
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
