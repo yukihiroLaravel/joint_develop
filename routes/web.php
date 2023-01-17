64c224f5ba976f5a0da44035b3ae197f4a866f30
yusuke okada <y.okada.2125@gmail.com>
2023-01-17 22:25:18 +0900

    #109 ユーザログイン・ログアウト途中1

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ログイン
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
