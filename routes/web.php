3b83e54c71c91fb969c3d1dcc7532543f40c0d98
yusuke okada <y.okada.2125@gmail.com>
2023-01-25 22:14:55 +0900

    #104 トップページの投稿表示

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,7 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('/', 'UsersController@index');
+//トップページ投稿表示
+Route::get('/', 'PostsController@index');
+
+//Route::get('/', 'UsersController@index');
