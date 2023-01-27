0d45c2f15d755cbdbbe2fd5c932f680c74367667
yusuke okada <y.okada.2125@gmail.com>
2023-01-27 19:09:43 +0900

    #114 トップページの投稿表示 修正

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -10,15 +10,14 @@
                     <p class="mb-2">{{ $post->content }}</p>
                     <p class="text-muted">{{ $post->created_at }}</p>
                 </div>
-
-            <!-- 削除・編集ボタン -->
-           <!-- <div class="d-flex justify-content-between w-75 pb-3 m-auto">
+            @if(Auth::check() && Auth::user()->id == $post->user_id)
+                <div class="d-flex justify-content-between w-75 pb-3 m-auto">
                     <form method="" action="">
                         <button type="submit" class="btn btn-danger">削除</button>
                     </form>
                         <a href="" class="btn btn-primary">編集する</a>
-                </div> -->
-
+                </div>
+            @endif
             </div>
         </li>
     </ul>
