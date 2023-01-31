8c173c8c216ba0005632af92c88707a3a3ad4018
youzou <youzou.pc@gmail.com>
2023-01-31 11:40:13 +0900

    #116 ユーザ詳細

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $users = User::orderBy('id','desc')->paginate(9);
+
+        return view('welcome', [
+            'users' => $users,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        $posts = $user->posts()->orderBy('id', 'desc')->paginate(9);
+        $data=[
+            'user' => $user,
+            'posts' => $posts,
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show',$data);
+    }
+
+    public function test() {
+        dd('testing in controller!');
     }
 }
