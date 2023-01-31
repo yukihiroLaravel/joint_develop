f6a0c3673e5bf421f01815dcb6e18ba703b6ea36
kishio taro <kishio@example.com>
2023-01-31 18:08:10 +0900

    #125 ユーザ退会

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,7 +37,7 @@ class UsersController extends Controller
     public function destroy ($id)
     {
         $user = User::findOrFail($id);
-        $user -> delete();
+        $user -> forceDelete();
         return redirect('/');
     }
 }
