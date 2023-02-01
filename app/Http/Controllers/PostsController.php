440639eaa752472315a6e7b391f4d636ebac4e2b
yusuke okada <y.okada.2125@gmail.com>
2023-02-01 21:21:16 +0900

    #124 投稿編集画面・更新

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Http\Requests\PostRequest;
 
 class PostsController extends Controller
 {
@@ -14,5 +15,24 @@ class PostsController extends Controller
         return view('welcome', [
             'posts' => $posts,
         ]);
+    }
+
+    public function edit($id)
+    {
+        $user = \Auth::user();
+        $post = Post::findOrFail($id);
+        $data =[
+            'user' => $user,
+            'post' => $post,
+        ];
+        return view('posts.edit', $data);
+    }
+
+    public function update(PostRequest $request, $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->content = $request->content;
+        $post->save();
+        return redirect("/");
     }
 }
