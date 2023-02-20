0cb9240805052267576c0e610c101fee8e1ecdc3
akane goto <goto.akane.8.25@gmail.com>
2023-02-20 19:11:19 +0900

    エラー解消

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +13,6 @@
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
-
 //ログイン時のみ
 Route::group(['middleware'=>'auth'], function () {
     Route::group(['prefix'=>'users'], function () {
@@ -26,18 +24,20 @@ Route::group(['middleware'=>'auth'], function () {
 Route::group(['prefix'=>'post'],function(){
     Route::get('{id}/edit','PostsController@edit')->name('post.edit');
     Route::put('{id}','PostsController@update')->name('post.update');
-});    
+});
 // ログイン後
     Route::delete('{id}', 'PostsController@destroy')->name('post.delete');
 });
 //新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
-
 //投稿一覧表示
 Route::get('/', 'PostsController@index');
-
 //ユーザ
 Route::group(['prefix'=>'users'], function () {
     Route::get('{id}', 'UsersController@show')->name('user.show');
 });
+
+
+
+    
