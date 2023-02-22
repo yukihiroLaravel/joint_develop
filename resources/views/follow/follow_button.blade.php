b80df52dd09c4ce4fedfb00319b8a91454ca8336
yusuke okada <y.okada.2125@gmail.com>
2023-02-23 02:31:53 +0900

    #127フォロー機能

--- resources/views/follow/follow_button.blade.php
+++ resources/views/follow/follow_button.blade.php
@@ -1,12 +1,12 @@
 @if (Auth::check() && Auth::id() !== $post->user_id)
     @if (Auth::user()->isFollow($post->user_id))
-        <form method="POST" action="{{ route('unFollow', $post->id) }}">
+        <form method="POST" action="{{ route('unFollow', $post->user_id) }}">
             @csrf
             @method('DELETE')
             <button type="submit" class="btn btn-danger">フォローを外す</button>
         </form>
     @else
-        <form method="POST" action="{{ route('follow', $post->id) }}">
+        <form method="POST" action="{{ route('follow', $post->user_id) }}">
             @csrf
             <button type="submit" class="btn btn-success">フォローする</button>
         </form>
