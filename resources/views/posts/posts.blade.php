65bdbc47af7b0e38c08cad0df6baf33cabc79486
youzou <youzou.pc@gmail.com>
2023-02-25 11:23:40 +0900

    #116 プルリク修正

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -3,7 +3,7 @@
         <li class="mb-3 text-center">
             <div class="d-flex d-inline-block w-75 pb-3 m-auto">
                 <img class="mr-2 rounded-circle" src="{{ Gravatar::src($post->user->email, 55) }}" alt="ユーザのアバター画像">
-                <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('users.show', $post->id) }}">＠{{ $post->user->name }}</a></p>
+                <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('users.show', $post->user_id) }}">＠{{ $post->user->name }}</a></p>
                 @include('follow.follow_button')
             </div>
             <div class="">
