76cf5d642618c21d94fcfa054a80e6789a78a607
yusuke okada <y.okada.2125@gmail.com>
2023-03-05 15:49:37 +0900

    タイトル、サブタイトルの変更3

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class PostsTableSeeder extends Seeder
 {
@@ -11,13 +12,111 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 10; $i++) {
-            DB::table('posts')->insert([
-                'user_id' => $i,
-                'content' => 'テスト投稿です。',
-                'created_at' => now(),
-                'updated_at' => now()
-            ]);
-        }
+        for ($i = 1; $i <= 1; $i++) {
+        $date = new Carbon('2023-01-15 10:24:57');
+        $date->addSecond();
+        DB::table('posts')->insert([
+            'user_id' => '1',
+            'content' => 'はじめまして！呪術廻戦のオープニングもエンディングも劇場版0も全部良いのでアニメと共に見て欲しいです！',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '2',
+            'content' => 'ef - a tale of memories/melodiesというアニメ是非オススメです。melodiesの方は、毎話OPが変わるので隙がありません！！',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '3',
+            'content' => '「ひぐらしのなく頃に」ってゲームもアニメもいい曲いっぱいありますよね。',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '5',
+            'content' => 'シュタインズゲートのOP「Hacking to the Gate」！アニメ面白いし、歌詞と内容がだんだん関連してきて、ラスト2話のOPは飛ばしちゃだめです。',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '4',
+            'content' => 'シャドウハーツ２のED曲（月恋花）すごく良いんで聴いてみてください。諌山さんの歌声が染み入ります。',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '10',
+            'content' => 'azumaさん。呪術廻戦、私も好きです！呪術廻戦0は3回も映画館に観にいっちゃいました。',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '3',
+            'content' => 'イケやんさん、諌山さんの曲でプレイス・オブ・ピリオドって曲も染み入りますよ！',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '4',
+            'content' => '品川さん聴いてみますね！オススメありがとうございます。',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '7',
+            'content' => 'プラネテスのWonderful Lifeって曲好きです。アニメと一緒に聴いて欲しいなあ〜。エンディングの入り方が最高なんですよ☆',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '8',
+            'content' => 'ターンAガンダムのMoon！壮大で聴き入っちゃいます。さすが菅野よう子さんです。',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '6',
+            'content' => '初めまして。ちょっと昔のゲームなんですが、横スクロールアクションのリターン・オブ・ダブルドラゴンとコントラスピリッツのBGMが好きです。わかる人います？',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '2',
+            'content' => 'BBさんMoon良いですね！自分はウイングガンダム好きです！！',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        $date->addSecond($i+12345);
+        DB::table('posts')->insert([
+            'user_id' => '9',
+            'content' => 'アニメ映画「パプリカ」の主題歌好きです。不思議な内容と平沢進さんの独特の曲が合っててすごく良いんです。',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+    }
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,14 +11,85 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 10; $i++) {
+
         DB::table('users')->insert([
-            'name' => 'ユーザ'.$i.'番',
-            'email' => 'test'.$i.'@test.com',
-            'password' => bcrypt('test'.$i),
+            'name' => 'azuma',
+            'email' => 'azuma@test.com',
+            'password' => bcrypt('test1'),
             'created_at' => now(),
             'updated_at' => now()
         ]);
-        }
+
+        DB::table('users')->insert([
+            'name' => 'shibuya',
+            'email' => 'shibuya@test.com',
+            'password' => bcrypt('test2'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => '品川',
+            'email' => 'shinagawa@test.com',
+            'password' => bcrypt('test3'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'イケやん',
+            'email' => 'ikeyan@test.com',
+            'password' => bcrypt('test4'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'あっきー',
+            'email' => 'aki@test.com',
+            'password' => bcrypt('test5'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Okubo Arata',
+            'email' => 'okuboarata@test.com',
+            'password' => bcrypt('test6'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Megu',
+            'email' => 'megu@test.com',
+            'password' => bcrypt('test7'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'BB',
+            'email' => 'bb@test.com',
+            'password' => bcrypt('test8'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'chika',
+            'email' => 'chika@test.com',
+            'password' => bcrypt('test9'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'rin uxei',
+            'email' => 'rinuxei@test.com',
+            'password' => bcrypt('test10'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
