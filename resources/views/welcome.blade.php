c079cdf19ca424b9b53fabe5a5912808f682820d
youzou <youzou.pc@gmail.com>
2023-03-06 22:20:58 +0900

    投稿削除のフラッシュメッセージ修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,18 +14,17 @@
             </ul>
         </div>
     @endif
-        <div class="w-75 m-auto">@include('commons.error_messages')
-            <div class="text-center mb-3">
-                <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
-                    @csrf
-                    <div class="form-group">
-                        <textarea class="form-control" name="content" rows="4">{{ old('content') }}</textarea>
-                        <div class="text-left mt-3">
-                            <button type="submit" class="btn btn-primary">投稿する</button>
-                        </div>
+        <div class="w-75 m-auto">@include('commons.error_messages')</div>
+        <div class="text-center mb-3">
+            <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
+                @csrf
+                <div class="form-group">
+                    <textarea class="form-control" name="content" rows="4">{{ old('content') }}</textarea>
+                    <div class="text-left mt-3">
+                        <button type="submit" class="btn btn-primary">投稿する</button>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     @endif
 @include( 'posts.posts', ['posts' => $posts])
