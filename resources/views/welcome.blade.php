37548b0992a8e775d091f7fd4d9c9ddde8513299
youzou <youzou.pc@gmail.com>
2023-03-07 16:47:42 +0900

    投稿削除のフラッシュメッセージ修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,26 +6,26 @@
         </div>
     </div>
     <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
-    @if(Auth::check())
-    @if (session('message'))
-        <div class="w-75 m-auto">
+    <div class="w-75 m-auto">
+        @if(Auth::check())
+        @if (session('message'))
             <ul class="alert alert-danger">
                 <li class="ml-4">{{ session('message') }}</li>
             </ul>
-        </div>
-    @endif
+        @endif
+    </div>
         <div class="w-75 m-auto">@include('commons.error_messages')</div>
-        <div class="text-center mb-3">
-            <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
-                @csrf
-                <div class="form-group">
-                    <textarea class="form-control" name="content" rows="4">{{ old('content') }}</textarea>
-                    <div class="text-left mt-3">
-                        <button type="submit" class="btn btn-primary">投稿する</button>
+            <div class="text-center mb-3">
+                <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
+                    @csrf
+                    <div class="form-group">
+                        <textarea class="form-control" name="content" rows="4">{{ old('content') }}</textarea>
+                        <div class="text-left mt-3">
+                            <button type="submit" class="btn btn-primary">投稿する</button>
+                        </div>
                     </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
     @endif
 @include( 'posts.posts', ['posts' => $posts])
 @endsection
