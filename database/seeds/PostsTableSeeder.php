39be0650184fb312d0da0afd5c67779e2f1eb2d1
yusuke okada <y.okada.2125@gmail.com>
2023-03-07 20:06:39 +0900

    タイトル、サブタイトルの変更5

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,9 +12,7 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 1; $i++) {
         $date = new Carbon('2023-01-15 10:24:57');
-        $date->addSecond();
         DB::table('posts')->insert([
             'user_id' => '1',
             'content' => 'はじめまして！呪術廻戦のオープニングもエンディングも劇場版0も全部良いのでアニメと共に見て欲しいです！',
@@ -22,7 +20,7 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '2',
             'content' => 'ef - a tale of memories/melodiesというアニメ是非オススメです。melodiesの方は、毎話OPが変わるので隙がありません！！',
@@ -30,7 +28,7 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '3',
             'content' => '「ひぐらしのなく頃に」ってゲームもアニメもいい曲いっぱいありますよね。',
@@ -38,7 +36,7 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '5',
             'content' => 'シュタインズゲートのOP「Hacking to the Gate」！アニメ面白いし、歌詞と内容がだんだん関連してきて、ラスト2話のOPは飛ばしちゃだめです。',
@@ -46,7 +44,7 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '4',
             'content' => 'シャドウハーツ２のED曲（月恋花）すごく良いんで聴いてみてください。諌山さんの歌声が染み入ります。',
@@ -54,7 +52,7 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '10',
             'content' => 'azumaさん。呪術廻戦、私も好きです！呪術廻戦0は3回も映画館に観にいっちゃいました。',
@@ -62,7 +60,7 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '3',
             'content' => 'イケやんさん、諌山さんの曲でプレイス・オブ・ピリオドって曲も染み入りますよ！',
@@ -70,7 +68,7 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '4',
             'content' => '品川さん聴いてみますね！オススメありがとうございます。',
@@ -78,7 +76,7 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '7',
             'content' => 'プラネテスのWonderful Lifeって曲好きです。アニメと一緒に聴いて欲しいなあ〜。エンディングの入り方が最高なんですよ☆',
@@ -86,7 +84,7 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '8',
             'content' => 'ターンAガンダムのMoon！壮大で聴き入っちゃいます。さすが菅野よう子さんです。',
@@ -94,7 +92,7 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '6',
             'content' => '初めまして。ちょっと昔のゲームなんですが、横スクロールアクションのリターン・オブ・ダブルドラゴンとコントラスピリッツのBGMが好きです。わかる人います？',
@@ -102,7 +100,7 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '2',
             'content' => 'BBさんMoon良いですね！自分はウイングガンダム好きです！！',
@@ -110,13 +108,12 @@ class PostsTableSeeder extends Seeder
             'updated_at' => $date
         ]);
 
-        $date->addSecond($i+12345);
+        $date->addSecond(12345);
         DB::table('posts')->insert([
             'user_id' => '9',
             'content' => 'アニメ映画「パプリカ」の主題歌好きです。不思議な内容と平沢進さんの独特の曲が合っててすごく良いんです。',
             'created_at' => $date,
             'updated_at' => $date
         ]);
-    }
     }
 }
