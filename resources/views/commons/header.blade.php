dc08ff6af0814197c6a099a50497b73352c2a8bf
橋本範昭 <nori.hasimo.89@gmail.com>
2023-03-20 23:07:00 +0900

    #200共通ファイル作成、#199ヘッダー・フッター & トップページのタイトルのみ表示「Topic Posts」

---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,15 @@
+<header class="mb-5">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Topic Posts</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="" class="nav-link">新規ユーザ登録</a></li>
+                <li class="nav-item"><a href="" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
