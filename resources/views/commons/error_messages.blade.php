fc5eccedf0f9eb986ddc8769326b78012d9f100e
yamadaman-bs <loxya3.sksy2@gmail.com>
2023-03-21 23:35:36 +0900

    共通ファイル/ヘッダー・フッター & トップページのタイトルのみ表示(修正)

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,3 +1,3 @@
-  <!-- <ul class="alert alert-danger" role="alert">
-            <li class="ml-4">エラーメッセージが入る場所</li>
-    </ul> -->
+  <ul class="alert alert-danger" role="alert">
+            <li class="ml-4">投稿は、必ず指定してください。</li>
+    </ul>
