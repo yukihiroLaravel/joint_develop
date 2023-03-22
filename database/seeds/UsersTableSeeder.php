bd0999faed004eaa844189ff2b99e79c0bfa6f4e
kenta sugihara <kenta03271127@gmail.com>
2023-03-22 15:03:18 +0900

    #170users（ユーザ）テーブルのマイグレーションとシーダー作成

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($val = 1; $val <= 10; $val++){
+            DB::table('users')->insert([
+                'name' => 'test'.$val,
+                'email' => 'test'.$val.'@test.com',
+                'password' => bcrypt('test'.$val)
+            ]);
+        }
+        
+    }
+}
