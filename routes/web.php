8aa7766ba997ad28d2554e469136c16d4d05a73e
橋本範昭 <nori.hasimo.89@gmail.com>
2023-03-23 23:04:05 +0900

    ログイン、ログアウト機能実装

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
