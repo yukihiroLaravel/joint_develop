2cfaab5c1991c1f0c19f94da590fdc84eb011891
kenta sugihara <kenta03271127@gmail.com>
2023-03-24 19:15:26 +0900

    #173 ユーザ新規登録 修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="text-center">
+    <div class="text-center">
         <h1><i class="fab fa-telegram fa-lg pr-3"></i>Topic Posts</h1>
     </div>
     <div class="text-center mt-3">
