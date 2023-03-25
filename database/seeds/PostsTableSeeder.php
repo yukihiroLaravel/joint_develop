e86801fa7a85dda13ebd5a058067b84b66333605
katsu <keshupinglin@gmail.com>
2023-03-25 23:55:47 +0900

    #197 posts（投稿）テーブルのマイグレーションとシーダー作成

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,10 +12,10 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         for ($val = 0; $val <= 9; $val++) {
-               DB::table('posts')->insert([
-                   'text' => 'test'.$val,
-                   'user_id' => $val + 1,
-               ]);
+            DB::table('posts')->insert([
+                'text' => 'test'.$val,
+                'user_id' => $val + 1,
+            ]);
         }
     }
 }
