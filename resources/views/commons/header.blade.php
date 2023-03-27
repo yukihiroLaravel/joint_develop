b422cc919d07e3a86582a3ea18a0830660100178
橋本範昭 <nori.hasimo.89@gmail.com>
2023-03-27 23:39:22 +0900

    #213ユーザーログイン、ログアウト機能実装

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -8,10 +8,11 @@
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
                 @if (Auth::check())
-                <li class="nav-item"><a href="{{route('logout')}}" class="nav-link">ログアウト</a></li>
-                <li class="nav-item"><a href="" class="nav-link">マイページ</a></li>
+                    <li class="nav-item"><a href="{{route('logout')}}" class="nav-link">ログアウト</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">マイページ</a></li>
                 @else
-                <li class="nav-item"><a href="{{route('login')}}" class="nav-link">ログイン</a></li>
+                    <li class="nav-item"><a href="{{route('login')}}" class="nav-link">ログイン</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">新規ユーザ登録</a></li>
                 @endif
             </ul>
         </div>
