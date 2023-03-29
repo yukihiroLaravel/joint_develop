e552098371d1b0263030e2a74c68311a07bd01dd
nidasi <nidasi@example.com>
2023-03-30 00:40:15 +0900

    #197 posts（投稿）テーブルのマイグレーションとシーダー作成

--- database/migrations/2023_03_23_004939_create_posts_table.php
+++ database/migrations/2023_03_23_004939_create_posts_table.php
@@ -22,7 +22,7 @@ class CreatePostsTable extends Migration
             $table->softDeletes();
            
             // 外部キー制約
-            //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             
     });
     }
