e17c0a949f3955ccd053ce678b999270124763a1
nidasi <nidasi@example.com>
2023-03-29 09:42:07 +0900

    マイグレーションとシーダー作成

---
+++ database/seeds/PostTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('postss')->insert([
+            'user_id' => 1,
+            'text' => 'test',
+        ]);
+        DB::table('postss')->insert([
+            'user_id' => 2,
+            'text' => 'test',
+        ]);
+
+        DB::table('postss')->insert([
+            'user_id' => 3,
+            'text' => 'test',
+        ]);
+        DB::table('postss')->insert([
+            'user_id' => 4,
+            'text' => 'test',
+        ]);
+    }
+}
+
