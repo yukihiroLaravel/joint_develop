5874821cf0cdc84e27e2cb86d373717ebc7ff677
tomoakisakizono <bless_aa_0107@icloud.com>
2023-04-04 09:41:12 +0900

    #194 トップページの投稿表示 修正版

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -6,7 +6,7 @@
             <li class="mb-3 text-center">
                 <div class="text-left d-inline-block w-75 mb-2">
                     <img class="mr-2 rounded-circle" src="{{ Gravatar::src($user->email, 55) }}" alt="ユーザのアバター画像">
-                    <p class="mt-3 mb-0 d-inline-block"><a href="#">{{ $post->user->name }}</a></p>
+                    <p class="mt-3 mb-0 d-inline-block"><a href="#">{{ $user->name }}</a></p>
                 </div>
                 <div class="">
                     <div class="text-left d-inline-block w-75">
