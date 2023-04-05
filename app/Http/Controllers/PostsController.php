697d81ef4860232e052c9e7b5174db2a6f570aae
tomoakisakizono <bless_aa_0107@icloud.com>
2023-04-05 12:05:34 +0900

    #181 投稿削除

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -15,5 +15,14 @@ class PostsController extends Controller
     
         return view('welcome', ['posts' => $posts]);
     }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        if (\Auth::id() === $post->user_id) {
+            $post->delete();
+        }
+        return back();
+    }
     
 }
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,11 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 // 投稿一覧表示
 Route::get('/', 'PostsController@index')->name('posts');
+
+// 投稿削除
+Route::group(['middleware' => 'auth'], function () {
+    // 投稿関連
+    Route::prefix('posts')->group(function () {
+        Route::delete('{id}', 'PostsController@destroy')->name('post.delete');
+    });
+});
