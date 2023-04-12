d19d1adea202a557d6332b6952f11f49b0736590
katsu <keshupinglin@gmail.com>
2023-04-12 23:07:25 +0900

    投稿編集

---
+++ resources/views/post/edit.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+<h2 class="mt-5">投稿を編集する</h2>
+<form method="" action="">
+    <div class="form-group">
+        <textarea id="content" class="form-control" name="" rows=""></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">更新する</button>
+</form>
+@endsection
