8cc8aa4b24822f38fe50b59bcf70a01b12bd25e2
tomoakisakizono <bless_aa_0107@icloud.com>
2023-04-13 09:22:24 +0900

    #186 ユーザ退会（ユーザ詳細画面に実装）

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -24,6 +24,30 @@
          <li class="nav-item"><a href="#" class="nav-link">フォロワー</a></li>
       </ul>
       @include('posts.post',['user' =>$user,'posts' => $posts])
+      <div class="d-flex justify-content-between">
+         <a class="btn btn-danger text-light" data-toggle="modal" data-target="#deleteConfirmModal">退会する</a>
+      </div>
    </div>
 </div>
 @endsection
+
+<div class="modal fade" id="deleteConfirmModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4>確認</h4>
+            </div>
+            <div class="modal-body">
+                <label>本当に退会しますか？</label>
+            </div>
+            <div class="modal-footer d-flex justify-content-between">
+                <form action="{{ route('users.delete', $user->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">退会する</button>
+                </form>
+                <button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+            </div>
+        </div>
+    </div>
+</div>
