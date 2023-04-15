424eb46e0a315f4800ee9095fbde3de04783937b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-04-15 15:17:26 +0900

    #214ユーザ編集画面・更新

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -79,7 +79,7 @@ return [
         'numeric' => 'The :attribute may not be greater than :max.',
         'file' => 'The :attribute may not be greater than :max kilobytes.',
         'string' => 'The :attribute may not be greater than :max characters.',
-        'array' => 'The :attribute may not have more than :max items.'
+        'array' => 'The :attribute may not have more than :max items.',
     ],
     'mimes' => 'The :attribute must be a file of type: :values.',
     'mimetypes' => 'The :attribute must be a file of type: :values.',
