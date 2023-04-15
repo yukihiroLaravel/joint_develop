e11363ba0880ad877f65895b511eb70b9e4bb5a3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-04-15 16:35:06 +0900

    #214ユーザ編集画面・更新実装

--- app/User.php
+++ app/User.php
@@ -29,7 +29,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
     /**
      * The attributes that should be cast to native types.
      *
@@ -38,7 +37,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
     public function posts()
     {
         return $this->hasMany(Posts::class);
