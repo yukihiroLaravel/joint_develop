57f20c10d0aa3f0495d8d0974f570924ba9a27b0
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-04-15 21:54:36 +0900

    #214ユーザ編集画面・更新実装

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use App\Http\Controllers\Controller;
 use App\User;
 use App\Posts;
 
@@ -12,11 +14,32 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::findOrFail($id);
-        $posts = $user->posts()->orderBy('id','desc')->paginate(10);
+        $posts = $user->posts()->orderBy('id', 'desc')->paginate(10);
         $data = [
             'user' => $user,
             'posts' => $posts,
         ];
-        return view('users.show',$data);
+        return view('users.show', $data);
+    }
+
+    public function edit($id)
+    {
+        if ($id == \Auth::id()) {
+            $user = \Auth::user();
+            $data = [
+                'user' => $user
+            ];
+            return view('users.edit', $data);
+        }
+    }
+
+    public function update(UserRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        return back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,21 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('/', 'PostController@index');
 //ユーザー
 Route::prefix('users')->group(function () {
-    Route::get('{id}', 'UsersController@show')->name('users.show');
+  Route::get('{id}', 'UsersController@show')->name('users.show');
 });
 //ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+//ユーザー編集、更新
+Route::group(['prefix' => 'users'], function () {
+  Route::get('{id}/edit', 'UsersController@edit')->name('user.edit');
+  Route::put('{id}', 'UsersController@update')->name('user.update');
+});
 
 // ログイン後
 Route::group(['middleware' => 'auth'], function () {
   // 投稿
   Route::prefix('post')->group(function () {
-      Route::post('', 'PostController@store')->name('post.store');
+    Route::post('', 'PostController@store')->name('post.store');
   });
 });
