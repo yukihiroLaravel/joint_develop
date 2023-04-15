7ff47bc6b8b70d1cb8c7e978d4e8b34939ac20bc
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-04-15 16:29:35 +0900

    #214ユーザ編集画面・更新実装

--- app/User.php
+++ app/User.php
@@ -26,7 +26,6 @@ class User extends Authenticatable
      *
      * @var array
      */
-
     protected $hidden = [
         'password', 'remember_token',
     ];
@@ -39,7 +38,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
     public function posts()
     {
         return $this->hasMany(Posts::class);
