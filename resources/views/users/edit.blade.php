7f64c980247d6f9566b50e089f6352f2c766f197
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-04-16 20:11:43 +0900

    ユーザー編集、更新機能(edit.blade.phpのインデント再度整理)

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -5,14 +5,13 @@
     @csrf
     @method('PUT')
     @include('commons.error_messages')
-    <div class="form-group">
+        <div class="form-group">
             <label for="name">ユーザー名</label for-"name">
             <input id="name" type="text" class="form-control" name="name" value="{{ old('name', $user->name) }}">
         </div>
         <div class="form-group">
             <label for="email">メールアドレス</label>
-            <input id="email" type="text" class="form-control" name="email"
-                value="{{ old('email', $user->email) }}">
+            <input id="email" type="text" class="form-control" name="email" value="{{ old('email', $user->email) }}">
         </div>
         <div class="form-group">
             <label for="password">パスワード</label>
@@ -20,12 +19,11 @@
         </div>
         <div class="form-group">
             <label for="password_confirmation">パスワードの確認</label>
-            <input id="password_confirmation" type="password" class="form-control" name="password_confirmation"
-                value="{{ old('password_confirmation') }}">
+            <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}">
         </div>
         <div class="d-flex justify-content-between">
             <button type="submit" class="mt-3 btn btn-danger ">退会する</button>
             <button type="submit"class="mt-3 btn btn-primary">更新する</a></button>
-    </div>
+        </div>
     </form>
 @endsection
