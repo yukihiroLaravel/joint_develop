6f569ae7a47be57c5253add8fc8f36cb066cfe32
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-04-16 18:36:21 +0900

    ユーザー編集、更機能新(フッター修正)

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="mt-2 pt-1 mb-5 bg-info">
+<footer class="mt-5  bg-info">
     <nav class="navbar navbar-dark justify-content-center">
         <span class="navbar-brand mx-auto">©Gut Familie, All rights reserved.</span>
     </nav>
