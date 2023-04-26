4f5076bdd93f6bc27bd4f1fb1624e6b4781c5274
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-04-26 20:05:21 +0900

    ユーザー退会機能(インデント)

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,15 +7,16 @@
             <div class="card-header">
                 <h3 class="card-title text-light">{{$user->name}}</h3>
             </div>
-            <div class="card-body">
+            < class="card-body">
                 <img class="rounded-circle img-fluid" src="{{ Gravatar::src($user->email, 400) }}" alt="">
                 @if (Auth::check() && Auth::user()->id == $user->id)
                     <div class="mt-3">
                         <a href="{{ route('users.edit',['id' => $user->id]) }}" class="btn btn-primary btn-block">ユーザ情報の編集</a>
                     </div>
                 @endif
-          </div>
-        </div>
+            </div>
+        </div><br>
+        @include('users.follow_button', ['followUser'=>$user])
     </aside>
     <div class="col-sm-8">
       <ul class="nav nav-tabs nav-justified mb-3">
@@ -23,7 +24,7 @@
           <li class="nav-item"><a href="#" class="nav-link">フォロー中</a></li>
           <li class="nav-item"><a href="#" class="nav-link">フォロワー</a></li>
       </ul>
-      @include('post.post',['posts' => $posts])
+      @include('post.post', ['posts' => $posts])
     </div>
  </div>
  @endsection
