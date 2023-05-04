d471bfa98886d073a6eea38e49d5d3f6f2de1afb
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-05-04 23:55:27 +0900

    シーダーの中身を変更する

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,13 +11,95 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        for($val = 1; $val <= 15; $val++){
-            DB::table('users')->insert([
-                'name' => 'test'.$val,
-                'email' => 'test'.$val.'@test.com',
-                'password' => bcrypt('test'.$val),
-                'created_at' => now()
-            ]);
-        }
+        DB::table('users')->insert([
+            'name' => '健太',
+            'email' => 'kenta@test.com',
+            'password' => bcrypt('Kenta'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Katsu',
+            'email' => 'Katsu@test.com',
+            'password' => bcrypt('Katsu'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => '橋本',
+            'email' => 'Hashimoto@test.com',
+            'password' => bcrypt('Hashimoto'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => '健太',
+            'email' => 'kenta1@test.com',
+            'password' => bcrypt('Kenta'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Katsu',
+            'email' => 'Katsu1@test.com',
+            'password' => bcrypt('Katsu'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => '橋本',
+            'email' => 'Hashimoto1@test.com',
+            'password' => bcrypt('Hashimoto'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => '健太',
+            'email' => 'kenta2@test.com',
+            'password' => bcrypt('Kenta'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Katsu',
+            'email' => 'Katsu2@test.com',
+            'password' => bcrypt('Katsu'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => '橋本',
+            'email' => 'Hashimoto3@test.com',
+            'password' => bcrypt('Hashimoto'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => '健太',
+            'email' => 'kenta4@test.com',
+            'password' => bcrypt('Kenta'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Katsu',
+            'email' => 'Katsu4@test.com',
+            'password' => bcrypt('Katsu'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => '橋本',
+            'email' => 'Hashimoto4@test.com',
+            'password' => bcrypt('Hashimoto'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => '健太',
+            'email' => 'kenta5@test.com',
+            'password' => bcrypt('Kenta'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Katsu',
+            'email' => 'Katsu5@test.com',
+            'password' => bcrypt('Katsu'),
+            'created_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => '橋本',
+            'email' => 'Hashimoto5@test.com',
+            'password' => bcrypt('Hashimoto'),
+            'created_at' => now()
+        ]);
     }
 }
