aef4920a0fe4f1cb99db2d468bed96a1ca2f66f5
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-05-06 07:14:39 +0900

    ユーザ新規登録が完了時のフラッシュメッセージ(インデント修正)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            ]);    
+        ]);    
     }
 
     protected function registered(Request $request,$user)
