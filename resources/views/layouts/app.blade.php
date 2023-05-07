e0487f676c125c261f5e63a3c4c3f943df819573
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-05-08 00:02:02 +0900

    app.blade.php修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <title>大谷翔平選手応援掲示板</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="shortcut icon" href="{{ asset('images/baseball.png') }}">
     </head>
     <body>
         @include('commons.header')
