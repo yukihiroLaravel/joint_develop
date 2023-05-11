9d2c83580747aedf5540a20f925a7f501bff1bf0
yuki ohashi <yukiohashi1119@gmail.com>
2023-05-12 00:26:19 +0900

    #272 users（ユーザ）テーブルのマイグレーションとシーダー作成

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
