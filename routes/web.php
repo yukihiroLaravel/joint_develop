1e20550b190be74a6c488490d5487995aae41ec7
u0222 <ootayy411@gmail.com>
2023-05-14 07:27:32 +0900

    ユーザー新規登録（途中）

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+// ユーザ新規登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 Route::get('/', function () {
     return view('welcome');
 });
