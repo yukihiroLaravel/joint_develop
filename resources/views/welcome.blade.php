070cc6e22d1fdf709ddc237d91ee9e0f763afee1
masahiro-mera <romasahiro5612@gmail.com>
2023-05-17 00:08:58 +0900

    共通ファイル作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="center jumbotron bg-info">
+    <div class="center jumbotron bg-info">
         <div class="text-center text-white mt-2 pt-1">
              <h1><i class="fab fa-telegram fa-lg pr-3"></i>Topic Posts</h1>
         </div>
