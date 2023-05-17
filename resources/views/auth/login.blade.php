50f95acffa8b1214809daaac6f554f4f20004579
daiki morishima <mighty19960220d@gmail.com>
2023-05-17 19:32:43 +0900

    #264ユーザログイン・ログアウト

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,15 +11,16 @@
     </div>
     <div class="row mt-5 mb-5">
         <div class="col-sm-6 offset-sm-3">
+        @include('commons.error_messages')
             <form method="POST" action="{{ route('login.post') }}">
                 @csrf
                 <div class="form-group">
                     <label for="email">メールアドレス</label>
-                    <input id="email" type="text" class="form-control" name="" value="">
+                    <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}">
                 </div>
                 <div class="form-group">
                     <label for="password">パスワード</label>
-                    <input id="password" type="password" class="form-control" name="" value="">
+                    <input id="password" type="password" class="form-control" name="password" value="{{ old('password') }}">
                 </div>
                 <button type="submit" class="btn btn-primary mt-2">ログイン</button>
             </form>
