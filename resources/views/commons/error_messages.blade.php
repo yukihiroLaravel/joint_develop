10ff98d8c991b43bb3665de98e5e780027752a06
masahiro-mera <romasahiro5612@gmail.com>
2023-05-18 16:26:10 +0900

    #275#278共通ファイル作成＋ヘッダー・フッターとトップページのタイトル表示

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,5 +1,7 @@
 @if (count($errors) > 0)
     <ul class="alert alert-danger" role="alert">
-            <li class="ml-4"></li>
+        @foreach ($errors->all() as $error)
+            <li class="ml-4">{{ $error }}</li>
+        @endforeach
     </ul>
 @endif
--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -9,9 +9,9 @@
                 <ul class="navbar-nav">                    
                      @if (Auth::check())
                        <li class="nav-item"><a href="" class="nav-link text-light">ログインユーザ名</a></li>
-                       <li class="nav-item"><a href="" class="nav-link">ログアウト</a></li>                   
+                       <li class="nav-item"><a href="{{ route('logout') }}"class="nav-link">ログアウト</a></li>                   
                      @else
-                       <li class="nav-item"><a href="get" class="nav-link">ログイン</a></li>
+                       <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">ログイン</a></li>
                        <li class="nav-item"><a href="" class="nav-link">新規ユーザ登録</a></li>
                      @endif
                 </ul>       
