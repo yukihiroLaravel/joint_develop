c4d68e4a662c1e8e2b3d18f6fb7bb73511720c4c
yuki ninomiya <kyuu.195@gmail.com>
2023-05-20 09:24:58 +0900

    #262 ユーザログイン・ログアウト

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@
 */
 
 Route::get('/','UsersController@index');
+// ログイン
+Route::get('login','Auth\LoginController@showLoginForm')->name('login');
+Route::post('login','Auth\LoginController@login')->name('login.post');
+Route::get('logout','Auth\LoginController@logout')->name('logout');
