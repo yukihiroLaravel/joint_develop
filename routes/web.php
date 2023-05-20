1b2f7bbf215b5888b34b72e4fae85d6243bab9b2
toshiki kawauchi <toshiki242zwo@gmail.com>
2023-05-20 19:12:09 +0900

    ユーザ詳細

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
+// ユーザ
+Route::get('/', 'VerificationControllerr@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'VerificationController@show')->name('user.show');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
