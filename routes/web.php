36c77a03e5131b107c01647e592e53207e368306
naoki mitamura <zango3333@yahoo.co.jp>
2023-05-24 20:35:31 +0900

    #301 ユーザー詳細

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
-//
-Route::get('/', 'UsersController@index');
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+Route::get('/', 'UsersController@index');
 
 Route::prefix('users')->group(function(){
     Route::get('{id}','UsersController@show')->name('users.show');
