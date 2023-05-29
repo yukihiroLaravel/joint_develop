9b1662085561f2f448e43cc0d6e901937b3a756e
naoki mitamura <zango3333@yahoo.co.jp>
2023-05-29 23:15:32 +0900

    #301 ユーザー詳細

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,7 +7,7 @@
                     <h3 class="card-title text-light">{{ $user->name }}</h3>
                 </div>
                 <div class="card-body">
-                <img class="rounded-circle img-fluid" src="{{ Gravatar::src($user->email, 55) }}" alt="ユーザーアバター画像">
+                <img class="rounded-circle img-fluid" src="{{ Gravatar::src($user->email, 400) }}" alt="ユーザーアバター画像">
                         <div class="mt-3">
                             <a href="" class="btn btn-primary btn-block">ユーザ情報の編集</a>
                         </div>
@@ -16,11 +16,13 @@
         </aside>
         <div class="col-sm-8">
             <ul class="nav nav-tabs nav-justified mb-3">
-                <li class="nav-item"><a href="{{ route( 'user.show', $user->id }}" class="nav-link {{ Request::is('users/'. $user->id) ? 'active' : '' }}">タイムライン</a></li>
+                <li class="nav-item"><a href="{{ route('user.show', $user->id) }}" class="nav-link {{ Request::is('users/'. $user->id) ? 'active' : '' }}">タイムライン</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">フォロー中</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">フォロワー</a></li>
             </ul>
-            @include('posts.posts', ['posts' => $posts])
+             <ul class="list-unstyled">
+              @include('posts.posts', ['posts' => $posts])
+             </ul>
         </div>
 </div>
 @endsection
