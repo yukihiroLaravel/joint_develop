ba50c2949df46d77f335bdc5f921e58ee165afc0
yuki ohashi <yukiohashi1119@gmail.com>
2023-05-29 23:18:03 +0900

    #327 投稿新規作成(ユウキ)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,20 @@
             <h1><i class="fab fa-telegram fa-lg pr-3"></i>Topic Posts</h1>
         </div>
     </div>
-    <h5 class="text-center mb-3">"〇〇"について140字以内で会話しよう！</h5>        
-    <div class="container">                  
-        @include('commons.error_messages')
-    </div>                  
-    <div class="text-center mb-3">
-        <form method="" action="" class="d-inline-block w-75">
-            <div class="form-group">                  
-            </div>         
+    <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>        
+    <div class="w-75 m-auto">@include('commons.error_messages')</div>
+        <div class="text-center mb-3">
+            @if (Auth::check())
+                <form method="POST" action="{{route('post.store')}}" class="d-inline-block w-75">
+                    @csrf
+                    <div class="form-group">
+                        <textarea class="form-control" name="content" rows="4"></textarea>
+                        <div class="text-left mt-3">
+                            <button type="submit" class="btn btn-primary">投稿する</button>
+                        </div>
+                    </div>
+                </form>
+            @endif   
         </form>
         @include('posts.posts', ['posts' => $posts])
     </div>        
