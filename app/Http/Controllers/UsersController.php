9388071f4e86ab2aabb532e510a509862893635c
masahiro-mera <romasahiro5612@gmail.com>
2023-06-04 00:45:48 +0900

    #302ユーザ編集・更新

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -49,8 +49,8 @@ class UsersController extends Controller
             $user->name = $request->name;
             $user->email = $request->email;
             $user->password = bcrypt($request->password);     
-            $user->save();
-            return back(); 
-        }       
+            $user->save();           
+        }     
+        return back();   
    }
 }
