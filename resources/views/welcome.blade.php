78dd17ab918ae6ec6fb9bf361eef9edb15083c21
tomoki amemiya <tomoki423@outlook.com>
2023-06-07 17:20:28 +0900

    #347 comments（コメント）テーブルのマイグレーションとシーダーと表示

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
-</div>
+    </div>
 @endif
 @if (session('redMessage'))
     <div class="alert alert-danger alert-dismissible fade show mx-auto w-75" role="alert">
