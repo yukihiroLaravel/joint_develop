55fb23685b68e42c6fdb90f49b408494d62b7198
tomoki amemiya <tomoki423@outlook.com>
2023-06-12 14:32:19 +0900

    #357 コメント削除(途中)

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -15,7 +15,7 @@
             <div class="">
                 <div class="text-left d-inline-block w-75">
                     <strong>
-                        <p class="mb-2">{{ $post->text }}</p>
+                        <p class="mb-2">{!!nl2br(e($post->text))!!}</p>
                     </strong>
                     <p class="text-muted">{{ $post->updated_at }}</p>
                 </div>
@@ -33,7 +33,7 @@
                     <h5 class="card-header">コメント</h5>
                     <div class="card-body">
                         @foreach ($post->comments as $comment)
-                            <div class="text-left d-inline-block w-75">
+                            <div class="text-left d-inline-block w-75 mb-2">
                                 <span>
                                     @if($comment->user->email)
                                         <img class="rounded-circle img-fluid" src="{{ Gravatar::src($comment->user->email, 30) }}"
@@ -44,19 +44,14 @@
                                     @endif
                                 </span><br>
                                 <span class="card-text">{!!nl2br(e($comment->comment))!!}</span>
-                                <p class="text-muted">
-                                    {{ $comment->updated_at }}
-                                    @if ($comment->user->id == Auth::id())
-                                        <a class="delete-comment" data-remote="true" rel="nofollow" data-method="delete"
-                                            href="/comments/{{ $comment->id }}">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                                <title>コメントを削除する</title>
-                                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                            </svg>
-                                        </a>
-                                    @endif
-                                </p>
+                                <p class="text-muted">{{ $comment->updated_at }}</p>
+                                @if ($comment->user->id == Auth::id())
+                                    <form method="POST" action="{{ route('comment.delete', $comment->id) }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger mb-2">コメント削除</button>
+                                    </form>
+                                @endif
                             </div>
                         @endforeach
                     </div>
