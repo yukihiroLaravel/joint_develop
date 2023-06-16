dc06385eaf7f811bc2bfdb009380df7929870d8f
yuki ohashi <yukiohashi1119@gmail.com>
2023-06-16 21:23:37 +0900

    #374 フォローによるタイムライン変更_修正ミス再修正（ユウキ）

--- resources/views/users/follower.blade.php
+++ resources/views/users/follower.blade.php
@@ -21,9 +21,9 @@
         </aside>
         <div class="col-sm-8">
             <ul class="nav nav-tabs nav-justified mb-3">
-                <li class="nav-item"><a href="{{ route('user.show', $user->id) }}" class="nav-link {{ Request::is('users/'. $user->id) ? 'active' : '' }}">タイムライン</a></li>
-                <li class="nav-item"><a href="{{ route('user.following', $user->id) }}" class="nav-link {{ Request::is('users/'. $user->id. '/followingList') ? 'active' : '' }}">フォロー中</a></li>
-                <li class="nav-item"><a href="{{ route('user.follower', $user->id) }}" class="nav-link {{ Request::is('users/'. $user->id. '/followerList') ? 'active' : '' }}">フォロワー</a></li>
+                <li class="nav-item"><a href="{{ route('user.show', $user->id) }}" class="nav-link {{ Request::routeIs('user.show') ? 'active' : '' }}">タイムライン</a></li>
+                <li class="nav-item"><a href="{{ route('user.following', $user->id) }}" class="nav-link {{ Request::routeIs('user.following') ? 'active' : '' }}">フォロー中</a></li>
+                <li class="nav-item"><a href="{{ route('user.follower', $user->id) }}" class="nav-link {{ Request::routeIs('user.follower') ? 'active' : '' }}">フォロワー</a></li>
             </ul>
             @include('follow.follower_list', ['follower' => $followers])
         </div>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -21,9 +21,9 @@
         </aside>
         <div class="col-sm-8">
             <ul class="nav nav-tabs nav-justified mb-3">
-                <li class="nav-item"><a href="{{ route('user.show', $user->id) }}" class="nav-link {{ Request::routeIs('user.show') }}">タイムライン</a></li>
-                <li class="nav-item"><a href="{{ route('user.following', $user->id) }}" class="nav-link {{ Request::routeIs('user.following') }}">フォロー中</a></li>
-                <li class="nav-item"><a href="{{ route('user.follower', $user->id) }}" class="nav-link {{ Request::routeIs('user.follower') }}">フォロワー</a></li>
+                <li class="nav-item"><a href="{{ route('user.show', $user->id) }}" class="nav-link {{ Request::routeIs('user.show') ? 'active' : '' }}">タイムライン</a></li>
+                <li class="nav-item"><a href="{{ route('user.following', $user->id) }}" class="nav-link {{ Request::routeIs('user.following') ? 'active' : '' }}">フォロー中</a></li>
+                <li class="nav-item"><a href="{{ route('user.follower', $user->id) }}" class="nav-link {{ Request::routeIs('user.follower') ? 'active' : '' }}">フォロワー</a></li>
             </ul>
             @include('posts.posts', ['posts' => $posts])
         </div>
