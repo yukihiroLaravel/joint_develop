c6bd2f78163d8063ca6588863652fd40f68d695e
yuki ninomiya <kyuu.195@gmail.com>
2023-06-30 22:35:56 +0900

    内容の修正2回目

--- resources/views/posts/new_post.blade.php
+++ resources/views/posts/new_post.blade.php
@@ -7,15 +7,19 @@
             @csrf
             <input type="hidden" name="posts">
             <div class="form-group">
-                <textarea class="form-control @error('text') is-invalid @enderror" name="text" rows="4" value="{{ old('text') }}"></textarea>
-                <i class="far fa-image"></i>
-                <input type="file" name="img_path" placeholder="画像投稿">
+                <div class ="mb-3">
+                    <textarea class="form-control @error('text') is-invalid @enderror" name="text" rows="4" value="{{ old('text') }}"></textarea>
+                </div>
+                <div>
+                    <i class="far fa-image"></i>
+                    <input type="file" name="img_path" placeholder="画像投稿">
+                </div>    
                 @error('text')
                     <span class="invalid-feedback text-left" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
-                <div class="text-left mt-3">
+                <div class="text-left mt-5">
                     <button type="submit" class="btn btn-primary">
                         <i class="fas fa-pen"></i> 投稿する
                     </button>
