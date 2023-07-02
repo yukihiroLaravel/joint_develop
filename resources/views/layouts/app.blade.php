62970b084bddb027fdcd9d2ae1dc3e1968483dba
tomoki amemiya <tomoki423@outlook.com>
2023-07-02 17:47:36 +0900

    #415 デザイン変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,12 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Topic Posts</title>
+
+    <link rel="shortcut icon" href="{{ asset('images/icon/favicon_32.ico') }}" />
+    <link rel="icon" type="image/vnd.microsoft.icon" href="{{ asset('images/icon/favicon_128.ico') }}" />
+    <link rel="apple-touch-icon" href="{{ asset('images/icon/favicon_128.ico') }}" />
+
+    <title>おうち時間の過ごし方</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
