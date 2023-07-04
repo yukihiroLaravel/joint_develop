e6be530c64113f0dc50fa19e5f527a86ffefbe8b
u0222 <ootayy411@gmail.com>
2023-07-04 15:30:58 +0900

    #447 全体の文章見直し

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,17 +9,17 @@
     <h5 class="text-center mb-5">「GiriGiri」は無料で楽しめるネット大喜利。<br>ユーザー登録すると<br>お題・ボケ投稿やワロタ・フォローもできます。</h5>
     <div class="w-75 m-auto">@include('commons.error_messages')</div>
     @if (Auth::check())
-    <div class="text-center mb-3">
-        <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
-            @csrf
-            <div class="form-group">
-                <textarea class="form-control" name="text" rows="4" placeholder="お題を入力してください">{{ old('text') }}</textarea>
-                <div class="text-left mt-3">
-                    <button type="submit" class="btn btn-primary">お題を投稿する</button>
+        <div class="text-center mb-3">
+            <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
+                @csrf
+                <div class="form-group">
+                    <textarea class="form-control" name="text" rows="4" placeholder="お題を入力してください">{{ old('text') }}</textarea>
+                    <div class="text-left mt-3">
+                        <button type="submit" class="btn btn-primary">お題を投稿する</button>
+                    </div>
                 </div>
-            </div>
-        </form>
-    </div>
+            </form>
+        </div>
     @endif
     <h1 class="text-center mb-3">《 お 題 》</h1>
     @include('posts.posts',['posts' => $posts])
