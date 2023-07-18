df93e3e79926db6e143fb4af587632fc2c370689
daiki tomomatsu <kurabutouryuumon@yahoo.co.jp>
2023-07-19 00:58:06 +0900

    Modelsディレクトリ削除. 既存のUserモデルの修正。Factoryを使わずにテストデータを2件追加。その後Factoryでテストデータを10件追加

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
+use App\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +14,23 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+            // DB::table('users')->insert([
+            //     [
+            //         'name' => 'テストユーザー1',
+            //         'email'     => 'test1@test.com',
+            //         'email_verified_at' => now(),
+            //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            //         'remember_token' => Str::random(10),
+            //     ],
+            //     [
+            //         'name' => 'テストユーザー2',
+            //         'email'     => 'test2@test.com',
+            //         'email_verified_at' => now(),
+            //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            //         'remember_token' => Str::random(10),
+            //     ]
+            // ]);
+                
         factory(User::class, 10)->create();
     }
 }
