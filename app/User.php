8e5b78caf9c9b9b04a4d9b6bd668adefdfdc105e
shinsuke kurokawa <x14pro1999@gmail.com>
2023-07-18 09:03:49 +0900

    postsテーブルのマイグレーションとシーダー作成

--- app/User.php
+++ app/User.php
@@ -5,10 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Softdeletes;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use Softdeletes;
 
     /**
      * The attributes that are mass assignable.
