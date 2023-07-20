3b41e9f321efb08134ce63729488c0864d15aaca
daiki tomomatsu <kurabutouryuumon@yahoo.co.jp>
2023-07-21 00:09:37 +0900

    コメントアウト部分削除

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
-// use Illuminate\Support\Str;  
 use Illuminate\Support\Facades\DB;
-
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -14,23 +12,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // DB::table('users')->insert([
-        //     [
-        //         'name' => 'テストユーザー1',
-        //         'email'     => 'test1@test.com',
-        //         'email_verified_at' => now(),
-        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        //         'password' => bcrypt('password'),
-        //         'remember_token' => Str::random(10),
-        //     ],
-        //     [
-        //         'name' => 'テストユーザー2',
-        //         'email'     => 'test2@test.com',
-        //         'email_verified_at' => now(),
-        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        //         'remember_token' => Str::random(10),
-        //     ]
-        // ]);
 
         for ($i=1; $i <=2 ; $i++) { 
             DB::table('users')->insert([
@@ -46,5 +27,6 @@ class UsersTableSeeder extends Seeder
         }
 
         factory(User::class, 10)->create();
+        
     }
 }
