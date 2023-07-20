53d82d7a77a0908786a8edc9d9c0e9aa98498f8a
daiki tomomatsu <kurabutouryuumon@yahoo.co.jp>
2023-07-20 22:20:52 +0900

    for文を使ってseederファイル修正。パスワードのハッシュ化処理修正。remember_tokenカラムへはデータを入れないように修正。created_atとupdated_atにはダミーデータをいれるように修正。

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
-use Illuminate\Support\Str;
+// use Illuminate\Support\Str;  
 use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
@@ -14,22 +14,36 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-            // DB::table('users')->insert([
-            //     [
-            //         'name' => 'テストユーザー1',
-            //         'email'     => 'test1@test.com',
-            //         'email_verified_at' => now(),
-            //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            //         'remember_token' => Str::random(10),
-            //     ],
-            //     [
-            //         'name' => 'テストユーザー2',
-            //         'email'     => 'test2@test.com',
-            //         'email_verified_at' => now(),
-            //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            //         'remember_token' => Str::random(10),
-            //     ]
-            // ]);
+        // DB::table('users')->insert([
+        //     [
+        //         'name' => 'テストユーザー1',
+        //         'email'     => 'test1@test.com',
+        //         'email_verified_at' => now(),
+        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //         'password' => bcrypt('password'),
+        //         'remember_token' => Str::random(10),
+        //     ],
+        //     [
+        //         'name' => 'テストユーザー2',
+        //         'email'     => 'test2@test.com',
+        //         'email_verified_at' => now(),
+        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //         'remember_token' => Str::random(10),
+        //     ]
+        // ]);
+
+        for ($i=1; $i <=2 ; $i++) { 
+            DB::table('users')->insert([
+                    [
+                        'name' => 'テストユーザー' . $i,
+                        'email'     => 'test' . $i . '@test.com',
+                        'email_verified_at' => now(),
+                        'password' => bcrypt('password'),
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ],
+            ]);
+        }
 
         factory(User::class, 10)->create();
     }
