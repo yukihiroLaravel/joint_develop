09af97316db240a60092e2f2ec9016bac3e56397
shinsuke kurokawa <x14pro1999@gmail.com>
2023-07-21 23:38:48 +0900

    seederfile修正。migrationfile作成。

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,26 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'test1',
-            'email' => 'test1@test.com',
-            'password' => bcrypt('test1')
-        ]);
-        DB::table('users')->insert([
-            'name' => 'test2',
-            'email' => 'test2@test.com',
-            'password' => bcrypt('test2')
-        ]);
-        DB::table('users')->insert([
-            'name' => 'test3',
-            'email' => 'test3@sample.com',
-            'password' => bcrypt('test3')
-        ]);
-        DB::table('users')->insert([
-            'name' => 'test4',
-            'email' => 'test4@test.com',
-            'password' => bcrypt('test4')
-        ]);
+        
     }
 }
     
