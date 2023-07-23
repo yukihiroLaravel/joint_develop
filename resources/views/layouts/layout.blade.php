c5345df0f51bc613279f1cbb6178d12f08b42fdd
daiki tomomatsu <kurabutouryuumon@yahoo.co.jp>
2023-07-23 18:48:57 +0900

    ログインフォーム作成。ログインフォームを表示させるルーティング、login/logoutメソッドまでのルーティング実装。ヘッダーでログイン・ログアウトができるように実装。またヘッダーにユーザー名が表示されるように実装。バリデーションメッセージが表示されるように、バリデーションメッセージようのテンプレートファイル作成。

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <title>@yield('title','Topic Posts')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
--- resources/views/posts/top.blade.php
+++ resources/views/posts/top.blade.php
@@ -6,6 +6,7 @@
     </div>
 </div>
 <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
+@if (Auth::check())
 <div class="w-75 m-auto">エラーメッセージが入る場所</div>
 <div class="text-center mb-3">
     <form method="" action="" class="d-inline-block w-75">
@@ -17,4 +18,5 @@
         </div>
     </form>
 </div>
+@endif
 @endsection
