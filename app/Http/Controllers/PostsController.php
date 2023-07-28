c630b7f89a12ced682244057a11e45248ae19d30
Shogo Uozumi <square.7.podod@gmail.com>
2023-07-28 13:16:19 +0900

    投稿登録・ユーザ詳細

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Post;
+use App\Http\Requests\PostRequest;
+
+class PostsController extends Controller
+{
+    public function store(PostRequest $request)
+    {
+        $post = new Post;
+        $post->text = $request->text;
+        $post->user_id = $request->user()->id;
+        $post->save();
+        return back();
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        if (\Auth::id() === $post->user_id) {
+            $post->delete();
+        }
+        return back();
+    }
+
+    public function edit($id)
+    {
+        $user = \Auth::user();
+        $post = Post::findOrFail($id);
+        $data=[
+            'user' => $user,
+            'post' => $post,
+        ];
+        return view('post.edit', $data);
+    }
+
+    public function update(PostRequest $request, $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->text = $request->text;
+        $post->user_id = $request->user()->id;
+        $post->save();
+        return back();
+    }
+}
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,9 @@
+<h2 class="mt-5">投稿を編集する</h2>
+    <form method="POST" action="{{ route('post.update', $post->id) }}">
+    @csrf
+    @method('PUT')
+    <div class="form-group">
+            <textarea id="content" class="form-control" name="text" rows="6"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">更新する</button>
+    </form>
