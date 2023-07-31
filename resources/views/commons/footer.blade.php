a8f52e95f224369b8b1aa11499e5db78ce3ef340
Shogo Uozumi <square.7.podod@gmail.com>
2023-08-01 08:27:16 +0900

    ヘッダー

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="mt-5">
+    <nav class="navbar navbar-dark bg-info justify-content-center">
+        <span class="navbar-brand">©Gut Familie, All rights reserved.</span>
+    </nav>
+</footer>
