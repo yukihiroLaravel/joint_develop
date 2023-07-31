e395354c40cffc920813f37b28250a0bc1895bfe
jyunpei fukano <fukanojyunpei@gmail.com>
2023-07-31 06:53:27 +0000

    posts

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function movies()
+    {
+        return $this->hasMany(posts::class);
+    }
 }
