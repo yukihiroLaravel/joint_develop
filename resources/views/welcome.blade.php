f4c93859f9160f14b9a888eab4d6b7aac3802976
Shogo Uozumi <square.7.podod@gmail.com>
2023-08-01 17:16:27 +0900

    ログインログアウト機能

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,12 +9,14 @@
     <div class="w-75 m-auto">エラーメッセージが入る場所</div>
     <div class="text-center mb-3">
         <form method="" action="" class="d-inline-block w-75">
-            <div class="form-group">
+        @if(Auth::check())    
+        <div class="form-group">
                 <textarea class="form-control" name="" rows=""></textarea>
                 <div class="text-left mt-3">
                     <button type="submit" class="btn btn-primary">投稿する</button>
                 </div>
             </div>
+        @endif
         </form>
     </div>
 @endsection
