df662335c2c160a65bb460928151253a694f1a70
Shogo Uozumi <square.7.podod@gmail.com>
2023-08-03 18:39:21 +0900

    共通ファイル・ヘッダー・フッターの作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="center jumbotron bg-info">
         <div class="text-center text-white mt-2 pt-1">
-            <h1><i class="pr-3 fa fa-paper-plane"></i>Topic Posts</h1>
+            <h1><i class="pr-3 fab fa-telegram fa-lg"></i>Topic Posts</h1>
         </div>
 </div>
 <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
