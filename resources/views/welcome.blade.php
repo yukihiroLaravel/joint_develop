5b2b731edf544638d034198085bb639b78065084
Shogo Uozumi <square.7.podod@gmail.com>
2023-08-05 14:57:08 +0900

    トップページの投稿表示

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
         <div class="form-group">
                 <textarea class="form-control" name="text" rows="4"></textarea>
                 <div class="text-left mt-3">
-                    <button type="submit" class="btn btn-primary">投稿する</button>
+                    <button type="submit" class="btn btn-primary mb-3">投稿する</button>
                 </div>
             </div>
         @endif
