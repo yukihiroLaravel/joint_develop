31414c099cde1cee5c2f927c048d72f76d81c50e
Shogo Uozumi <square.7.podod@gmail.com>
2023-08-07 08:28:29 +0900

    ユーザー詳細

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -4,7 +4,7 @@
         
         <div class="text-left d-inline-block w-75 mb-2">
             <img class="mr-2 rounded-circle" src="{{ Gravatar::src($post->user->email, 55) }}" alt="ユーザのアバター画像">
-            <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('user.show',$post->user->id) }}">{{ $post->user->name }}</a></p>
+            <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('user.show', $post->user->id) }}">{{ $post->user->name }}</a></p>
         </div>
         
         <div class="">
