c7153697b028656f6d32d7d062871012b5b45ab3
yukihiro seki <yukihiro@example.com>
2023-08-06 23:26:37 +0900

    #574 ユーザ編集画面・更新

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Request\UsersRequest;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function edit($id)
+    {
+        $user = \Auth::user();
+        $data=[
+            'user' => $user,
+        ];
+        return view('users.edit', $data);
+    }
+
+    public function update(UsersRequest $request, $id)
+    {
+        $user = user::findOrFail($id);
+        $user->email = $request->email;
+        $user->password = $request->password; 
+        $user->save();
+        return back();
+    }
+
+}    
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+Route::get('/', 'UsersController@index');
+
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -20,3 +22,14 @@ Route::get('/', 'PostsController@index');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+
+// ログイン後
+Route::group(['middleware' => 'auth'], function () {
+    // ユーザ情報編集
+    Route::prefix('users')->group(function () {
+        Route::get('{id}', 'UsersController@edit')->name('user.edit');
+        Route::put('{id}', 'UsersController@update')->name('users.update');
+    });
+});
+
