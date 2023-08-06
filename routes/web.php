2ab3e405e18af447256936749e9caac99a74e1b8
yamada naoki <naoki19980422@gmail.com>
2023-08-06 16:37:22 +0900

    3章 ログイン機能

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
+// ログイン
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
 Route::get("/","UsersController@index");
 // Route::get('/', function () {
 //     return view('welcome');
