13d2a3988014b26ac285b1d040166139dbbe830a
yukihiro seki <yukihiro@example.com>
2023-08-08 23:40:32 +0900

    #574 ユーザ編集画面・更新

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,15 +7,10 @@ use App\Http\Request\UsersRequest;
 
 class UsersController extends Controller
 {
-    public function index()
-    {
-        return view('welcome');
-    }
-
-    public function edit($id)
+       public function edit($id)
     {
         $user = \Auth::user();
-        $data=[
+        $data = [
             'user' => $user,
         ];
         return view('users.edit', $data);
@@ -23,7 +18,7 @@ class UsersController extends Controller
 
     public function update(UsersRequest $request, $id)
     {
-        $user = user::findOrFail($id);
+        $user = User::findOrFail($id);
         $user->name = $repuest->name;
         $user->email = $request->email;
         $user->password = bcrypt($request->password); 
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-Route::get('/', 'UsersController@index');
-
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
