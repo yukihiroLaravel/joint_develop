2ad1354b33869ea59325ad3e48fb3e20a0f460e3
daiki tomomatsu <kurabutouryuumon@yahoo.co.jp>
2023-08-09 19:09:43 +0900

    ユーザー情報編集画面作成。ユーザー情報更新のときのバリデーション作成。ユーザー情報を編集してDBへ保存する機能実装。

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,19 +4,57 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use App\user;
 use App\Post;
+use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\Auth;
 
-use Illuminate\Http\Request;
-
-/**
- * トップページを表示。
- * @param void
- * @return view
- */
 class PostController extends Controller
 {
+    /**
+     * トップページを表示。
+     * @param void
+     * @return view
+     */
     public function showTop () {
         $posts = Post::orderBy('created_at','desc')->paginate(10);
 
         return view('posts.top', ['posts' => $posts]);
+    }
+
+    /**
+     * ユーザー編集フォームを表示。
+     * @param $id
+     * @return view
+     */
+    public function showUserEdit ($id) {
+        if (Auth::id() === (int)$id) {
+            $users = User::find($id);
+            return view('posts.edit_user', ['users' => $users]);
+        }else{
+            abort(404);
+        }
+    }
+
+    /**
+     * 更新したユーザー情報をDBへ保存。
+     * @param object $request
+     * @return view
+     */
+    public function updateUser (UserRequest $request) {
+        $inputs = $request->all();
+        DB::BeginTransaction();
+        try {
+            $user = User::find($inputs['id']);
+            $user->fill([
+                'name' => $inputs['name'],
+                'email' => $inputs['email'],
+                'password' => bcrypt($inputs['password']),
+            ]);
+            $user->save();
+            DB::commit();
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            abort(500);
+        }
+        return redirect(route('top'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', 'PostController@showTop')->name('top');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('loginform');
 Route::post('login', 'Auth\LoginController@login')->name('login');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('users/{id}/edit', 'PostController@showUserEdit')->name('editform');
+Route::post('update/user', 'PostController@updateUser')->name('updateuser');
 
 // Route::get('/', function () {
 //     return view('welcome');
