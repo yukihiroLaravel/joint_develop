03c80c1dac76e3ee71bfa5e27fc2fc10c63a5e6f
jyunpei fukano <fukanojyunpei@gmail.com>
2023-08-12 05:05:05 +0000

    バリデーションメッセージの日本語化

--- resources/lang/ja/auth.php
+++ resources/lang/ja/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => '認証に失敗しました。',
+    'throttle' => 'ログイン試行が規定回数を超えました。:seconds秒後に再開できます。',
 
 ];
