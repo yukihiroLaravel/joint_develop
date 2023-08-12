7112d5043ed07cb32df138b6350ed584c643f180
Shogo Uozumi <square.7.podod@gmail.com>
2023-08-12 17:16:20 +0900

    フォロー機能

--- resources/views/users/follow_follower_count.blade.php
+++ resources/views/users/follow_follower_count.blade.php
@@ -13,10 +13,10 @@
     
     
 <div class="row ml-2">
-<span class="col-6">フォロー中！
-    <span class="badge badge-pill badge-success">{{ $countFollowingUsers }}</span>
-</span>
-<span class="col-6">フォロワー！
-    <span class="badge badge-pill badge-success">{{ $countFollowerUsers }}</span>
-</span>
+    <span class="col-6">フォロー中！
+        <span class="badge badge-pill badge-success">{{ $countFollowingUsers }}</span>
+    </span>
+    <span class="col-6">フォロワー！
+        <span class="badge badge-pill badge-success">{{ $countFollowerUsers }}</span>
+    </span>
 </div>
