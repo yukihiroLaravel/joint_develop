26f7458fa008b6f33770a0df222d3b375416a518
jyunpei fukano <fukanojyunpei@gmail.com>
2023-08-13 13:29:00 +0000

    ユーザー詳細

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,17 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        $user = $user->orderBy('id', 'desc');
+        $data=[
+            'user' => $user,
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show',$data);
+    }
+    
     public function index()
     {
         return view('welcome');
