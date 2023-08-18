c448b78e0b52c131627cd5dfdcf3177ec65afe70
yukihiro seki <yukihiro@example.com>
2023-08-19 03:26:36 +0900

    #557 新規投稿作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\User;
 use App\Post;
 use App\Http\Request\PostsRequest; 
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="center jumbotron bg-info">
-    <div class="text-center text-white mt-2 pt-1">
-         <h1><i class="fab fa-telegram pr-2000 d-inline"></i>Topic Posts</h1>
+    <div class="center jumbotron bg-info">
+        <div class="text-center text-white mt-2 pt-1">
+            <h1><i class="fab fa-telegram pr-2000 d-inline"></i>Topic Posts</h1>
+        </div>
     </div>
-</div>
-<h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
+    <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
 
-<form method="POST" action="{{ route('post.store') }}">
+    <form method="POST" action="{{ route('post.store') }}">
 
 @if(Auth::check())
 @include('commons.error_messages')
 @csrf
-<div class="text-center">
-    <div class="form-group">
-　　<textarea style="width:860px;" rows="5" name="text" value="{{ old('text') }}"></textarea>
+    <div class="text-center">
+        <div class="form-group">
+            <textarea class="form-control" name="text"  rows="4" value="{{ old('text') }}"></textarea>
+        </div>
+    </div>    
+    <div class="mb-3 text-center">
+        <div class="text-left d-inline-block w-75 mb-2">
+            <button type="submit" class="btn btn-primary">投稿する</a>
+        </div>
     </div>
-</div>    
-<div class="mb-3 text-center">
-    <div class="text-left d-inline-block w-75 mb-2">
-    <button type="submit" class="btn btn-primary">投稿する</a>
-    </div>
-</div>
-</form>
+    </form>
 @endif
 @endsection
