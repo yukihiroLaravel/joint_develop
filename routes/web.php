f3a09e1c4816f6448ddb8d76443e30bd92155fb5
jyunpei fukano <fukanojyunpei@gmail.com>
2023-08-26 15:24:53 +0000

    ユーザー詳細朝改3

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,15 @@ Route::get('/', 'PostsController@index');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('users/{id}', 'UsersController@show')->name('user.show');
+
+// ログイン後
+Route::group(['middleware' => 'auth'], function () 
+{
+    // ユーザ情報編集
+    Route::prefix('users')->group(function () {
+        Route::get('{id}/edit', 'UsersController@edit')->name('user.edit');
+        Route::put('{id}', 'UsersController@update')->name('users.update');
+    });
+});
