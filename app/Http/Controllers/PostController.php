d716d2fa748083d96c589eab8ab460d20bd7cd77
shinsuke kurokawa <x14pro1999@gmail.com>
2023-08-27 22:58:49 +0900

    投稿編集画面とDBを更新するロジックを実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Post;
 use App\Http\Requests\PostRequest;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -30,5 +31,43 @@ class PostController extends Controller
             abort(500);
         }
         return redirect (route('top'));
+    }
+
+    /**
+     * 投稿編集画面を表示。
+     * @param string $id
+     * @return view
+     */
+    public function showEdit ($id) {
+        $post = Post::find($id);
+
+        return view('posts.edit', ['post' => $post]);
+    }
+
+    /**
+     * 投稿編集内容をDBに保存。
+     * @param　 object $request
+     * @param string $id
+     * @return view
+     */
+    public function updatePost (PostRequest $request, $id) {
+        $inputs = $request->all();
+        $post = Post::find($id);
+        if (Auth::id() === $post->user->id) {
+            DB::BeginTransaction();
+            try {
+                $post->fill([
+                    'content' => $inputs['content'],
+                    'user_id' => Auth::id(),
+                ]);
+                $post->save();
+                DB::commit();
+            } catch (\Throwable $e) {
+                DB::rollBack();
+                abort(500);
+            }
+            return redirect(route('top'));
+        }
+        abort(404);
     }
 }
