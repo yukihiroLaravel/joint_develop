70d091e77ccfc18ef05f440c4de9e6a14d2bddc5
jyunpei fukano <fukanojyunpei@gmail.com>
2023-08-29 06:54:26 +0000

    新ユーザー詳細

---
+++ resources/views/users/users.blade.php
@@ -0,0 +1,5 @@
+<div class="col-lg-4 mb-5">
+                    </div>
+                <div class="movie text-left d-inline-block">
+                    <a href="{{ route('user.show', $user->id) }}">＠{{ $user->name }}</a>
+                    </div> 
--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,10 @@ Route::group(['middleware' => 'auth'], function ()
     Route::prefix('users')->group(function () {
         Route::get('{id}/edit', 'UsersController@edit')->name('user.edit');
     });
-});
+
         Route::put('{id}', 'UsersController@update')->name('users.update');
+    });
+
     //投稿新規作成
     Route::prefix('posts')->group(function () {
         Route::post('', 'PostsController@store')->name('post.store');
