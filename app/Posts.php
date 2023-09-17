8d7b56019a46169f49ad022e26f3d09e424a405a
Kyosuke Horigome <horigome@outlook.jp>
2023-09-17 21:14:44 +0900

    #685 posts（投稿）テーブルのマイグレーションとシーダー作成

--- app/Posts.php
+++ app/Posts.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\softDeletes;
 class Posts extends Model
 {
     use softDeletes;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)
+    }
+
 }
