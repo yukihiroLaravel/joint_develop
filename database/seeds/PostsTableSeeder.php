3c796063503d8bbde18ded1ecea81fcf54ee78b3
Kyosuke Horigome <horigome@outlook.jp>
2023-09-18 01:03:18 +0900

    #685 posts（投稿）テーブルのマイグレーションとシーダー作成

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -13,57 +13,57 @@ class PostsTableSeeder extends Seeder
     {
         DB::table('posts')->insert([
             'user_id' => 1,
-            'content,140' => 'テスト1',
+            'content' => 'テスト1',
 
         ]);
         DB::table('posts')->insert([
             'user_id' => 2,
-            'content,140' => 'テスト2',
+            'content' => 'テスト2',
             
         ]);
         DB::table('posts')->insert([
             'user_id' => 3,
-            'content,140' => 'テスト3',
+            'content' => 'テスト3',
             
         ]);
         DB::table('posts')->insert([
             'user_id' => 4,
-            'content,140' => 'テスト4',
+            'content' => 'テスト4',
             
         ]);
         DB::table('posts')->insert([
             'user_id' => 5,
-            'content,140' => 'テスト5',
+            'content' => 'テスト5',
             
         ]);
         DB::table('posts')->insert([
             'user_id' => 6,
-            'content,140' => 'テスト6',
+            'content' => 'テスト6',
             
         ]);
         DB::table('posts')->insert([
             'user_id' => 7,
-            'content,140' => 'テスト7',
+            'content' => 'テスト7',
             
         ]);
         DB::table('posts')->insert([
             'user_id' => 8,
-            'content,140' => 'テスト8',
+            'content' => 'テスト8',
             
         ]);
         DB::table('posts')->insert([
             'user_id' => 9,
-            'content,140' => 'テスト9',
+            'content' => 'テスト9',
             
         ]);
         DB::table('posts')->insert([
             'user_id' => 10,
-            'content,140' => 'テスト10',
+            'content' => 'テスト10',
             
         ]);
         DB::table('posts')->insert([
             'user_id' => 11,
-            'content,140' => 'テスト11',
+            'content' => 'テスト11',
             
         ]);
     }
