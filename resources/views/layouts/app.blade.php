a5498aba7d0fb107597275279480b75922b91550
rahmat ramdani <rahmat.ramdani85@gmail.com>
2023-09-18 12:15:36 +0900

    top-page_fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,7 @@
     <body>
         @include('commons.header')
         <div class="container">
-            @include('commons.error_messages')
-            @yield('content')
+              @yield('content')
         </div>
         @include('commons.footer')
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,4 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'UsersController@index');
+Route::get('/', 'PostsController@index');
