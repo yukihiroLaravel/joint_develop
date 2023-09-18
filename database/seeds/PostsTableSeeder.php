4f376f8e3b8a5ef3f0f8dd9da82e09d713351178
Kyosuke Horigome <horigome@outlook.jp>
2023-09-18 15:10:49 +0900

    #685 posts（投稿）テーブルのマイグレーションとシーダー作成

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -14,57 +14,46 @@ class PostsTableSeeder extends Seeder
         DB::table('posts')->insert([
             'user_id' => 1,
             'content' => 'テスト1',
-
         ]);
         DB::table('posts')->insert([
             'user_id' => 2,
-            'content' => 'テスト2',
-            
+            'content' => 'テスト2',     
         ]);
         DB::table('posts')->insert([
             'user_id' => 3,
-            'content' => 'テスト3',
-            
+            'content' => 'テスト3',          
         ]);
         DB::table('posts')->insert([
-            'user_id' => 4,
-            'content' => 'テスト4',
-            
+            'user_id' => 1,
+            'content' => 'テスト4',    
         ]);
         DB::table('posts')->insert([
-            'user_id' => 5,
-            'content' => 'テスト5',
-            
+            'user_id' => 2,
+            'content' => 'テスト5',           
         ]);
         DB::table('posts')->insert([
-            'user_id' => 6,
-            'content' => 'テスト6',
-            
+            'user_id' => 3,
+            'content' => 'テスト6',    
         ]);
         DB::table('posts')->insert([
-            'user_id' => 7,
-            'content' => 'テスト7',
-            
+            'user_id' => 1,
+            'content' => 'テスト7',   
         ]);
         DB::table('posts')->insert([
-            'user_id' => 8,
-            'content' => 'テスト8',
-            
+            'user_id' => 2,
+            'content' => 'テスト8',  
         ]);
         DB::table('posts')->insert([
-            'user_id' => 9,
-            'content' => 'テスト9',
-            
+            'user_id' => 3,
+            'content' => 'テスト9',   
         ]);
         DB::table('posts')->insert([
-            'user_id' => 10,
-            'content' => 'テスト10',
-            
+            'user_id' => 1,
+            'content' => 'テスト10', 
         ]);
         DB::table('posts')->insert([
-            'user_id' => 11,
+            'user_id' => 2,
             'content' => 'テスト11',
-            
         ]);
     }
 }
