fe285d060484a3d1887cbf7e42ecbafa2dde8e32
jun onodera <o8jmsch@outlook.com>
2023-09-21 02:21:21 +0900

    #686 users（ユーザ）テーブルのマイグレーションとシーダー作成

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($num = 1; $num < 6; $num++){
+            DB::table('users')->insert([
+                'name' => 'test'.$num,
+                'email' => 'test'.$num.'@test.jp',
+                'password' => bcrypt('test'.$num.'1pass')
+            ]);
+        }
+    }
+}
