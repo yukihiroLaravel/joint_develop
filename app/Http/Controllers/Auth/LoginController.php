49de2baf63d8b1237442a0c92c358d67f9a06d98
jun onodera <o8jmsch@outlook.com>
2023-09-26 02:43:16 +0900

    #682_(再修正)ユーザログイン・ログアウト

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';    //ログイン後はトップページへ遷移するように変更
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
