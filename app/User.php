7e3e656bac87cdfa827f90d83cc568c8e9554ca2
Kyosuke Horigome <horigome@outlook.jp>
2023-09-29 23:36:59 +0900

    変更

--- app/User.php
+++ app/User.php
@@ -40,13 +40,7 @@ class User extends Authenticatable
     ];
 
     public function posts()
-<<<<<<< HEAD
-     {
-         return $this->hasMany(Post::class);
-     }
-=======
     {
         return $this->hasMany(Post::class);
     }
->>>>>>> develop_b_nagatsuki_rab
 }
--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//トップページ（投稿一覧表示）
-Route::get('/', 'PostsController@index');
-
-<<<<<<< HEAD
-//ユーザー詳細
-Route::prefix('users')->group(function () {
-    Route::get('{id}', 'UsersController@show')->name('user.show');
-});
-=======
-// ユーザ新規登録
+//ユーザ新規登録
 Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup','Auth\RegisterController@register')->name('signup.post');
->>>>>>> develop_b_nagatsuki_rab
 
 //ログイン・ログアウト
 Route::get('login','Auth\LoginController@showLoginForm')->name('login');
 Route::post('login','Auth\LoginController@login')->name('login.post');
 Route::get('logout','Auth\LoginController@logout')->name('logout');
+
+//トップページ（投稿一覧表示）
+Route::get('/', 'PostsController@index');
+
+//ユーザー詳細
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
