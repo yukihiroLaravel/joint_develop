ad2452e6cb56bcf765e1ba0a0f4f7b5b3b39c522
daihubemmono <ryotu_t@yahoo.co.jp>
2023-09-29 17:01:28 +0900

    posts(投稿)テーブルのマイグレーションとシーダー作成

--- database/migrations/2023_09_20_213117_create_posts_table.php
+++ database/migrations/2023_09_20_213117_create_posts_table.php
@@ -15,9 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
             $table->string('content')->nullable(false);
             $table->bigInteger('user_id')->unsigned()->index();
+            $table->timestamps();
             $table->softDeletes();
             // 外部キー制約
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,12 +12,44 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         DB::table('posts')->insert([
-            'content' => 'abc',
+            'content' => '投稿の本文です。',
             'user_id' => 1
         ]);
         DB::table('posts')->insert([
-            'content' => 'def',
+            'content' => '投稿の本文です。',
             'user_id' => 2
+        ]);
+        DB::table('posts')->insert([
+            'content' => '投稿の本文です。',
+            'user_id' => 3
+        ]);
+        DB::table('posts')->insert([
+            'content' => '投稿の本文です。',
+            'user_id' => 4
+        ]);
+        DB::table('posts')->insert([
+            'content' => '投稿の本文です。',
+            'user_id' => 5
+        ]);
+        DB::table('posts')->insert([
+            'content' => '投稿の本文です。',
+            'user_id' => 6
+        ]);
+        DB::table('posts')->insert([
+            'content' => '投稿の本文です。',
+            'user_id' => 7
+        ]);
+        DB::table('posts')->insert([
+            'content' => '投稿の本文です。',
+            'user_id' => 8
+        ]);
+        DB::table('posts')->insert([
+            'content' => '投稿の本文です。',
+            'user_id' => 9
+        ]);
+        DB::table('posts')->insert([
+            'content' => '投稿の本文です。',
+            'user_id' => 10
         ]);
     }
 }
