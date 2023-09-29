fb93be795f3e03e78d0ff77bd210ca0dad5a03e5
YasuLaravel <mina98to@gmail.com>
2023-09-30 07:59:03 +0900

    posts（投稿）テーブルのマイグレーションとシーダー作成

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -27,38 +27,7 @@ class PostsTableSeeder extends Seeder
             'user_id' => 4,
             'content' => 'テスト4',
         ]);
-        DB::table('posts')->insert([
-            'user_id' => 5,
-            'content' => 'テスト5',
-        ]);
-        DB::table('posts')->insert([
-            'user_id' => 6,
-            'content' => 'テスト6',
-        ]);
-        DB::table('posts')->insert([
-            'user_id' => 7,
-            'content' => 'テスト7',
-        ]);
-        DB::table('posts')->insert([
-            'user_id' => 8,
-            'content' => 'テスト8',
-        ]);
-        DB::table('posts')->insert([
-            'user_id' => 9,
-            'content' => 'テスト9',
-        ]);
-        DB::table('posts')->insert([
-            'user_id' => 10,
-            'content' => 'テスト10',
-        ]);
-        DB::table('posts')->insert([
-            'user_id' => 11,
-            'content' => 'テスト11',
-        ]);
-        DB::table('posts')->insert([
-            'user_id' => 12,
-            'content' => 'テスト12',
-        ]);
+
 
  }
 }
