244d9f5188d25ad8556ea8d1eee895ceac5d58c5
Kyosuke Horigome <horigome@outlook.jp>
2023-10-04 02:45:36 +0900

    投稿編集画面•更新

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Http\Requests\PostRequest;
 use App\User;
 
 use Illuminate\Http\Request;
@@ -13,5 +14,28 @@ class PostsController extends Controller
     {
         $posts = Post::orderBy('id','desc')->paginate(10);
         return view('welcome', ['posts' => $posts,]);
+    }
+
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        if (\Auth::id() === $post->user_id) {
+        $data=[
+            'user' => $user,
+            'post' => $post,
+        ];
+        return view('posts.edit', $data);
+        } else {
+            return back();
+        }
+    }
+
+    public function update(PostRequest $request, $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->text = $request->text;
+        $post->user_id = $request->user()->id;
+        $post->save();
+        return back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+//トップページ（投稿一覧表示）
+Route::get('/', 'PostsController@index');
+
 //ユーザ新規登録
 Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup','Auth\RegisterController@register')->name('signup.post');
@@ -19,10 +23,13 @@ Route::get('login','Auth\LoginController@showLoginForm')->name('login');
 Route::post('login','Auth\LoginController@login')->name('login.post');
 Route::get('logout','Auth\LoginController@logout')->name('logout');
 
-//トップページ（投稿一覧表示）
-Route::get('/', 'PostsController@index');
-
 //ユーザー詳細
 Route::prefix('users')->group(function () {
     Route::get('{id}', 'UsersController@show')->name('user.show');
+});
+
+// ログイン後
+Route::group(['middleware' => 'auth'], function () {
+Route::get('{id}/edit','PostsController@edit')->name('post.edit');
+Route::put('{id}', 'PostsController@update')->name('post.update');
 });
