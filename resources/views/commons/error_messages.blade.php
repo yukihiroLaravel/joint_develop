596f9cfc7e32f26b22e3fc5a5bd4eacc800e43ad
kouji hirose <yukihiro@example.com>
2023-10-05 17:00:08 +0900

    #668 共通ファイル&ヘッダーフッター & トップページのタイトルのみ表示「Topic Posts」

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -5,8 +5,3 @@
         @endforeach
     </ul>
 @endif 
-
-<ul class="alert alert-danger" role="alert">
-            <li class="ml-4">エラーメッセージが入る場所</li>
-    </ul>
-
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,8 @@
         </div>
     </div>
     <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
-        <div class="w-75 m-auto">@include('commons.error_messages')</div> 
+        <div class="w-75 m-auto">
+            @include('commons.error_messages')</div> 
         <div class="text-center mb-3">
             <form method="" action="" class="d-inline-block w-75">
                 <div class="form-group">
