92e6c2be07d5660f0b747f39cd2ee367717d4ae2
yukihiro seki <yukihiro@example.com>
2023-10-12 10:09:00 +0900

    #699 ユーザ新規登録

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -1,20 +1,26 @@
 <header class="mb-5">
-    <nav class="navbar navbar-expand-sm navbar-dark bg-info">
-        <a class="navbar-brand" href="/">Topic Posts</a>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">YouTubeまとめ<br>&ensp;×コミュニケーション</a>
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                @if (Auth::check())
-                    <li class="nav-item"><a href="" class="nav-link">{{ Auth::user()->name }}</a></li>
+            @if (Auth::check())
+                    <li class="nav-item"><a href="{{ route('movie.create') }}" class="nav-link">動画登録する</a></li>
+                    <li class="nav-item"><a href="{{ route('user.show', Auth::id()) }}" class="nav-link">マイページ</a></li>
                     <li class="nav-item"><a href="{{ route('logout') }}" class="nav-link">ログアウト</a></li>
                 @else
                     <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">ログイン</a></li>
-                    <li class="nav-item"><a href="" class="nav-link">新規ユーザ登録</a></li>
+                    <li class="nav-item"><a href="{{ route('signup') }}" class="nav-link">新規ユーザ登録</a></li>
                 @endif
-            </ul>        
+            </ul>
         </div>
     </nav>
 </header>
+@if(Auth::check())
+    <p class="text-right mr-3 pb-3">
+        ユーザー：<span class="user-name">{{ Auth::user()->name }}</span>
+    </p>
+@endif
