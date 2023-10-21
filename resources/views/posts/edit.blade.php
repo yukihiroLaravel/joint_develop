41a9a04c4f64798473c21d3d1c469f858efd7d18
kazufumi katsuyama <kattsunt970138@gmail.com>
2023-10-21 16:48:35 +0900

    #707 投稿編集画面・更新2(かずふみ)

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 @section('content') 
 <h2 class="mt-5">投稿を編集する</h2>
-    <form method="POST" action="{{ route('post.update', $post->id) }}">
-        @csrf
-        @method('PUT')
-        <div class="form-group">
-            <textarea id="content" class="form-control" name="content" rows="5">{{ old('content', $post->content) }}</textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">更新する</button>
-    </form> 
+<div class="m-auto">@include('commons.error_messages')</div>
+    <div class="test-center mb-3">
+        <form method="POST" action="{{ route('post.update', $post->id) }}" class="d-inline-block w-75">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <textarea id="content" class="form-control" name="content" rows="5">{{ old('content', $post->content) }}</textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">更新する</button>
+        </form> 
 @endsection
