9c179d69f3e506623dd289875205f79b1c360461
kazufumi katsuyama <kattsunt970138@gmail.com>
2023-10-22 21:02:19 +0900

    #707 投稿編集画面・更新3(かずふみ)

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,11 +3,11 @@
 <h2 class="mt-5">投稿を編集する</h2>
 <div class="m-auto">@include('commons.error_messages')</div>
 <div class="test-center mb-3">
-    <form method="POST" action="{{ route('post.update', $post->id) }}" class="d-inline-block w-75">
+    <form method="POST" action="{{ route('post.update', $post->id) }}" class="d-inline-block w-100">
         @csrf
         @method('PUT')
         <div class="form-group">
-            <textarea id="content" class="form-control" name="content" rows="5">{{ old('content', $post->content) }}</textarea>
+            <textarea id="content" class="form-control" name="content" rows="4">{{ old('content', $post->content) }}</textarea>
         </div>
         <button type="submit" class="btn btn-primary">更新する</button>
     </form>
