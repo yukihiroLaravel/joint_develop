52834342b80c2f833020d18e1b2c6225d31cb913
kouji hirose <yukihiro@example.com>
2023-10-28 00:42:15 +0900

    #664 トップページの投稿表示

--- app/User.php
+++ app/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function posts()
+    public function post()
     {
         return $this->hasMany(Post::class);
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,10 @@
 @section('content')
 <div class="center jumbotron bg-info">
         <div class="text-center text-white mt-2 pt-1">
-            <h1><i class="pr-3"></i>Topic Posts</h1>
+            <h1><i class="fab fa-telegram fa-lg pr-3"></i>Topic Posts</h1>
         </div>
     </div>
     <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
-        <div class="w-75 m-auto">@include('commons.error_messages')</div> 
-        <div class="text-center mb-3">
-            <form method="" action="" class="d-inline-block w-75">
-                <div class="form-group">
-                    <textarea class="form-control" name="" rows=""></textarea>
-                    <div class="text-left mt-3">
-                        <button type="submit" class="btn btn-primary">投稿する</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-
+        @include('post.posts',['posts' => $posts])
 @endsection
  
