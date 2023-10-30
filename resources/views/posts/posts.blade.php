a5c9f795714f2fe7a565affcff65086521e4f989
kouji hirose <yukihiro@example.com>
2023-10-30 20:40:48 +0900

    #664 トップページの投稿表示

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -4,16 +4,14 @@
                 <div class="text-left d-inline-block w-75 mb-2">
                     <img class="mr-2 rounded-circle" src="{{ Gravatar::src( $post->user->email , 55) }}" alt="ユーザのアバター画像">
                     <p class="mt-3 mb-0 d-inline-block"><a href="">{{ $post->user->name }}</a></p>
-                    </div>
-                    <div class="">
-                        <div class="text-left d-inline-block w-75">
-                            <p class="mb-2">{{ $post->content }}</p>
-                            <p class="text-muted">{{ $post->created_at }}</p>
-                        </div>
-                    </div>
+                </div>
+                <div class="text-left d-inline-block w-75">
+                    <p class="mb-2">{{ $post->content }}</p>
+                    <p class="text-muted">{{ $post->created_at }}</p>
+                </div>
                 @if (\Auth::id() === $post->user_id)
                   <div class="d-flex justify-content-between w-75 pb-3 m-auto">
-                     <form method="POST" action="">
+                     <form method="" action="">
                          <button type="submit" class="btn btn-danger">削除</button>
                      </form>
                      <a href="{{ route('users.edit', $post->user_id) }}" class="btn btn-primary">編集する</a>
