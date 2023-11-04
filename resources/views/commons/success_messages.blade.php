510d14be924652a289588689406794597ca83b61
kazufumi katsuyama <kattsunt970138@gmail.com>
2023-11-04 11:48:46 +0900

    #165 投稿のフラッシュメッセージ修正(かずふみ)

--- resources/views/commons/success_messages.blade.php
+++ resources/views/commons/success_messages.blade.php
@@ -1,19 +1,17 @@
 <!-- 投稿に成功　-->
 @if (session('successMessage'))
-        <div class="alert alert-success w-75 m-auto">
-            {{ session('successMessage') }}
-        </div>
-@endif
+    <div class="alert alert-success w-75 m-auto">
+        {{ session('successMessage') }}
+    </div>
 
 <!-- 削除に成功　-->
-@if (session('destroyMessage'))
+    @elseif (session('destroyMessage'))
         <div class="alert alert-success w-75 m-auto">
             {{ session('destroyMessage') }}
         </div>
-@endif
 
 <!-- 編集に成功　-->
-@if (session('editMessage'))
+    @elseif (session('editMessage'))
         <div class="alert alert-success w-75 m-auto">
             {{ session('editMessage') }}
         </div>
