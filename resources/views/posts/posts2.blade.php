606b7ec330efe760ac8c4024cd2fadf1e0eb1fe3
yamada naoki <naoki19980422@gmail.com>
2023-11-05 17:33:24 +0900

    一時保存　新規ブランチ

--- resources/views/posts/posts2.blade.php
+++ resources/views/posts/posts2.blade.php
@@ -67,8 +67,10 @@
                 </div>
                 @if (\Auth::id() === $post->user_id)
                   <div class="d-flex justify-content-between w-75 pb-3 m-auto">
-                     <form method="" action="">
-                         <button type="submit" class="btn btn-danger">削除</button>
+                     <form method="POST" action="{{ route('post.delete', $post->id) }}">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="btn btn-danger">削除</button>
                      </form>
                      <a href="{{ route('users.edit', $post->user_id) }}" class="btn btn-primary">編集する</a>
                   </div>
