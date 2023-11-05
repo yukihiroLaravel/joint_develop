b4354694c3bffe5870edb5384d617f461fc79477
yamada naoki <naoki19980422@gmail.com>
2023-11-05 09:30:06 +0900

    一時保存

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,35 @@
 @extends('layouts.app')
 @section('content')
-<div class="center jumbotron bg-info">
+<!-- <div class="center jumbotron bg-info">
     <div class="text-center text-white mt-2 pt-1">
         <h1><i class="pr-3 "></i>Topic Posts</h1><i class="fa-solid fa-paper-plane"></i>
     </div>
-</div>
+</div> -->
+
+<h4 class="text-center">地域の選択</h4>
+    <div class="map_sea">
+    <div class="map-container">
+        <a href="九州のリンク" class="region-box" style="top: 50%; left: 20%; height: 140px">
+            九州
+        </a>
+        <a href="四国・中国のリンク" class="region-box" style="top: 40%; left: 28%; width:150px">
+            中国・四国
+        </a>
+        <a href="四国・中国のリンク" class="region-box" style="top: 52%; left: 31%; width:100px; height:75px">
+            
+        </a>
+        <a href="近畿・東海のリンク" class="region-box" style="top: 40%; left: 42%; height: 160px">
+            近畿・東海
+        </a>
+        <a href="東北・関東のリンク" class="region-box l-box" style="top: 20%; left: 70%;">
+            東北・関東
+        </a>
+        <a href="北海道のリンク" class="region-box" style="top: 10%; left: 90%;">
+            北海道
+        </a>
+    </div>
+    </div>
+
     <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
     @if (Auth::check())
         <div class="w-75 m-auto">
