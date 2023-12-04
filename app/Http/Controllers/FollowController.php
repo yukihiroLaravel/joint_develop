0a32b38b8916fdba2369e3d9144d7dd039558a68
tatsuhiro hayashi <t.hayashi4218538@gmail.com>
2023-12-04 17:34:24 +0900

    フォロー機能途中

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class FollowController extends Controller
 {
-    //
+    public function store($id)
+    {
+        \Auth::user()->follow($id);
+        return back();
+    }
+    public function destroy($id)
+    {
+        \Auth::user()->unfollow($id);
+        return back();
+    }
 }
