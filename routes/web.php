1d123ecc8a48871a2f0bcd5af0e2193dd2ecc2da
tatsuhiro hayashi <t.hayashi4218538@gmail.com>
2023-12-08 22:50:36 +0900

    ユーザー詳細

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,16 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
+//ユーザー詳細
+Route::prefix('users')->group(function() {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
+
 // ユーザ編集(ログインユーザのみ)＆ユーザー退会
 // ログイン後
 Route::group(['middleware' => 'auth'], function(){
     // ユーザ
     Route::prefix('users')->group(function() {
-        Route::get('{id}', 'UsersController@show')->name('user.show');
         Route::get('{id}/edit', 'UsersController@edit')->name('user.edit');
         Route::put('{id}', 'UsersController@update')->name('user.update');
         Route::delete('{id}', 'UsersController@destroy')->name('user.delete');
