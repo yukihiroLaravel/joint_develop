67af5e7d15ac7ff594c02400f18b62328c500809
tatsuhiro hayashi <t.hayashi4218538@gmail.com>
2024-01-12 22:57:58 +0900

    更新時の画像の設定

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,7 +5,6 @@ use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\PostRequest;
-use Illuminate\Support\Facades\Storage;
 
 
 class PostsController extends Controller
