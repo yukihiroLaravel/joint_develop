c2f4b77075ff6e26e52cb6bc474ac9830c88fa20
adash333(ニックネームなど) <xxxxx@xxx.com>
2024-01-29 14:38:49 +0000

    4章 動画#897 posts（投稿）テーブルのマイグレーションとシーダー作成

---
+++ database/migrations/2024_01_29_095154_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('text'); //追記
+            $table->bigInteger('user_id')->unsigned()->index(); //追記
+            $table->timestamps(); //追記
+            $table->softDeletes(); //追記
+           
+            // 外部キー制約
+            //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); //追記
+            
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+           $this->call(PostsTableSeeder::class); //追記
+           $this->call(UsersTableSeeder::class); //追記
     }
 }
