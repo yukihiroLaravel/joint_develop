ca748a7b0580f5e38e357659f3c1ef3d6f81b9aa
abeai0 <115936489+abeai0@users.noreply.github.com>
2024-01-29 22:56:15 +0900

    ”ユーザー新規登録機能”

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ユーザー新規登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
