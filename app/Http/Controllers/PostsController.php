7e741d7f97a97b6216e6e0daa0928dcb193b17c6
toshiyuki ohtomo <otoshi2007@gmail.com>
2024-02-03 06:21:59 +0000

    #896_Create_common_file_header_footer_and_top_page_title

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class PostsController extends Controller
 {
     public function index()
     {
