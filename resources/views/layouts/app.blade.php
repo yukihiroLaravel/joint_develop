e08803800c41b227e2fc7851f4cced61e2fa2270
osmnishida <osmnishida@gmail.com>
2024-02-04 19:27:32 +0900

    #911 ユーザ新規登録再修正２

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,7 @@
     <body>
         @include('commons.header')
         <div class="container">
-            <div class="col-sm-6 offset-sm-3">
-                @yield('content')
-            </div>
+            @yield('content')
         </div>
         @include('commons.footer')
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
