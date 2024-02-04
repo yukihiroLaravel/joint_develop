849b4f19a312bdbe947b6a991f1efe183faec003
kenta oohashi <kerutomusic@gmail.com>
2024-02-04 17:46:48 +0900

    #895ユーザ新規登録

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,9 @@
 |
 */
 
+//ユーザー新規登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+
 Route::get('/', 'PostsController@index');
