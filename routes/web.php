2a0f74663dc668ff109cd6610107f25b063f86e5
Natsumi Kawahara <natsumi.kawahara@epkotsoftware.co.jp>
2024-02-05 21:44:16 +0900

    コンフリクトの解消2(川 原)

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@
 // ユーザ新規登録
 Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup','Auth\RegisterController@register')->name('signup.post');
-Route::get('/', 'UsersController@index');
 
 Route::get('/', 'UsersController@index');
 // ログイン
