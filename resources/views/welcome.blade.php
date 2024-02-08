f856cb9ea200892bae945bbf9c369edca2768d67
Celadon67 <se06.program@gmail.com>
2024-02-08 21:28:03 +0900

    #892 トップページの投稿表示（小林）（修正3版）

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,22 +7,21 @@
     </div>
     <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
     <div class="container">
-@include('commons.error_messages')
-    <div class="text-center mb-3">
-        <form method="" action="" class="d-inline-block w-75">
-            <div class="form-group">
-                <textarea class="form-control" name="" rows=""></textarea>
-                <div class="text-left mt-3">
-                    <button type="submit" class="btn btn-primary">投稿する</button>
-                </div>
-            </div>
-        </form>
-
-        {{-- ログインしている場合にのみ表示 --}}
-    @if(Auth::check())
-        @include('users.users', ['users' => $users])
-    @endif
-
-        @include('posts.posts', ['posts' => $posts])
+        @include('commons.error_messages')
+        <div class="text-center mb-3">
+            {{-- ユーザーがログインしている場合にのみフォームを表示 --}}
+            @auth
+                <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
+                    @csrf
+                    <div class="form-group">
+                        <textarea class="form-control" name="content" rows="3"></textarea>
+                        <div class="text-left mt-3">
+                            <button type="submit" class="btn btn-primary">投稿する</button>
+                        </div>
+                    </div>
+                </form>
+            @endauth
+            @include('posts.posts', ['posts' => $posts])
+        </div>
     </div>
 @endsection
