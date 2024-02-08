079042293cecf8bb66e845488d63ebde6503dda2
hiroshi inoue <samurai49green@icioud.com>
2024-02-08 21:56:36 +0900

    インデント修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,15 @@
             </div>
     </div>
         <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
-            <div class="w-75 m-auto">@include('commons.error_messages'){{--エラーメッセージが入る場所--}}</div>
-            <div class="text-center mb-3">
-                <form method="" action="" class="d-inline-block w-75">
-                    <div class="form-group">
-                        <textarea class="form-control" name="" rows=""></textarea>
-                        <div class="text-left mt-3">
-                            <button type="submit" class="btn btn-primary">投稿する</button>
-                        </div>
-                    </div>
-                </form>
+    <div class="w-75 m-auto">@include('commons.error_messages'){{--エラーメッセージが入る場所--}}</div>
+    <div class="text-center mb-3">
+        <form method="" action="" class="d-inline-block w-75">
+            <div class="form-group">
+                <textarea class="form-control" name="" rows=""></textarea>
+                <div class="text-left mt-3">
+                    <button type="submit" class="btn btn-primary">投稿する</button>
+                </div>
             </div>
+        </form>
+    </div>
 @endsection
