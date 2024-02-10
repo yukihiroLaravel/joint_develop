f39224b2007996d380d3962766a9c39a173ab7ee
abeai0 <115936489+abeai0@users.noreply.github.com>
2024-02-10 15:31:32 +0900

    微修正

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 // ログイン後
 Route::group(['middleware' => 'auth'], function () {
   Route::prefix('user')->group(function () {
-    Route::get('edit', 'UserController@edit')->name('user_edit');
-    Route::put('edit', 'UserController@update')->name('user_update');
+    Route::get('edit', 'UserController@edit')->name('user.edit');
+    Route::put('edit', 'UserController@update')->name('user.update');
   });
 });
