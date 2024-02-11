5a6e8d61f0b15cae3ff57815b6f18be1a4db7e9b
osmnishida <osmnishida@gmail.com>
2024-02-11 14:14:54 +0900

    新規投稿

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Post;
+use App\Http\Requests\PostRequest;
 
 class PostsController extends Controller
 {
-    public function create()
+    public function store(PostRequest $request)
     {
-        
+        $post = new Post;
+        $post->content = $request->content;
+        $post->user_id = $request->user()->id;
+        $post->save();
+        return back();
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,10 @@
     <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
         <div class="w-75 m-auto">エラーメッセージが入る場所</div>
         <div class="text-center mb-3">
-            <form method="" action="" class="d-inline-block w-75">
+            <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
+                @csrf
                 <div class="form-group">
-                    <textarea class="form-control" name="" rows=""></textarea>
+                    <textarea class="form-control" name="content" rows=""></textarea>
                     <div class="text-left mt-3">
                         <button type="submit" class="btn btn-primary">投稿する</button>
                     </div>
