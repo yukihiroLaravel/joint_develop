de5c20f728acc4e5ff3cc211226fcf3b031575aa
kenta oohashi <kerutomusic@gmail.com>
2024-02-12 22:03:54 +0900

    #895ユーザ新規登録

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,5 +1,5 @@
 @if (count($errors) > 0)
-    <ul class="alert alert-danger" role="alert" style="max-width: 540px; margin: 0 auto; text-align: left;">
+    <ul class="alert alert-danger" role="alert">
         @foreach ($errors->all() as $error)
             <li class="ml-4">{{ $error }}</li>
         @endforeach
