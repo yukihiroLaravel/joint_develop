5c3ccd5438b6ef052bcfecc309607250c0391138
kenta oohashi <kerutomusic@gmail.com>
2024-02-16 17:39:14 +0900

    #891 ユーザ詳細

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,17 @@
 |
 */
 
+//ユーザー新規登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 // ログイン
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-//ユーザー新規登録
-Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
-Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+//ユーザ詳細
+Route::get('users/{id}', 'UsersController@show')->name('user.show');
 
 // トップページ表示
 Route::get('/', 'PostsController@index'); //追記
