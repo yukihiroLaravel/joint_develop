137b4fa7d47dbc17964e1cbd2242591443aaea84
koudai onodera <iamlucky6406@gmail.com>
2024-02-16 19:32:46 +0900

    ユーザー詳細途中

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <div class="text-center mb-3">
         <form method="" action="" class="d-inline-block w-75">
             <div class="form-group">
-                <textarea class="form-control" name="" rows=""></textarea>
+                <textarea class="form-control" name="content" rows="4"></textarea>
                 <div class="text-left mt-3">
                     <button type="submit" class="btn btn-primary">投稿する</button>
                 </div>
