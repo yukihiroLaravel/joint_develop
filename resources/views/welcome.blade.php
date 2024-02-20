d0a6c5002a2904ba8a9fad70251b270023c0443a
abeai0 <115936489+abeai0@users.noreply.github.com>
2024-02-20 23:24:16 +0900

    トップページの@endifの位置と</dev>が多かったので修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,9 +17,8 @@
                         <button type="submit" class="btn btn-primary">投稿する</button>
                     </div>
                 </div>
+            </form>
         </div>
-        </form>
-        </div>
-        @include('posts.posts', ['posts' => $posts])
-    @endsection
-@endif
+    @endif
+    @include('posts.posts', ['posts' => $posts])
+@endsection
