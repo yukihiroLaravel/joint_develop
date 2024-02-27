f52412b2c8e2f320cf72ab4799d55f4705db321d
IDE Daisuke <win10yamaha@outlook.jp>
2024-02-27 15:45:49 +0900

    投稿削除機能 修正

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -39,14 +39,4 @@ class UsersController extends Controller
         $user->save();
         return redirect()->route('users.edit', ['id' => $user->id]);
     }
-
-    public function destroy($id)
-    {
-        if ($id === Auth::id()) {
-            $user = User::findOrFail($id);
-            $user->delete();
-            return redirect('/');
-        }
-        abort(404);
-    }
 }
