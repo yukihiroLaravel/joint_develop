36ea1f81913fe08c0e5ddc271e09fb11a383a6a6
Celadon67 <se06.program@gmail.com>
2024-03-03 17:13:16 +0900

    #885 フォロー機能（第十七稿）

--- resources/views/follow/follows.blade.php
+++ resources/views/follow/follows.blade.php
@@ -9,7 +9,7 @@
              <li class="mb-3 text-center">
                  <div class="text-left d-inline-block w-75 mb-2">
                      <img class="mr-2 rounded-circle" src="{{ Gravatar::src ($follow->email, 55) }}" alt="ユーザのアバター画像">
-                     <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('users.show', $follow ->id) }}">{{ $follow ->name }}</a></p>
+                     <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('user.show', $follow ->id) }}">{{ $follow ->name }}</a></p>
                  </div>
              </li>
          </ul>
