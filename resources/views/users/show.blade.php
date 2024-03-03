1a4150a3fb0c2e377d4595090d5d574da69b342f
Celadon67 <se06.program@gmail.com>
2024-03-03 20:39:08 +0900

    #885 フォロー機能（第十九稿）

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="row">
-   <aside class="col-sm-4 mb-5">
       @include('partials.user_profile', ['user' => $user])
-   </aside>
    <div class="col-sm-8">
       @include('partials.user_tabs', ['user' => $user])
       @include('posts.posts', ['user' => $user, 'posts' => $posts])
