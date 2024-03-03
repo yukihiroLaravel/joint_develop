7559b1f89047f1d358005d288c1dbb89f7bd9946
Celadon67 <se06.program@gmail.com>
2024-03-03 17:50:02 +0900

    #885 フォロー機能（第十八稿）

--- resources/views/partials/user_tabs.blade.php
+++ resources/views/partials/user_tabs.blade.php
@@ -1,11 +1,14 @@
 <ul class="nav nav-tabs nav-justified mb-3">
     <li class="nav-item">
-        <a href="{{ route('user.show', $user->id) }}" class="nav-link {{ Route::is('user.show') ? 'active' : '' }}">タイムライン<br><div class="badge badge-secondary">{{ isset($countPosts) }}</div></a>
+        <a href="{{ route('user.show', $user->id) }}" class="nav-link {{ Route::is('user.show') ? 'active' : '' }}">タイムライン<br>
+        </a>
     </li>
     <li class="nav-item">
-        <a href="{{ route('user.follows', $user->id) }}" class="nav-link {{ Route::is('user.follows') ? 'active' : '' }}">フォロー中<br><div class="badge badge-secondary">{{ isset($countFollows) }}</div></a>
+        <a href="{{ route('user.follows', $user->id) }}" class="nav-link {{ Route::is('user.follows') ? 'active' : '' }}">フォロー中<br>
+        </a>
     </li>
     <li class="nav-item">
-        <a href="{{ route('user.followers', $user->id) }}" class="nav-link {{ Route::is('user.followers') ? 'active' : '' }}">フォロワー<br><div class="badge badge-secondary">{{ isset($countFollowers) }}</div></a>
+        <a href="{{ route('user.followers', $user->id) }}" class="nav-link {{ Route::is('user.followers') ? 'active' : '' }}">フォロワー<br>
+        </a>
     </li>
 </ul>
