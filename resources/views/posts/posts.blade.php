69a3fedac68860dc1b14130edc5a71b8ba0a329e
abeai0 <115936489+abeai0@users.noreply.github.com>
2024-03-05 10:23:06 +0900

    posts.blade.phpの見た目を整えました

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -11,22 +11,24 @@
         @endif
     </h5>
 @endif
-<ul class="list-unstyled">
+<ul class="list-unstyled d-flex align-items-center flex-column show_list_style">
     @foreach ($posts as $post)
-        <li class="mb-3 text-center">
-            <div class="text-left d-inline-block w-75 mb-2">
+        <li class="col-11 col-sm-10 col-lg-8 pt-3 pb-3">
+            <div class="d-flex align-items-center justify-content-centor mb-2">
                 <img class="mr-2 rounded-circle" src="{{ Gravatar::src($post->user->email, 55) }}" alt="ユーザのアバター画像">
-                <p class="mt-3 mb-0 d-inline-block"><a
-                        href="{{ route('user.show', $post->user_id) }}">{{ $post->user->name }}</a>
+                <p><a href="{{ route('user.show', $post->user_id) }}">{{ $post->user->name }}</a>
                     @include('follows.follow_button', ['id' => $post->user_id])</p>
             </div>
             <div>
-                <div class="text-left d-inline-block w-75">
-                    <p class="mb-2">{!! nl2br(e($post->content)) !!}</p>
-                    <p class="text-muted">{{ $post->created_at }}</p>
+                <div class="text-left d-inline-block col-12 mb-2">
+                    <p class="mb-2 post_content">{!! nl2br(e($post->content)) !!}</p>
+                    <time class="text-muted">{{ $post->created_at }}</time>
+                </div>
+                <div class="pt-2 col-12 action_area">
+                    {{-- いいね・コメントのボタンが入ります --}}
                 </div>
                 @if ($post->user_id == Auth::id())
-                    <div class="d-flex justify-content-between w-75 pb-3 m-auto">
+                    <div class="d-flex justify-content-between pb-3 m-auto">
                         <form method="POST" action="{{ route('post.delete', $post->id) }}">
                             @csrf
                             @method('DELETE')
