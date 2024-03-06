4ac8dd777e8f550a80c3fa394bb2574521a7a1cf
abeai0 <115936489+abeai0@users.noreply.github.com>
2024-03-06 15:38:37 +0900

    サーチコントラローラー削除

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -18,7 +18,6 @@ class PostsController extends Controller
         ];
         $posts = Post::query();
         $users = User::query();
-
         if (!empty($request->searchWords)) {
             $searchWords = mb_convert_kana($request->searchWords, 's');
             $arraySearchWords = preg_split('/[\s,]+/', $searchWords, -1, PREG_SPLIT_NO_EMPTY);
@@ -31,7 +30,6 @@ class PostsController extends Controller
                 $users->orwhere('name', 'LIKE', '%' . $searchWord . '%');
             }
         }
-
         $posts = $posts->orderBy('id', 'desc')->paginate(10, ["*"], 'posts-page')->appends(["users-page" => $request->input('users-page')]);
         $users = $users->orderBy('id', 'desc')->paginate(10, ["*"], 'users-page')->appends(["posts-page" => $request->input('posts-page')]);
         $data += [
