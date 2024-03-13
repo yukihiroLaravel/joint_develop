9bde34c5abb36bad81f3b81c6c5013f37f1570c5
IDE Daisuke <win10yamaha@outlook.jp>
2024-03-13 17:49:13 +0900

    flashMessage コンフリクト修正

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+<div class="container">
     <div class="row">
         <aside class="col-sm-4 mb-5">
             <div class="card bg-info">
@@ -17,22 +18,71 @@
         <div class="col-sm-8">
             <ul class="nav nav-tabs nav-justified mb-3">
                 <li class="nav-item">
-                    <a href="{{ $user->id }}" class="nav-link {{ Request::is('users/'. $user->id) ? 'active' : '' }}"> タイムライン</a>
+                    <a href="{{ route('user.show', $user->id) }}" class="nav-link {{ Request::is('users/'. $user->id) ? 'active' : '' }}"> タイムライン</a>
                 </li>
-                <li class="nav-item"><a href="#" class="nav-link">フォロー中</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">フォロワー</a></li>
+                <li class="nav-item">
+                    <a href="{{ route('user.followingUsers', $user->id) }}" class="nav-link {{ Request::is('users/'. $user->id. '/followingUsers') ? 'active' : '' }}">フォロー</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('user.followers', $user->id) }}" class="nav-link {{ Request::is('users/'. $user->id. '/followers') ? 'active' : '' }}">フォロワー</a>
+                </li>
             </ul>
-            @if (session('deleteMessage'))
-                <div class="alert alert-warning text-center">
-                {{ session('deleteMessage') }}
-                </div> 
+
+            {{-- タイムラインが表示される場合 --}}
+            @if(Request::is('users/'. $user->id))
+                @if (session('deleteMessage'))
+                    <div class="alert alert-warning text-center">
+                    {{ session('deleteMessage') }}
+                    </div>
+                @endif
+                @if (session('updateSuccessMessage'))
+                    <div class="alert alert-primary text-center">
+                    {{ session('updateSuccessMessage') }}
+                    </div>
+                @endif
+                @include('posts', ['user' => $user])
             @endif
-            @if (session('updateSuccessMessage'))
-                <div class="alert alert-primary text-center">
-                {{ session('updateSuccessMessage') }}
-                </div> 
+
+            <!-- フォローが表示される場合 -->
+            @if(Request::is('users/'. $user->id. '/followingUsers'))
+            <ul class="list-unstyled">
+                @foreach ($users as $user)
+                <li class="mb-3 text-center">
+                    <div class="text-left d-inline-flex w-75 mb-2 align-items-center justify-content-between">
+                        <div>
+                            <img class="mr-2 rounded-circle" src="{{ Gravatar::src($user->email, 55) }}" alt="ユーザのアバター画像">
+                            <p class="mt-3 mb-0 d-inline-block"><a href="">{{ $user->name }}</a></p>
+                        </div>
+                        <!-- フォローボタン -->
+                        <div>
+                            @include('follow.follow', ['user' => $user])
+                        </div>
+                    </div>
+                </li>
+                @endforeach
+            </ul>
             @endif
-        @include('posts', ['$posts' => $posts])        
+
+            <!-- フォロワーが表示される場合 -->
+            @if(Request::is('users/'. $user->id. '/followers'))
+            <ul class="list-unstyled">
+                @foreach ($users as $user)
+                <li class="mb-3 text-center">
+                    <div class="text-left d-inline-flex w-75 mb-2 align-items-center justify-content-between">
+                        <div>
+                            <img class="mr-2 rounded-circle" src="{{ Gravatar::src($user->email, 55) }}" alt="ユーザのアバター画像">
+                            <p class="mt-3 mb-0 d-inline-block"><a href="">{{ $user->name }}</a></p>
+                        </div>
+                    <!-- フォローボタン -->
+                        <div>
+                            @include('follow.follow', ['user' => $user])
+                        </div>
+                    </div>
+                </li>
+                @endforeach
+            </ul>
+            @endif
         </div>
     </div>
+</div>
 @endsection
