f3af38f5d34b6114efcb5135c2368732c2adf3a6
kenta oohashi <kerutomusic@gmail.com>
2024-03-18 01:01:14 +0900

    コメント投稿機能

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -1,6 +1,9 @@
 <h5 class="mt-5">コメント一覧</h5>
 <ul>
     @foreach ($comments as $comment)
-        <li>{{ $comment->body }}</li> 
+        <li>
+            {{ $comment->body }}
+            <small class="text-muted">{{ $comment->created_at->diffForHumans() }}</small>
+        </li>
     @endforeach
 </ul>
