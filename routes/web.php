2a285520bfd6abb49518251e59eaeaad9aa5db9f
Sorato Ishi <sky.movie.1012@gmail.com>
2024-03-20 21:55:03 +0900

    画像投稿機能

--- routes/web.php
+++ routes/web.php
@@ -30,14 +30,14 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::prefix('users/{id}')->group(function () {
     Route::get('/', 'UsersController@show')->name('users.show');
     Route::group(['middleware' => 'auth'], function () {
-            Route::get('edit', 'UsersController@edit')->name('users.edit');
-            Route::put('/', 'UsersController@update')->name('users.update');
-            Route::delete('/', 'UsersController@destroy')->name('users.delete');
-            // フォロー機能
-            Route::post('follow', 'FollowController@store')->name('follow');
-            Route::delete('unFollow', 'FollowController@destroy')->name('unFollow');
-            Route::get('follows', 'FollowController@follows')->name('user.follows');
-            Route::get('followers', 'FollowController@followers')->name('user.followers');
+        Route::get('edit', 'UsersController@edit')->name('users.edit');
+        Route::put('/', 'UsersController@update')->name('users.update');
+        Route::delete('/', 'UsersController@destroy')->name('users.delete');
+        // フォロー機能
+        Route::post('follow', 'FollowController@store')->name('follow');
+        Route::delete('unFollow', 'FollowController@destroy')->name('unFollow');
+        Route::get('follows', 'FollowController@follows')->name('user.follows');
+        Route::get('followers', 'FollowController@followers')->name('user.followers');
     });
 });
 
