8542a7c40a5e07190274dbe20cf90e05d5ac2921
root <root@myassistant.localdomain>
2024-04-14 17:37:57 +0900

    共通ファイル・ヘッダー・フッター & トップページのタイトル表示「Topic Posts」

---
+++ resources/views/commons/error_messages.blade.php
@@ -0,0 +1,7 @@
+@if (count($errors) > 0)
+    <ul class="alert alert-danger" role="alert">
+        @foreach ($errors->all() as $error)
+            <li class="ml-4">{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
