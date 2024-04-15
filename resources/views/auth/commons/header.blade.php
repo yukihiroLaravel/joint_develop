6c3b4dca24fbb583207675fbc53e37a3319d652d
manabuandy <andybell1022@gmail.com>
2024-04-15 22:47:05 +0900

    header内容記述済

---
+++ resources/views/auth/commons/header.blade.php
@@ -0,0 +1,4 @@
+            <ul class="navber-nav">
+                 <li  class="nav-item"><a href="{{ route('signup') }}" class="nav-link">新規ユーザ登録</a></li>
+                 <li class="nav-item"><a href="" class="nav-link">ログイン</a></li>
+            </ul>
