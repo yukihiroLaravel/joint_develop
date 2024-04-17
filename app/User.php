543e48492c032e55742971d91f27971c183413f8
mizuki ito <popobi1102@yahoo.com>
2024-04-17 22:11:01 +0900

    usertable add migration and seeder

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
