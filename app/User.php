bf150fe01d812ee018cd597827bc586dd31c38b6
mizuki ito <popobi1102@yahoo.com>
2024-04-19 22:43:13 +0900

    feature/mizuki/migration_seeder fix

--- app/User.php
+++ app/User.php
@@ -7,9 +7,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
