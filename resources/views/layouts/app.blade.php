0fdf0ccb0b978a11cf48086feac8f1d6d11e1a51
shintaro yasuda <shin38@ymail.ne.jp>
2024-04-19 20:54:13 +0900

    #911 ユーザ新規登録修正３

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
         @include('commons.header')
         <div class="container">
             @yield('content')
-            @include('commons.error_messages')
         </div>
         @include('commons.footer')
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
