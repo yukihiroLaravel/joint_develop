7381bccdf0bf3628d9ba3b396a30ea2073e280ae
manabuandy <andybell1022@gmail.com>
2024-04-21 07:51:56 +0900

    ユーザ新規登録（たわｒつみだ　まなぶ）

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//ユーザ新規登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+   //  return view('welcome');
+// });
