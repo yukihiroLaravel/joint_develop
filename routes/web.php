2decbd3166c2c476c5ed567c02f97bf84af688f1
aya yamamoto <trfl.ack3@gmail.com>
2024-04-22 22:10:09 +0900

    ”ユーザログイン・ログアウトの修正”

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,4 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('/', 'UsersController@index');
 Route::get('/', 'PostsController@index')->name('posts.index');
