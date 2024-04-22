3a8c132a739ac53f232b118dd1e25d47248c34f0
mtakk <forsyuka321@gmail.com>
2024-04-22 13:00:36 +0900

    #907 ユーザ詳細 レビュー修正　bladeファイルの分離箇所修正

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -25,13 +25,7 @@
         <!-- ここから投稿表示部分追加 -->
         <ul class="list-unstyled">
             @foreach ($posts as $post)
-            <li class="mb-3 text-center">
-                <div class="text-left d-inline-block w-75 mb-2">
-                    <img class="mr-2 rounded-circle" src="{{ Gravatar::src( $user->email , 55) }}" alt="ユーザのアバター画像">
-                    <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('user.show', $user->id) }}">{{ $user->name }}</a></p>
-                </div>
                 @include('posts.post', ['user' => $user, 'post' => $post])
-            </li>
             @endforeach
         </ul>
         <div class="m-auto" style="width: fit-content">
