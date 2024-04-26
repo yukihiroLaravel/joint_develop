03bc4427842c58d82274cecb5ef52af69e04cdfa
yusuke tosaka <tosakafullyremote@gmail.com>
2024-04-27 08:46:07 +0900

    #910 ユーザログイン・ログアウト

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,8 @@
 */
 // トップページの投稿表示
 Route::get('/', 'PostsController@index');
+
+// ユーザー　ログイン・ログアウト
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
