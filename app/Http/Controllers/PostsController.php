28ffabb6ff3ad86680c83ad836658452e421e92e
root <root@myassistant.localdomain>
2024-04-28 00:17:52 +0900

    トップページの投稿表示・修正５回目

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,7 +10,8 @@ class PostsController extends Controller
   public function index()
   {
     $posts = Post::orderBy('id', 'desc')->paginate(10);
-    
-    return view('welcome', ['posts' => $posts ]);
+    return view('welcome', [
+      'posts' => $posts
+    ]);
   }
 }
