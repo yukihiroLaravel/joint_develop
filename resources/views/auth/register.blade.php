eb9b3989a82d7f7bff2f83f769d8d894d3805f6f
manabuandy <andybellphp@gmail.com>
2024-05-04 23:07:23 +0900

    パスワード入力・確認値保持解消

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,12 +24,12 @@
                     <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}">
                 </div>
                 <div class="form-group">
-                    <lavel for="password">パスワード</label>
-                    <input id="password" type="password" class="form-control" name="password">
+                    <lavel for="password">パスワード</label>"
+                    <input id="password" type="password" class="form-control" name="password" value="{{ ('password') }}">
                 </div>
                 <div class="form-group">
                     <lavel for="password_confirmation">パスワード確認</label>
-                    <input id="password_confirmation" type="password" class="form-control" name="password">
+                    <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" value="{{ ('password_confirmation') }}">
                 </div>
                 <button type="submit" class="btn btn-primary mt-2">新規登録</button>
             </from>
