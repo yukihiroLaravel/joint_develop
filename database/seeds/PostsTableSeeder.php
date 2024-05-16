3ede89572d88b691a0925dfc7c94c43baea11dd0
masashitamayama <okita-souji.7-7-3@outlook.jp>
2024-05-17 08:42:42 +0900

    #1086フォロー機能（タイムライン（ユーザ詳細画面）の変更）

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -11,19 +11,19 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        $seedMaxUser = 100; // 4から100に変更、100人のユーザーが存在するため
+        $seedMaxUser = 100; // 100人のユーザーが存在するため
         $posts = []; // 空配列を初期値として設定
 
         // 繰り返し処理で初期ポスト用の多次元配列を作成
-        for ($i = 1; $i <= 100; $i++) { // 30回から100回に変更、100件の投稿を生成
-            // ユーザーIDを割り算の余りで計算
-            $userId = ($i % $seedMaxUser !== 0) ? $i % $seedMaxUser : $seedMaxUser;
-            $posts[] = [
-                'user_id' => $userId, // 1から100までのユーザーIDを割り当て
-                'content' => '初期ポスト' . $i, // 初期ポスト1〜30
-                'created_at' => now(), // 現在日時
-                'updated_at' => now() // 現在日時
-            ];
+        for ($userId = 1; $userId <= $seedMaxUser; $userId++) {
+            for ($i = 1; $i <= 11; $i++) {
+                $posts[] = [
+                    'user_id' => $userId, // 1から100までのユーザーIDを割り当て
+                    'content' => 'ユーザー' . $userId . 'の初期ポスト' . $i, // ユーザーごとの初期ポスト1〜11
+                    'created_at' => now(), // 現在日時
+                    'updated_at' => now() // 現在日時
+                ];
+            }
         }
 
         // データベースに投稿を挿入
