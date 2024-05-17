268579a4362a1d83af1042b45ade3fc680ac4eff
mizuki ito <popobi1102@yahoo.com>
2024-05-17 23:41:24 +0900

    4ユーザ詳細　再々修正

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -2,13 +2,13 @@
 <ul class="list-unstyled">
                     
         @foreach ($posts as $post)
-            <li class="mb-3 text-center">
-                <div class="text-left d-inline-block w-75 mb-2">
-                      <img class="mr-2 rounded-circle" src="{{ Gravatar::src($post->user->email, 55) }}" alt="ユーザのアバター画像">
-                       <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('user.show', ['id' => $post->user->id]) }}">{{ $post->user->name }}</a></p>
-                       <p>{{ $post->content }}</p>
-                       <p>{{ $post->created_at }}</p>
-                </div>
+            <li class="mb-3">
+                <div class="row">
+                    <div class="col-5 w-75">
+                        <div class="text-left d-inline-block mb-2">
+                             <img class="mr-2 rounded-circle" src="{{ Gravatar::src($post->user->email, 55) }}" alt="ユーザのアバター画像">
+                             <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('user.show', ['id' => $post->user->id]) }}">{{ $post->user->name }}</a></p>
+                        </div>
                     <p>{{ $post->content }}</p>
                     <p>{{ $post->created_at }}</p> 
             </li>
@@ -25,7 +25,7 @@
         </div>
                 @endif
 </ul>
-
 <div class="m-auto" style="width: fit-content">
 {{ $posts->links('pagination::bootstrap-4') }}
+</div>
 </div>
