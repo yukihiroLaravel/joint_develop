b4eebc3509534b31e2a4736f254c56e2d60775ad
masashitamayama <okita-souji.7-7-3@outlook.jp>
2024-05-26 12:11:21 +0900

    #1119 検索機能　アップデート

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -1,6 +1,6 @@
 {{-- 検索ワード入力 --}}
-<form action="{{ isset($user) ? route('users.show', ['id' => $user->id]) : route('top') }}" method="GET" class="form-inline my-2 my-lg-0 w-100 search-form">
-    <input class="form-control mr-sm-2 w-75" type="search" placeholder="Search" aria-label="Search" name="keyword" value="{{ request('keyword') }}">
+<form action="{{ isset($user) ? route('users.show', ['id' => $user->id]) : route('top') }}" method="GET" class="form-inline my-2 my-lg-0 w-75 search-form"><!-- w-100をw-75に修正 -->
+    <input class="form-control mr-sm-2 " type="search" placeholder="検索キーワードを入力" aria-label="Search" name="keyword" value="{{ request('keyword') }}">
     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
         <i class="fas fa-search"></i> <!-- Font Awesomeの虫眼鏡アイコン -->
     </button>
