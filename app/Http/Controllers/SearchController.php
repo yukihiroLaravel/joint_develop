5aae45c1a622c15aa3461401fb23fe43f35db947
saitoh <mintorimoto@gmail.com>
2024-05-27 00:40:02 +0900

    記事検索4

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,34 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\SearchController;
 use App\Post;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
+    public function index(Request $request)
+    {
+        $keyword = $request->input('keyword');
 
-public function index(Request $request)
-{
-    $keyword = $request->input('keyword');
-
-    // 投稿をページネーション付きで取得
-    $posts = Post::where('content', 'like', '%' . $keyword . '%')
-                 ->orWhere('content', 'like', '%' . $keyword . '%')
-                 ->paginate(10); // 1ページに表示するアイテム数を指定
-
-    // フィルタリングされた投稿の総数をカウント
-    $totalPosts = Post::where('content', 'like', '%' . $keyword . '%')
-                 ->orWhere('content', 'like', '%' . $keyword . '%')
-                 ->count();
+        $posts = Post::where('content', 'like', '%' . $keyword . '%');
+        $totalPosts = $posts->count();
+        $posts = $posts->paginate(10);
 
         if ($posts->isEmpty()) {
             // 検索結果が空の場合
             $message = "検索した記事は見つかりませんでした。　(ヾﾉ･ω･`)ﾅｲﾅｲ";
-            return view('search-results', compact('message'));
+            return view('search-results', [
+                'message' => $message,
+            ]);
         }
 
-    return view('search-results', compact('posts', 'totalPosts'));
-}
-
+        return view('search-results', [
+            'posts' => $posts,
+            'totalPosts' => $totalPosts,
+        ]);
+    }
 }
