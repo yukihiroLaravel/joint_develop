72e6a4087595171b129d8c0d1bf04865d7ce2524
masashitamayama <okita-souji.7-7-3@outlook.jp>
2024-05-27 22:26:58 +0900

    #1119 ※ユーザ情報の編集ボタン表示修正（たまやま）

--- resources/views/layouts/user_profile.blade.php
+++ resources/views/layouts/user_profile.blade.php
@@ -5,9 +5,11 @@
     </div>
     <div class="card-body">
         <img class="rounded-circle img-fluid" src="{{ Gravatar::src($user->email, 308) }}" alt="ユーザのアバター画像">
+        @if (Auth::id() == $user->id)
         <div class="mt-3">
             <a href="{{ route('users.edit', ['id' => $user->id]) }}" class="btn btn-primary btn-block">ユーザ情報の編集</a>
         </div>
+        @endif
     </div>
 </div>
 <!-- ここにフォローボタンを挿入 -->
