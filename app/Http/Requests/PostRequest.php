b5e1a6f144cd8ae5298b7f80bf1abda379d46e17
saitoh <mintorimoto@gmail.com>
2024-06-05 02:35:57 +0900

    投稿画像2

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -24,7 +24,7 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            'content' => 'required|max:140',
+            'content' => 'required|max:140'
         ];
     }
 
