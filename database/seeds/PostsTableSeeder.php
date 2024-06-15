f729ffea5634676783c58d6f60ca755078945c2a
kagino tadashi <kimonoremark@yahoo.co.jp>
2024-06-16 01:47:51 +0900

    #95 posts（投稿）テーブルのマイグレーションとシーダー作成

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,10 +12,33 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         DB::table('posts')->insert([
-            'content' => 'テストです',
-            'user_id' => 1,
+            'content' => 'テスト文1です',
+            'user_id' => 2,
             
         ]);
        
+        DB::table('posts')->insert([
+            'content' => 'テスト文2です',
+            'user_id' => 2,
+            
+        ]);
+
+        DB::table('posts')->insert([
+            'content' => 'テスト文3です',
+            'user_id' => 3,
+            
+        ]);
+
+        DB::table('posts')->insert([
+            'content' => 'テスト文4です',
+            'user_id' => 4,
+            
+        ]);
+
+        DB::table('posts')->insert([
+            'content' => 'テスト文5です',
+            'user_id' => 5,
+            
+        ]);
     }
 }
