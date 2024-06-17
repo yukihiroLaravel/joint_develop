44641fd8053ccd617f63b0876054bcb5f5e6b135
mizuki ito <popobi1102@yahoo.com>
2024-06-17 16:48:35 +0900

    ユーザログイン・ログアウト

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,11 @@
 |
 */
 
+// ログイン
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+
+
 Route::get('/', 'PostController@index');
