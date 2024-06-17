32f19ceab3ececcb9c3ef4303d42d28cd1b9d8b3
kagino tadashi <kimonoremark@yahoo.co.jp>
2024-06-17 21:53:33 +0900

    #1151 #95 posts（投稿）テーブルのマイグレーションとシーダー作成（鍵野）

--- database/migrations/2024_06_14_235613_create_posts_table.php
+++ database/migrations/2024_06_14_235613_create_posts_table.php
@@ -21,10 +21,8 @@ class CreatePostsTable extends Migration
             $table->softDeletes();
             // 外部キー制約
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            
         });
     }
-
     /**
      * Reverse the migrations.
      *
