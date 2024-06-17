681d106e8b4c52b97bf3ce6621f5d942f2d9d971
mizuki ito <popobi1102@yahoo.com>
2024-06-17 12:11:41 +0900

    ヘッダー・フッター & トップページのタイトル表示 part2~

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UsersController extends Controller
+class PostController extends Controller
 {
     public function index()
     {
