87eda08c5e4cc7ef6bb5f81e894ebd3aee27b93a
masaaki tsuji <masaaki@example.com>
2024-06-24 08:21:43 +0900

    #115 ユーザー詳細

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@section('content')
+    <h1>{{ $user->name }}</h1>
+    <ul class="nav nav-tabs nav-justified mt-5 mb-2">
+        <li class="nav-item nav-link {{ Request::is('users/'. $user->id) ? 'active' : '' }}">
+        <a href="{{ route('user.show', $user->id) }}">投稿<br>
+        <div class="row">
+        <aside class="col-sm-4 mb-5">
+            <div class="card bg-info">
+                <div class="card-header">
+                    <h3 class="card-title text-light"></h3>
+                </div>
+                <div class="card-body">
+                    <img class="rounded-circle img-fluid" src="" alt="">
+                        <div class="mt-3">
+                            <a href="" class="btn btn-primary btn-block">ユーザ情報の編集</a>
+                        </div>
+                </div>
+            </div>
+        </aside>
+        <div class="col-sm-8">
+            <ul class="nav nav-tabs nav-justified mb-3">
+                <li class="nav-item"><a href="" class="nav-link {{ Request::is('users/'. $user->id) ? 'active' : '' }}">タイムライン</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">フォロー中</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">フォロワー</a></li>
+            </ul>
+        </div>
+    </div>
+    @include('posts.posts',['user' => $user, 'posts'=> $posts])
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,10 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+// ユーザ
+Route::get('/', 'UsersController@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
 
 Route::get('/', 'PostController@index');
