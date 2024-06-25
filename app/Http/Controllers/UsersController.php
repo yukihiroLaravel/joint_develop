6ce77f229a6cb8e0cffcafb17bfd479b0071c1df
yukihiro seki <freegoodplayernow@gmail.com>
2024-06-25 23:22:38 +0900

    ユーザー詳細

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\User;
+class UsersController extends Controller
+{
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        $posts = $user->posts()->orderBy('id', 'desc')->paginate(9);
+        $data=[
+            'user' => $user,
+            'posts' => $posts,
+        ];
+        return view('users.show',$data);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
