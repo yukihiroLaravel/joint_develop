b0752ac5afdabc6b507a1f0551d3321b3b884dc0
mizuki ito <popobi1102@yahoo.com>
2024-06-28 10:04:19 +0900

    ~ユーザ編集画面・更新~

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use App\User;
+use App\Post;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
+class UsersController extends Controller
+{
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        $posts = $user->posts();
+        $data = [
+            'user'=> $user,
+            'posts'=>$posts
+        ];
+        return view('users.show', $data);
+    }
+    
+    //ユーザ編集画面・更新
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        if (Auth::user()->id !== $user->id) {
+            abort(403);
+        }
+        return view('users.edit', ['user' => $user]);
+    }   
+    
+    public function update(UserRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password); 
+        $user->save();
+        return redirect()->route('user.show',['id'=> $user->id]);
+    }
+}
