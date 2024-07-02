abca68f3c1d36d63a470c2b501a7646806df0192
masaaki tsuji <masaaki@example.com>
2024-07-02 20:21:35 +0900

    #115 ユーザー詳細

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,18 +1,17 @@
 @extends('layouts.app')
 @section('content')
-    <h1>{{ $user->name }}</h1>
+    <h1> </h1>
     <ul class="nav nav-tabs nav-justified mt-5 mb-2">
-        <li class="nav-item nav-link {{ Request::is('users/'. $user->id) ? 'active' : '' }}">
         <div class="row">
         <aside class="col-sm-4 mb-5">
             <div class="card bg-info">
                 <div class="card-header">
-                    <h3 class="card-title text-light"></h3>
+                    <h3 class="card-title text-light">{{ $user->name }}</h3>
                 </div>
                 <div class="card-body">
-                <img class="mr-2 rounded-circle" src="{{ Gravatar::src($user->email, 55) }}" alt="ユーザのアバター画像">
+                <img class="mr-2 rounded-circle" src="{{ Gravatar::src($user->email, 400) }}" alt="ユーザのアバター画像">
                         <div class="mt-3">
-                            <a href="" class="btn btn-primary btn-block">ユーザ情報の編集</a>
+                            
                         </div>
                 </div>
             </div>
@@ -23,7 +22,8 @@
                 <li class="nav-item"><a href="#" class="nav-link">フォロー中</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">フォロワー</a></li>
             </ul>
+            @include('posts.posts', ['posts' => $posts]) 
         </div>
     </div>
-    @include('posts.posts', ['posts' => $posts]) 
+    
 @endsection
