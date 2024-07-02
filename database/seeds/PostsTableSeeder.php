4d706fcf011f6efa1bdd3c87e617e998c2a4c19e
mizuki ito <popobi1102@yahoo.com>
2024-07-02 10:43:30 +0900

    投稿削除

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,48 +12,8 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         DB::table('posts')->insert([
-            'content' => 'テスト文1です',
-            'user_id' => 1,  
-        ]);
-        DB::table('posts')->insert([
-            'content' => 'テスト文2です',
-            'user_id' => 2,
-        ]);
-        DB::table('posts')->insert([
-            'content' => 'テスト文3です',
-            'user_id' => 3,
-        ]);
-        DB::table('posts')->insert([
-            'content' => 'テスト文4です',
-            'user_id' => 4,
-        ]);
-        DB::table('posts')->insert([
-            'content' => 'テスト文5です',
-            'user_id' => 1,
-        ]);
-        DB::table('posts')->insert([
-            'content' => 'テスト文6です',
-            'user_id' => 2,  
-        ]);
-        DB::table('posts')->insert([
-            'content' => 'テスト文7です',
-            'user_id' => 3,
-        ]);
-        DB::table('posts')->insert([
-            'content' => 'テスト文8です',
-            'user_id' => 4,
-        ]);
-        DB::table('posts')->insert([
-            'content' => 'テスト文9です',
-            'user_id' => 1,
-        ]);
-        DB::table('posts')->insert([
-            'content' => 'テスト文10です',
-            'user_id' => 2,
-        ]);
-        DB::table('posts')->insert([
-            'content' => 'テスト文11です',
-            'user_id' => 3,
+            'content' => 'あかさたなはまやらわ',
+            'user_id' => 5,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('{id}/edit', 'UsersController@edit')->name('users.edit');
         Route::put('{id}', 'UsersController@update')->name('users.update');
     });
+    //投稿削除
+    Route::prefix('posts')->group(function () {    
+        Route::delete('{id}', 'PostController@destroy')->name('post.delete');
+    });
 });
