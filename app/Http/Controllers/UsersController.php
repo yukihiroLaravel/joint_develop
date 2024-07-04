a9cbf3ae0704752ebf3e89ff38cb13647fc3a3cd
masaaki tsuji <masaaki@example.com>
2024-07-05 07:30:46 +0900

    #115 ユーザー詳細

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,7 +13,10 @@ class UsersController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $post = Post::orderBy('id','desc')->paginate(10);
+        return view('welcome', [
+            'posts' => $post,
+        ]);
     }
     public function show($id)
     {
