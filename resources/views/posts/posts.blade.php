6d5820283c9b31e61ea638ef160ed3b1c0e9f694
mizuki ito <popobi1102@yahoo.com>
2024-07-05 22:17:20 +0900

    投稿削除３回目

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -12,7 +12,7 @@
                 </div>
                 @if (Auth::id() === $post->user_id)
                     <div class="d-flex justify-content-between w-75 pb-3 m-auto">
-                        <form method="POST" action="{{ route('post.delete', $post->id) }}">
+                        <form method="POST" action="{{ route('posts.delete', $post->id) }}">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-danger">削除</button>
