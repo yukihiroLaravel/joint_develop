39440fad9ae5b5c147c2a2a906fa9c5a9a19f11f
masaaki tsuji <masaaki@example.com>
2024-07-05 20:54:34 +0900

    #115 ユーザー詳細

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -31,20 +31,20 @@ class UsersController extends Controller
      //ユーザ編集画面・更新
     public function edit($id)
     {
-         $user = User::findOrFail($id);
-         if (Auth::user()->id !== $user->id) {
+        $user = User::findOrFail($id);
+        if (Auth::user()->id !== $user->id) {
              abort(403);
          }
-         return view('users.edit', ['user' => $user]);
+        return view('users.edit', ['user' => $user]);
     }   
     public function update(UserRequest $request, $id)
     {
-         $user = User::findOrFail($id);
-         $user->name = $request->name;
-         $user->email = $request->email;
-         $user->password = Hash::make($request->password); 
-         $user->save();
-         return back();
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password); 
+        $user->save();
+        return back();
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 // ユーザ
-Route::get('/', 'UsersController@index');
 Route::prefix('users')->group(function () {
     Route::get('{id}', 'UsersController@show')->name('user.show');
 });
@@ -33,7 +32,6 @@ Route::group(['middleware' => 'auth'], function () {
     Route::prefix('users')->group(function () {
         Route::get('{id}/edit', 'UsersController@edit')->name('users.edit');
         Route::put('{id}', 'UsersController@update')->name('users.update');
-        Route::get('users/{id}', 'UsersController@show')->name('users.show');
     });
 });
 
