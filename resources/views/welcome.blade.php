3b291bf9db571c5fd357cbe0d39e9851f13a24c4
Kazuya Matsumoto <momo.choco0203@gmail.com>
2024-07-05 09:58:32 +0900

    #119 投稿新規作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,10 @@
         <div class="w-75 m-auto">
         </div>
         <div class="text-center mb-3">
-            <form method="" action="" class="d-inline-block w-75">
+            <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
+                @csrf
                 <div class="form-group">
-                    <textarea class="form-control" name="" rows=""></textarea>
+                    <textarea class="form-control" name="content" rows="4"></textarea>
                     <div class="text-left mt-3">
                         <button type="submit" class="btn btn-primary">投稿する</button>
                     </div>
