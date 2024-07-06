78234ebcc7472e07e01ca5c645f64059ad54cc5e
Kazuya Matsumoto <momo.choco0203@gmail.com>
2024-07-06 19:21:36 +0900

    #119 投稿新規作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,17 +10,17 @@
         <div class="w-75 m-auto">
         </div>
         <div class="text-center mb-3">
-        @if (Auth::check())
-            <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
-                @csrf
-                <div class="form-group">
-                    <textarea class="form-control" name="content" rows="4"></textarea>
-                    <div class="text-left mt-3">
-                        <button type="submit" class="btn btn-primary">投稿する</button>
+            @if (Auth::check())
+                <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
+                    @csrf
+                    <div class="form-group">
+                        <textarea class="form-control" name="content" rows="4"></textarea>
+                        <div class="text-left mt-3">
+                            <button type="submit" class="btn btn-primary">投稿する</button>
+                        </div>
                     </div>
-                </div>
-            </form>
-        @endif
+                </form>
+            @endif
         </div>
         @include('posts.posts',['posts'=> $posts])
 @endsection
