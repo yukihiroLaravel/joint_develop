ff6c4d3900081d22a96621248037fbf29fda48a7
Kazuya Matsumoto <momo.choco0203@gmail.com>
2024-07-16 06:12:12 +0900

    #119 投稿新規作成　修正

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 Route::group(['middleware' => 'auth'], function () {
     Route::prefix('posts')->group(function(){
         Route::post('','PostsController@store')->name('post.store');
-});
+    });
     Route::prefix('users')->group(function () {
         Route::get('{id}/edit', 'UsersController@edit')->name('users.edit');
         Route::put('{id}', 'UsersController@update')->name('users.update');
