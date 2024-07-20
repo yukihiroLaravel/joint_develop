cb4c347e555f48cc186ce1b4f98205ba01774b83
masaaki tsuji <masaaki@example.com>
2024-07-21 00:01:28 +0900

    Save changes before switching branches

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace App\Console;
+
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -12,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         //
     ];
+
     /**
      * Define the application's command schedule.
      *
@@ -23,6 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
     }
+
     /**
      * Register the commands for the application.
      *
@@ -35,4 +40,5 @@ class Kernel extends ConsoleKernel
         require base_path('routes/console.php');
     }
 }
+
 
