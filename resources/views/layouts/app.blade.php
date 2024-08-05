264be6110ce774b5f7ff4267c91df0026d9a4631
rie nishio <6nekohime@gmail.com>
2024-08-05 22:51:07 +0900

    フォントの変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,14 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
         <link rel="stylesheet" href="{{ asset('/css/font.css') }}">
-        
+        <style>@font-face {           
+             font-family: 'waltograph';            
+             src: url('waltograph42.ttf') format('truetype');        
+             }        
+             body {            
+                font-family: 'waltograph', sans-serif;       
+                 }   
+         </style>     
     </head>
     <body>
         @include('commons.header')
