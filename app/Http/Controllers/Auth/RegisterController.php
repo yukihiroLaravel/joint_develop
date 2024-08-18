1c140172e6340b63bff342b56ff9405b617bd3e4
irochi1184 <ken.office.arita@gmail.com>
2024-08-18 09:35:28 +0900

    #107 ユーザ新規登録(有田)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,8 +29,8 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
+    protected $redirectTo = '/';
+    
     /**
      * Create a new controller instance.
      *
