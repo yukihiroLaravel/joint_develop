607009079841aaa100786ff55a511aa4933fbfd2
irochi1184 <ken.office.arita@gmail.com>
2024-08-22 09:13:25 +0900

    ユーザ詳細(有田)

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,15 +1,5 @@
 {{-- @extends('layouts.app') --}}
 {{-- @section('content') --}}
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-  <meta charset="utf-8">
-  <title></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="{{ asset('/css/styles.css') }}">
-</head>
 <div class="row">
     <aside class="col-sm-4 mb-5">
         <div class="card bg-info">
