a9b422cb8b11e2eef25c9e64b605173970b1e709
shoko nishiyama <superawesome.kujira@gmail.com>
2024-08-30 10:56:00 +0900

    #115 ユーザ詳細

---
+++ resources/views/posts/posts.blade.php
@@ -0,0 +1,26 @@
+<ul class="posts list-unstyled">
+    @foreach($posts as $post)
+        <li class="mb-3 text-center">
+            <div class="text-left d-inline-block w-75 mb-2">
+                <img class="mr-2 rounded-circle" src="{{ $post->user->avatar }}" alt="ユーザのアバター画像">
+                <p class="mt-3 mb-0 d-inline-block"><a href="">投稿ユーザ名</a></p>
+            </div>
+            <div class="post">
+                <div class="text-left d-inline-block w-75">
+                    <p class="mb-2">{{ $post->content }}</p>
+                    <p class="text-muted">{{ $post->created_at->format('Y-m-d H:i:s') }}</p>
+                </div>
+                    <div class="d-flex justify-content-between w-75 pb-3 m-auto">
+                        <form method="POST" action="">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">削除</button>
+                        </form>
+                        <a href="" class="btn btn-primary">編集する</a>
+                    </div>
+            </div>
+        </li>
+    @endforeach    
+</ul>
+<div class="m-auto" style="width: fit-content"></div>
+
