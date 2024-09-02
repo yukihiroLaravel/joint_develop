2889dee03907192ad12f4671401d410aa0cc7585
masafumi oohata <masafumi.oohata@gmail.com>
2024-09-02 22:21:26 +0900

    posts(投稿)テーブルのマイグレーションとシーダー作成(修正4-1)

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,3 @@
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
-
-Route::get('/', 'UsersController@index');
