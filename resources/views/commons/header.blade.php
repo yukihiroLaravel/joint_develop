219b8aa87e92ff7190c537191cf873a7bdad33ed
kazuya takei <takei0831ka@gmail.com>
2024-09-06 18:46:17 +0900

    ユーザログイン・ログアウト#110

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -17,8 +17,4 @@
         </div>
     </nav>
 </header>
-<!-- @if(Auth::check())
-    <p class="text-right mr-3 pb-3">
-        ユーザー：<span class="user-name">{{ Auth::user()->name }}</span>
-    </p>
-@endif -->
+
