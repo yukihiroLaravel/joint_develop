5c6b6053c5a92428ff6f77928009ff82d32a1655
masafumi oohata <masafumi.oohata@gmail.com>
2024-09-16 22:11:41 +0900

    共通ファイル＆ヘッダー・フッター＆トップページのタイトルのみ表示「Topic Posts」修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     </div>
 </div>
 <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
-    <div class="w-75 m-auto">@extends('commons.error_messages')</div>
+    <div class="w-75 m-auto">@include('commons.error_messages')</div>
     @if (Auth::check())
     <div class="text-center mb-3">
         <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
