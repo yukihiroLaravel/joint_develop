7bab1f2e836f9b36c103308c4142211fb1a7bba8
irochi1184 <ken.office.arita@gmail.com>
2024-09-24 09:33:32 +0900

    ミドルウェア修正

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,9 +50,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($exception instanceof PostTooLargeException) {
-            return redirect()->back()->withErrors(['image' => 'ファイルサイズが大きすぎます。最大50MBまでです。']);
-        }
         return parent::render($request, $exception);
     }
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \App\Http\Middleware\CustomPostSizeMiddleware::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
@@ -52,6 +51,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'image.post' => \App\Http\Middleware\CustomPostSizeMiddleware::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
