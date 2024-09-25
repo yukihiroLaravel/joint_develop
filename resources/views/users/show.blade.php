ce1c6bd11611ef1376fc0c41e4ea5a7885ed72f6
shoko nishiyama <superawesome.kujira@gmail.com>
2024-09-25 22:15:23 +0900

    #127 フォロー機能

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,6 +5,7 @@
             <div class="card bg-info">
                 <div class="card-header">
                     <h3 class="card-title text-light">{{ $user->name }}</h3>
+                    @include('follow.follow_button', ['user' => $user])
                 </div>
                 <div class="card-body">
                     <img class="rounded-circle img-fluid" src="{{ Gravatar::src($user->email, 350) }}" alt="ユーザのアバター画像">
