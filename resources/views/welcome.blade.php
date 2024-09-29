3bd3b72f0939df6b86c5cffcda16125701ee34fb
masafumi oohata <masafumi.oohata@gmail.com>
2024-09-30 01:58:09 +0900

    フラッシュメッセージ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,25 @@
     </div>
 </div>
 <h5 class="text-center mb-3">"○○"について140字以内で会話しよう！</h5>
+
+    <!-- フラッシュメッセージ用モーダル -->
+            @if (session('flashmessage'))
+                <div class="modal fade" id="flashMessageModal" tabindex="-1" role="dialog" aria-labelledby="flashMessageModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="flashMessageModalLabel">お知らせ</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                {{ session('flashmessage') }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
     <div class="w-75 m-auto">@include('commons.error_messages')</div>
     @if (Auth::check())
     <div class="text-center mb-3">
@@ -21,4 +40,21 @@
     </div>
     @endif    
 @include('posts.posts', ['posts => $posts'])
+    <!-- フラッシュメッセージ用モーダルを表示し、5秒後に自動的に閉じるスクリプト -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            // フラッシュメッセージがあればモーダルを表示
+            @if (session('flashmessage'))
+                $('#flashMessageModal').modal('show');
+                // 5秒後にモーダルを自動的に閉じる
+                setTimeout(function() {
+                    $('#flashMessageModal').modal('hide');
+                }, 5000);  // 5000ミリ秒 = 5秒
+            @endif
+        });
+    </script>
 @endsection
