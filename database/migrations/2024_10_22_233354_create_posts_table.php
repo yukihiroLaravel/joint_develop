812d2424551d67c8887bde542cc0b9d38c411c5b
take <panicdepanic@gmail.com>
2024-10-24 08:10:50 +0900

    postsテーブルのマイグレーションとシーダー作成

---
+++ database/migrations/2024_10_22_233354_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    //マイグレーションファイル（DBテーブルの履歴を管理するファイル）
+
+    public function up() //投稿する。
+    {
+        Schema::create('posts', function (Blueprint $table) { //マイグレーションは複数形。モデルは単数系。laravel側で自動判定。
+            $table->bigIncrements('id'); //主キー。idが自動的に追加。
+            //$table->string('title')->nullable(); //タイトル（文字列）nullでも可。
+            $table->text('content'); //投稿内容。X(twitter)でもtext型のため。
+            $table->bigInteger('user_id')->unsigned()->index(); //ユーザーのid。マイナス符号がない大きい整数。indexで検索しやすい。
+            $table->timestamps(); //メソッドが実行された時間（投稿した時間）
+            $table->softDeletes(); //論理削除(優しい削除)
+
+            //外部キー制約
+            //$table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');//ユーザーテーブルのidとポストテーブルのuser_idを外部キー制約。
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i=1;$i<=5;$i++){ //テストを５件
+            DB::table('posts')->insert([
+                'content' => 'テスト投稿だよ' .$i,
+                'user_id' => $i,
+            ]);
+        }
+    }
+}
