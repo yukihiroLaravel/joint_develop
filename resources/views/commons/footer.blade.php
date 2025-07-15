b609c40b16950e3e8b69894b6e74bafdad2f1ffb
hideki kishimoto <hidekishi_15109858986721@yahoo.co.jp>
2025-07-15 23:54:54 +0900

    #97 共通ファイル&ヘッダー・フッター & トップページのタイトルのみ表示Topic Posts

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="mt-5">
+    <nav class="navbar navbar-dark bg-dark justify-content-center">
+        <span class="navbar-brand">©Gut Familie, All rights reserved.</span>
+    </nav>
+</footer>
