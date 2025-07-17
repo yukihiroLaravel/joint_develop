a9fe009eb72cc3ecb42eee62a12ccf46e96a0f00
hideki kishimoto <hidekishi_15109858986721@yahoo.co.jp>
2025-07-17 23:16:56 +0900

    #110 ユーザログイン・ログアウト

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,9 @@
 |
 */
 
+// ログイン・ログアウト
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
 Route::get('/', 'PostsController@index');
