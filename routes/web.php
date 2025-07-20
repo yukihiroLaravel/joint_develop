706d1ebe21e0eebad2c25128e359be8a0481e14d
yoshitsugu <ynwaerjkdbl@gmail.com>
2025-07-21 02:24:45 +0900

    #115 ユーザ詳細(なかよし)

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,9 @@
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// ユーザ
 Route::get('/', 'PostsController@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
