2674542f471694767c5f27e1841a3d96288387b5
yoshitsugu <ynwaerjkdbl@gmail.com>
2025-07-21 21:54:49 +0900

    ユーザ詳細(なかよし)

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -1,5 +1,5 @@
 <div class="col-lg-4 mb-5">
-    <div class="movie text-left d-inline-block">
+    <div class="posts text-left d-inline-block">
         <a href="{{ route('user.show', $user->id) }}"></a>
     </div>    
 </div>    
