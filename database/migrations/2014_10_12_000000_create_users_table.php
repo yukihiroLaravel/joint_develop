54461d0c96f2fc09426e93a1ee3d67e5e1a62145
mohamad redzuan <mrmy162@gmail.com>
2025-11-20 22:21:53 +0900

    #94 users（ユーザ）テーブルのマイグレーションとシーダー作成

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();//user情報を削除
         });
     }
 
