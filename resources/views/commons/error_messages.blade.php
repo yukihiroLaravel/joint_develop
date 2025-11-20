206b0ec1185a52d9df42efe94fd82197699ca1ca
ryo maruyama <ryo@example.com>
2025-11-20 21:33:21 +0900

    #107 ユーザ新規登録

---
+++ resources/views/commons/error_messages.blade.php
@@ -0,0 +1,7 @@
+@if ($errors->any())
+    <ul class="alert alert-danger" role="alert">
+        @foreach ($errors->all() as $error)
+            <li class="ml-4">{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+// ユーザ新規登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
