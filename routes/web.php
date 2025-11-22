f503db27a5dca54b0241d066fdbff0e44facfae7
mitsuru tamaki <tera0087@yahoo.co.jp>
2025-11-22 23:34:55 +0900

    #97 共通ファイル&ヘッダー・フッター & トップページのタイトルのみ表示「Topic Posts」

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// トップページの表示
+Route::get('/', 'UsersController@index');
