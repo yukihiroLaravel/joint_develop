68c6ce7d98d5b509cfda6ad8d251fa66d5b2efe5
ryo maruyama <ryo@example.com>
2025-11-25 18:50:44 +0900

    #115 ユーザー詳細

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@
 // トップページの表示
 Route::get('/', 'PostsController@index');
 
+// ユーザ詳細
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'PostsController@show')->name('user.show');
+});
+
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
