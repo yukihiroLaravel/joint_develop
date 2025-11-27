d221840f25f5c32d1d52054ecde8808160b577d2
ryo maruyama <ryo@example.com>
2025-11-28 00:06:23 +0900

    #119 投稿新規作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -11,7 +11,10 @@ class PostsController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $posts = Post::orderBy('id','desc')->paginate(10);
+        return view('welcome', [
+            'posts' => $posts,
+        ]);
     }
 
     public function show($id)
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,9 +18,10 @@
                         <textarea class="form-control" name="content" rows="4"></textarea>
                         <div class="text-left mt-3">
                             <button type="submit" class="btn btn-primary">投稿する</button>
-                        </div>
-                    </div>
+                        </div>                        
+                    </div>                    
                 </form>
-            @endif
+            @endif            
         </div>
+    @include('posts.posts', ['posts' => $posts])
 @endsection
