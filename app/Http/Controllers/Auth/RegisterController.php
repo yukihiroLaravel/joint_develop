1c4cf9105b11a3fe0f192f2226450925baa480bd
ryo sakashita <ryosyo2006@gmail.com>
2025-11-30 18:11:03 +0900

    #107 ユーザ新規登録

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,14 +29,19 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/';
+
+
+
+
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+        public function __construct()
+
     {
         $this->middleware('guest');
     }
@@ -50,10 +55,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+     'name' => ['required', 'string', 'max:255'],
+     'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+     'password' => ['required', 'string', 'min:8', 'confirmed'],
+]);
     }
 
     /**
