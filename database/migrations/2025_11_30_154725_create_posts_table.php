d77b9ce6cc4375dc36e2f444cef7f950086a0f24
Yoisana <34060nagafuchidaisuke@gmail.com>
2025-12-01 11:19:52 +0000

    #95 posts（投稿）テーブルのマイグレーションとシーダー作成

--- database/migrations/2025_11_30_154725_create_posts_table.php
+++ database/migrations/2025_11_30_154725_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned()->index(); // 整数入力＆検索
-            $table->string('content', 14);
+            $table->string('content', 140);
             $table->timestamps();
             $table->softDeletes();
             // 外部キー制約
