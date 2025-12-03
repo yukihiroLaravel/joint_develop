950cd820b95b4d329718358b9a40a6c2b4b44a96
k susa <susaukp@gmail.com>
2025-12-03 20:46:04 +0900

    #113 トップページの投稿表示(ヘッダー・フッター・共通ファイルを削除)

--- resources/views/commons/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="mt-5">
-    <nav class="navbar navbar-dark bg-info justify-content-center">
-        <span class="navbar-brand">©Gut Familie, All rights reserved.</span>
-    </nav>
-</footer>
