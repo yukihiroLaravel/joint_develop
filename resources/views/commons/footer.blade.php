5a39575c55d153159d16f8dd8f4d3d98d184ed1d
Yoisana <34060nagafuchidaisuke@gmail.com>
2025-12-07 02:01:27 +0000

    コンフリ解消後のpostsテーブルのマイグレーション

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="mt-5">
+    <nav class="navbar navbar-dark bg-dark justify-content-center">
+        <span class="navbar-brand">©team_a_shimotsuki_sna, All rights reserved.</span>
+    </nav>
+</footer>
