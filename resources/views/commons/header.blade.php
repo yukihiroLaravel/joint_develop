9c0a6124575f5ef1bb5416c4a71c406f4e13bec8
kenta <stk2nd1986@gmail.com>
2025-12-09 22:02:54 +0900

    コンフリクト解消

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -1,13 +1,11 @@
-<header>
+<header class="mb-5">
     <nav class="navbar navbar-expand-sm navbar-dark bg-info">
         <a class="navbar-brand" href="/">Topic Posts</a>
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
-
             <ul class="navbar-nav">
                 @if(Auth::check())
                     <li class="nav-item"><a href="" class="nav-link text-light">ログアウト</a></li>
