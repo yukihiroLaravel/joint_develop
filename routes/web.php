984925e0b8e96e626184c1dcf9765870b32dba25
keishi morimura <sunshinemorimura777@gmail.com>
2025-12-11 21:04:23 +0900

    post_migration修正

--- routes/web.php
+++ routes/web.php
@@ -20,26 +20,6 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('/', 'PostsController@index');
 
 
-//投稿表示
-Route::get('/posts/welcome', [PostsController::class, 'index'])->name('posts.index');
-
-Route::middleware('auth')->group(function(){
-
-    //投稿表示
-    Route::get('/posts/create', [PostsController::class, 'create'])->name('post.create');
-
-
-    //ユーザ編集画面・更新
-    Route::post('/posts/edit/{id}', [PostsController::class, 'edit'])->name('post.edit');
-
-
-    //ユーザ編集画面・更新
-    Route::post('/posts/user/{id}', [PostsController::class, 'user'])->name('post.user');
-
-    //投稿削除
-    Route::get('/posts/delete/{id}', [PostsController::class, 'delete'])->name('post.delete');
-
-});
 //ログイン
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
