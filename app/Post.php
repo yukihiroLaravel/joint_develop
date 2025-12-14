1028b2f419d6498a963f6707f9075ef82c55973b
Yoisana <34060nagafuchidaisuke@gmail.com>
2025-12-14 01:42:26 +0000

    #913 posts（投稿）テーブルのマイグレーションとシーダー作成

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\User;
 
-class Post extends Model
+class Posts extends Model
 {
     use SoftDeletes;
     public function user()
--- app/User.php
+++ app/User.php
@@ -5,14 +5,14 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\SoftDeletes; 
+use Illuminate\Database\Eloquent\SoftDeletes;  // 追記
 use App\Post;
 
 
 class User extends Authenticatable
 {
     use Notifiable;
-    use SoftDeletes;   
+    use SoftDeletes; // 追記
     /**
      * The attributes that are mass assignable.
      *
