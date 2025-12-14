5c8d94bf3fa0f900ef7187bd0ee401115b6729aa
Yoisana <34060nagafuchidaisuke@gmail.com>
2025-12-14 02:50:17 +0000

    #913 posts（投稿）テーブルのマイグレーションとシーダー作成

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,26 +12,26 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         DB::table('posts')->insert([
-            'user_id'=>1,
-            'content'=>'これはテスト投稿です。',
+            'user_id' => 1,
+            'content' => 'これはテスト投稿です。',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
         DB::table('posts')->insert([
-            'user_id'=>1,
-            'content'=>'phpはわかりやすいプログラミング言語です。',
+            'user_id' => 1,
+            'content' => 'phpはわかりやすいプログラミング言語です。',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
         DB::table('posts')->insert([
-            'user_id'=>1,
-            'content'=>'laravelはとても便利なフレームワークです。',
+            'user_id' => 1,
+            'content' => 'laravelはとても便利なフレームワークです。',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
-        DB::table('posts') -> insert([
-            'user_id'=>1,
-            'content'=>'プログラミングは難しいが楽しい。',
+        DB::table('posts')->insert([
+            'user_id' => 1,
+            'content' => 'プログラミングは難しいが楽しい。',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
