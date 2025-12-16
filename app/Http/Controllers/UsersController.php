9a318d915f2290da964168842ed2703abd86b4fb
kenta <stk2nd1986@gmail.com>
2025-12-16 16:35:08 +0900

    #115 ユーザ詳細

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,15 +7,7 @@ use App\User;
 
 class UsersController extends Controller
 {
-    public function index()
-    {
-        $users = User::orderBy('id','desc')->paginate(9);
-        
-        return view('welcome', [
-            'users' => $users,
-        ]);       
-    }
-
+    
     public function show($id)
     {
         $user = User::findOrFail($id);
