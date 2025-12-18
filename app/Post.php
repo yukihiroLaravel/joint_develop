5895aced3dd97aace93b20eaef3101cd6f661046
ryo maruyama <ryo@example.com>
2025-12-18 11:32:18 +0900

    #1649 紐付け日時の追加

--- app/Post.php
+++ app/Post.php
@@ -23,6 +23,6 @@ class Post extends Model
 
     public function tags()
     {
-        return $this->belongsToMany(Tag::class);
+        return $this->belongsToMany(Tag::class)->withTimestamps();
     }
 }
--- database/migrations/2025_12_16_104145_create_post_tag_table.php
+++ database/migrations/2025_12_16_104145_create_post_tag_table.php
@@ -15,9 +15,9 @@ class CreatePostTagTable extends Migration
     {
         Schema::create('post_tag', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
             $table->unsignedBigInteger('post_id');
             $table->unsignedBigInteger('tag_id');
+            $table->timestamps();
             //外部キー制約
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
