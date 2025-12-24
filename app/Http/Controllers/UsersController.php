4eef7c472162577cbdc17d7f66d59ba87035caf8
k susa <susaukp@gmail.com>
2025-12-24 13:01:55 +0900

    #1656 コントローラー修正

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,35 +11,38 @@ class UsersController extends Controller
     {
         $user = User::findOrFail($id);
         $posts = $user->posts()->orderBy('id', 'desc')->paginate(10);
-
-        $countPosts = $user->posts()->count();
-        $countFollowings = $user->follows()->count();
-        $countFollowers = $user->followed()->count();
-        
-        return view('users.show', compact('user', 'posts', 'countPosts', 'countFollowings', 'countFollowers'))->with('users', collect([]));
+        $data = [
+            'user' => $user,
+            'posts' => $posts,
+            'users' => collect([]),
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show', $data);
     }
 
     public function followings($id)
     {
         $user = User::findOrFail($id);
         $users = $user->follows()->paginate(10);
-
-        $countPosts = $user->posts()->count();
-        $countFollowings = $user->follows()->count();
-        $countFollowers = $user->followed()->count();
-        
-        return view('users.show', compact('user', 'users', 'countPosts', 'countFollowings', 'countFollowers'))->with('posts', collect([]));
+        $data = [
+            'user' => $user,
+            'users' => $users,
+            'posts' => collect([]),
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show', $data);
     }
 
     public function followers($id)
     {
         $user = User::findOrFail($id);
         $users = $user->followed()->paginate(10);
-
-        $countPosts = $user->posts()->count();
-        $countFollowings = $user->follows()->count();
-        $countFollowers = $user->followed()->count();
-        
-        return view('users.show', compact('user', 'users', 'countPosts', 'countFollowings', 'countFollowers'))->with('posts', collect([]));
+        $data = [
+            'user' => $user,
+            'users' => $users,
+            'posts' => collect([]),
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show', $data);
     }
 }
