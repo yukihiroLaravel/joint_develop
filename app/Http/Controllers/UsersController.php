1814db8d9d257f5e58471bb81b935d7f297f657a
daisuke nagafuchi <34060nagafuchidaisuke@gmail.com>
2025-12-24 19:32:38 +0900

    #908 トップページの投稿表示(修正3)

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use App\Http\Requests\UpdateRequest;
+use App\Post;
+use App\Tag;
+use App\Http\Requests\PostRequest; 
 
 class UsersController extends Controller
 {
-    public function index()
+    // 編集画面
+    public function edit($id)
     {
-        return view('welcome');
+        // ユーザ取得（存在しなければ404）
+        $user = User::findOrFail($id);
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+
+        // そのユーザの投稿一覧を取得
+        $posts = $user->posts()->orderBy('created_at', 'desc')->paginate(9);
+        return view('users.show', 
+        [
+            'user'  => $user,
+            'posts' => $posts,
+        ]);
     }
+
+    //ユーザ削除
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $this->authorize('update', $user);
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        //　パスワード更新
+        if ($request->filled('password')) 
+        {
+            $user->password = bcrypt($request->password);
+        }
+         $user->save();
+        
+        return redirect()->route('user.show', $id);
 }
