49422f23140ad545f5df7e1f57c5af4e64403943
mitsuru tamaki <tera0087@yahoo.co.jp>
2025-12-28 23:10:33 +0900

    #121 投稿削除

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -35,4 +35,10 @@ class UsersController extends Controller
         return redirect()->route('user.show', $id);
         
     }
+    
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect('/');
+    }
 }
