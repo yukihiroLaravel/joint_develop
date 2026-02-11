b0c721d8c91ba8ecad7fd82f2c19d2276808d1f6
masuzaki <nakelmg@icloud.com>
2026-02-11 20:50:09 +0900

    ユーザ詳細作業途中

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        $movies = $user->movies()->orderBy('id','desc')->paginate(9);
+        $deta=[
+
+            'user' => $user,
+            'movies' =>$movies,
+
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show',$data);
+    }
 }
