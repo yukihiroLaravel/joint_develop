f286293e1dfe5db35e7f2277c4a40fecd8a775ab
kazuho ishii <kazuho.ishii@gmail.com>
2026-02-20 04:32:52 +0900

    ”トップページに、投稿一覧を表示するver3”

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,8 +9,8 @@ class PostsController extends Controller
 {
     public function index()
     {
-        // 全投稿を最新順で取得
-        $posts = Post::latest()->get();
+        // 全投稿を最新順で1ページに2件表示するように取得
+        $posts = Post::latest()->paginate(2);
 
         // 'welcome' ビューに $posts を渡す
         return view('welcome', compact('posts'));
---
+++ resources/views/posts/post.blade.php
@@ -0,0 +1,36 @@
+    @if($posts->isEmpty())
+        <p>投稿はありません。</p>
+    @else
+        <ul class="list-unstyled">
+            @foreach($posts as $post)
+                <li class="mb-3 text-center">
+                    
+                <div class="text-left d-inline-block w-75 mb-2">
+                    <img class="mr-2 rounded-circle" src="{{ Gravatar::src($post->user->email, 55) }}" alt="ユーザのアバター画像">
+                    <p class="mt-3 mb-0 d-inline-block"><a href="">{{ $post->user->name }}</a></p>
+                    <p class="mt-3 mb-0 d-inline-block">
+                    <small>　投稿日：
+                        @if($post->created_at)
+                            {{ $post->created_at->format('Y/m/d H:i:s') }}
+                        @else
+                            日付未設定
+                        @endif
+                    </small>
+                    </p>
+                </div>
+                <div class="">
+                    <div class="text-left d-inline-block w-75">
+                        <p class="mb-2">{{ $post->title }}</p>
+                        <p class="text-muted">{{ $post->content }}</p>
+                    </div>
+                    <div class="d-flex justify-content-between w-75 pb-3 m-auto">
+                        <form method="" action="">
+                            <button type="submit" class="btn btn-danger">削除</button>
+                        </form>
+                        <a href="" class="btn btn-primary">編集する</a>
+                    </div>
+                </div>
+                </li>
+            @endforeach
+        </ul>
+    @endif
