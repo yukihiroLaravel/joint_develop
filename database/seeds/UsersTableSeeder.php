2e3b26711263bf277a5aeed5c67d3f58ee480b6e
kazuho ishii <kazuho.ishii@gmail.com>
2026-02-20 23:23:40 +0900

    ”トップページに、投稿一覧を表示するver6”

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,26 +15,22 @@ class UsersTableSeeder extends Seeder
             'name' => 'test1',
             'email' => 'test1@test.com',
             'password' => bcrypt('test1'),
-            'avatar' => 'sample_user1.jpg',
         ]);
         DB::table('users')->insert([
             'name' => 'test2',
             'email' => 'test2@test.com',
             'password' => bcrypt('test2'),
-            'avatar' => 'sample_user2.jpg',
 
         ]);
         DB::table('users')->insert([
             'name' => 'test3',
-            'email' => 'test3@sample.com',
+            'email' => 'test3@test.com',
             'password' => bcrypt('test3'),
-            'avatar' => 'sample_user3.jpg',
         ]);
         DB::table('users')->insert([
             'name' => 'test4',
             'email' => 'test4@test.com',
             'password' => bcrypt('test4'),
-            'avatar' => 'sample_user4.jpg',
         ]);
     }
 }
