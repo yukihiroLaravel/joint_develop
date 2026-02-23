9a79e9aae3e68c6be531237863cdf651e8f39e4e
masuzaki <nakelmg@icloud.com>
2026-02-23 23:57:07 +0900

    ユーザ詳細修正

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,20 +7,8 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-
-    public function __construct()
-    {
-        //$this->middleware('auth');
-    }
-
     public function show($id)
     {
-    // 【仮】ログイン機能ができるまでの代わり（今はID 1の人だと仮定）
-    $current_user_id = 1; 
-    // URLのID ($id) と、仮のログインIDが一致しない場合はエラー
-    if ($current_user_id != $id) 
-        return abort(403, '他のユーザーのページは見られません');
-        
         $user = User::findOrFail($id);
         $posts = $user->posts()->paginate(10);
 
@@ -30,11 +18,4 @@ class UsersController extends Controller
         ];
         return view('users.show',$data);
     }
-
-    public function edit($id)
-    {
-        $user = User::findOrFail($id);
-        return view('users.edit', ['user' => $user]);
-    }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,6 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 //ユーザ詳細
 Route::prefix('users')->group(function () {
     Route::get('{id}', 'UsersController@show')->name('user.show');
-    Route::get('{id}/edit', 'UsersController@edit')->name('user.edit');
-    //Route::put('{id}', 'UsersController@update')->name('user.update');
 });
 
 
