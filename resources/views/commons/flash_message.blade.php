8c520f35f42ee8954bc2d8c9846ede70589c6288
masuzaki <nakelmg@icloud.com>
2026-03-21 18:06:50 +0900

    フラッシュメッセージ修正

--- resources/views/commons/flash_message.blade.php
+++ resources/views/commons/flash_message.blade.php
@@ -1,11 +1,13 @@
-@if (session('success'))
-    <div class="alert alert-success role="alert">
-        {{ session('success') }}
-    </div>
-@endif
+<div class="container mt-3">
+    @if (session('success'))
+        <div class="alert alert-success role="alert">
+            {{ session('success') }}
+        </div>
+    @endif
 
-@if (session('danger'))
-    <div class="alert alert-danger role="alert">
-        {{ session('danger') }}
-    </div>
-@endif
+    @if (session('danger'))
+        <div class="alert alert-danger role="alert">
+            {{ session('danger') }}
+        </div>
+    @endif
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
-        <div class="container">
-            {{-- ここに1行追加 --}}
             @include('commons.flash_message')
-        </div>
     <body>
         
         @include('commons.header')
