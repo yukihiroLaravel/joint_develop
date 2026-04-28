e7f2bbc2b5d3c2ebbbc96885dc4406a32c59f5f6
yukihiro sugimoto <ruslanyuki0012@gmail.com>
2026-04-28 21:25:05 +0900

    ポストテーブルのシーダーとマイグレーションの作成

---
+++ database/seeds/PostTableSeeder.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('ALTER TABLE posts AUTO_INCREMENT = 1');
+        DB::table('posts')->insert([
+            'id' => 1,
+            'user_id' => 1,
+            'content' => 'あああああ'
+        ]);
+        DB::table('posts')->insert([
+            'id' => 2,
+            'user_id' => 1,
+            'content' => 'いいいい'
+        ]);
+        DB::table('posts')->insert([
+            'id' => 3,
+            'user_id' => 2,
+            'content' => 'うううう'
+        ]);
+        DB::table('posts')->insert([
+            'id' => 4,
+            'user_id' => 3,
+            'content' => 'ええええ'
+        ]);
+        DB::table('posts')->insert([
+            'id' => 5,
+            'user_id' => 4,
+            'content' => 'おおおおお'
+        ]);
+        DB::table('posts')->insert([
+            'id' => 6,
+            'user_id' => 5,
+            'content' => 'かかかか'
+        ]);
+        DB::table('posts')->insert([
+            'id' => 7,
+            'user_id' => 6,
+            'content' => 'きききき'
+        ]);
+        DB::table('posts')->insert([
+            'id' => 8,
+            'user_id' => 7,
+            'content' => 'くくくく'
+        ]);
+        DB::table('posts')->insert([
+            'id' => 9,
+            'user_id' => 8,
+            'content' => 'けけけけ'
+        ]);
+        DB::table('posts')->insert([
+            'id' => 10,
+            'user_id' => 9,
+            'content' => 'ここここ'
+        ]);
+        DB::table('posts')->insert([
+            'id' => 11,
+            'user_id' => 10,
+            'content' => 'ささささ'
+        ]);
+    }
+}
