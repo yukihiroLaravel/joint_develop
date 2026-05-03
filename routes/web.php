dc2d6440bd817cd4936709e381254e7ad2b7451c
yukihiro sugimoto <ruslanyuki0012@gmail.com>
2026-05-03 21:08:43 +0900

    #107ユーザー新規登録

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@
 */
 
 Route::get('/', 'PostsController@index');
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+// ユーザ登録（作成時）
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
