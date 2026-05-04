e40417f452f3352c132e2a07bfe158ebbdfd10bb
yukihiro sugimoto <ruslanyuki0012@gmail.com>
2026-05-04 21:16:52 +0900

    #1708ユーザ新規登録

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -7,12 +7,6 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-<<<<<<< HEAD
-                <li class="nav-item"><a href="" class="nav-link text-light">ログインユーザ名</a></li>
-                <li class="nav-item"><a href="" class="nav-link text-light">ログアウト</a></li>
-                <li class="nav-item"><a href="" class="nav-link text-light">ログイン</a></li>
-                <li class="nav-item"><a href="{{ route('signup') }}" class="nav-link">新規ユーザ登録</a></li>
-=======
                 @if(Auth::check())
                     <li class="nav-item"><a href="" class="nav-link text-light">{{ Auth::user()->name}}</a></li>
                     <li class="nav-item"><a href="{{ route('logout') }}" class="nav-link text-light">ログアウト</a></li>
@@ -20,8 +14,7 @@
                     <li class="nav-item"><a href="{{ route('login') }}" class="nav-link text-light">ログイン</a></li>
                     <li class="nav-item"><a href="{{ route('signup') }}" class="nav-link text-light">新規ユーザ登録</a></li>
                     <!-- 「新規ユーザ登録」完了後に調整 -->
-                    @endif
->>>>>>> 2df9449fd8309ef2683eead9e8908ef5a71f2071
+                @endif
             </ul>
         </div>
     </nav>
