842d081628ea000735dcd73c7c4394edb992ef4a
daiki yaegashi <dashubazhongjian@gmail.com>
2026-05-31 20:51:03 +0900

    #119 投稿新規作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
             <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75">
                 @csrf
                 <div class="form-group">
-                    <textarea id="content" class="form-control" name="content" rows="4" value="{{ old('content') }}"></textarea>
+                    <textarea id="content" class="form-control" name="content" rows="4" value="">{{ old('content') }}</textarea>
                     <div class="text-left mt-3">
                         <button type="submit" class="btn btn-primary">投稿する</button>
                     </div>
