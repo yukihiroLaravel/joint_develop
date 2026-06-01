e390e34d6c25a737c514c6ddcb2870266578e3fa
yukihiro sugimoto <ruslanyuki0012@gmail.com>
2026-06-01 23:02:07 +0900

    トップページの投稿表示

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -4,7 +4,7 @@
         <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('signup.post', $post->user->id) }}"> {{$post->user->name}}</a></p>
         <div class="">
             <div class="text-left d-inline-block w-75">
-                <p class="mb-2">{{$post->content}}</p>
+                <p class="mt-3 mb-0">{{$post->content}}</p>
                 <p class="text-muted">{{$post->created_at}}</p>
             </div>
         </div>
