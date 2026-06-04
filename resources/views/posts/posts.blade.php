cd8844cc3cc7a11e2539a708e2eefe2254672088
yukihiro sugimoto <ruslanyuki0012@gmail.com>
2026-06-04 22:24:47 +0900

    バグ修正

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -1,7 +1,7 @@
 <div style="margin-left: 140px;">
     @foreach ($posts as $post)
         <img class="mr-2 rounded-circle" src="{{ Gravatar::src($post->user->email, 55) }}" alt="ユーザのアバター画像">                
-        <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('signup.post', $post->user->id) }}"> {{$post->user->name}}</a></p>
+        <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('user.show', $post->user->id) }}"> {{$post->user->name}}</a></p>
         <div class="">
             <div class="text-left d-inline-block w-75">
                 <p class="mt-3 mb-0">{{$post->content}}</p>
