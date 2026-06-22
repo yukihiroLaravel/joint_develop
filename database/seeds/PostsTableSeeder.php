378aadb09117a6a8509ba0a2637b8895ff07683a
hiroko.k <okojowebworks@gmail.com>
2026-06-23 00:49:46 +0900

    修正：content文字数制限contentカラムに140文字制限を追加、シーダーのデータを12件に変更、timestamp追加

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,10 +12,14 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         for ($i = 1; $i < 5; $i++) {
-            DB::table('posts')->insert([
-                'user_id' => $i,
-                'content' => ("testpost_テスト投稿 {$i}")
-            ]);
+            for ($postcount = 1; $postcount < 4; $postcount++) {
+                DB::table('posts')->insert([
+                    'user_id' => $i,
+                    'content' => "testpost_テスト投稿{$i}-{$postcount}",
+                    'created_at' => now()->subDays(rand(1, 30)), // ランダムで過去1〜30日
+                    'updated_at' => now(),
+                ]);
+            }
         }
     }
 }
