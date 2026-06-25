dfaacd6e5c2ce28b841aef145cc9482f58b2a013
daishukeyoshida <tengtianzhenghe379@gmail.com>
2026-06-25 23:06:18 +0900

    #107　ユーザ新規登録

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {return view('welcome');});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ユーザ新規登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
