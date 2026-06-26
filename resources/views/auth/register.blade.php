d400fd2e1f0cfcd7e460250ebba71f66676b04df
Minami <ued.minami@icloud.com>
2026-06-27 01:28:40 +0900

    fix: register validation error display

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,6 +12,15 @@
 
         <div class="row mt-5 mb-5">
             <div class="col-sm-6 offset-sm-3">
+                @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul class="mb-0">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+                @endif
                 <form
                     method="POST"
                     action="{{ route('signup.post') }}"
@@ -30,7 +39,9 @@
                         >
 
                         @error('name')
+                            <div class="text-danger">
                             <p>{{ $message }}</p>
+                            </div>
                         @enderror
                     </div>
 
@@ -48,7 +59,9 @@
                         >
 
                         @error('email')
-                            <p>{{ $message }}</p>
+                            <div class="text-danger">
+                                <p>{{ $message }}</p>
+                            </div>
                         @enderror
                     </div>
 
@@ -65,7 +78,9 @@
                         >
 
                         @error('password')
-                            <p>{{ $message }}</p>
+                            <div class="text-danger">
+                                {{ $message }}
+                            </div>
                         @enderror
                     </div>
 
