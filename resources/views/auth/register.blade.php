aa80f291c76c53d037dc72e70ec27db9b14ef53c
Minami <ued.minami@icloud.com>
2026-06-27 10:23:25 +0900

    #111 バリデーションの日本語化(Minami)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -30,7 +30,9 @@
                         >
 
                         @error('name')
+                        <div class="text-danger">
                             <p>{{ $message }}</p>
+                        </div>
                         @enderror
                     </div>
 
@@ -48,7 +50,9 @@
                         >
 
                         @error('email')
+                        <div class="text-danger">
                             <p>{{ $message }}</p>
+                        </div>
                         @enderror
                     </div>
 
@@ -65,7 +69,9 @@
                         >
 
                         @error('password')
+                        <div class="text-danger">
                             <p>{{ $message }}</p>
+                        </div>
                         @enderror
                     </div>
 
