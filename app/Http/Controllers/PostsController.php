d8b5a3224570f7609cac4a1d51bdd4d25a56c363
hiroko.k <okojowebworks@gmail.com>
2026-07-08 19:58:45 +0900

    一時保存

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,4 +14,6 @@ class PostsController extends Controller
             'posts' => $posts,
         ]);
     }
+
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,33 @@
 // 投稿一覧
 Route::get('/', 'PostsController@index')->name('posts');
 
-// ログイン機能完成後に有効化
-// Route::group(['middleware' => 'auth'], function () {
+// Route::prefix('posts')->middleware('auth')->group(function () { //login機能が実装されたらこちらを有効化し、下を削除
+Route::prefix('posts')->group(function () {
+    // Route::get('{id}/edit', 'PostsController@edit')->name('posts.edit'); //西川さんが追加することを想定
+    // Route::put('{id}', 'PostsController@update')->name('posts.update'); //西川さんが追加することを想定
+    Route::delete('{id}', 'PostsController@destroy')->name('posts.delete');
+});
+
 Route::prefix('users')->group(function () {
     // ユーザ詳細
     Route::get('{id}', 'UsersController@show')->name('users.show');
     // ユーザ編集・更新
-    Route::get('{id}/edit', 'UsersController@edit')->name('users.edit');
-    Route::put('{id}', 'UsersController@update')->name('users.update');
+    Route::get('{id}/edit', 'UsersController@edit')->name('users.edit'); //->middleware('auth') login機能が実装されたら入れる
+    Route::put('{id}', 'UsersController@update')->name('users.update'); //->middleware('auth') login機能が実装されたら入れる
 });
-// });
 
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+
+// ▼▼▼ テスト用（動作確認用の仮ログイン）コミット前に削除すること ▼▼▼
+Route::get('/test-login/{id}', function ($id) {
+    \Illuminate\Support\Facades\Auth::loginUsingId($id);
+    return redirect('/');
+});
+Route::get('/test-logout', function () {
+    \Illuminate\Support\Facades\Auth::logout();
+    return redirect('/');
+});
+// ▲▲▲ テスト用ここまで ▲▲▲
