aaadb1a0e41031b4b7f63a4ba35c185017bfef3a
hiroko.k <okojowebworks@gmail.com>
2026-07-09 00:36:33 +0900

    #121 投稿削除（ヒロコ）

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::get('users/{id}', 'UsersController@show')->name('users.show');
 // Route::group(['middleware' => 'auth'], function () {
 // 新規投稿
 Route::post('posts', 'PostsController@store')->name('posts.store');
+// 投稿削除
+Route::delete('posts/{id}', 'PostsController@destroy')->name('post.delete');
+
 Route::prefix('users')->group(function () {
     // ユーザ編集・更新
     Route::get('{id}/edit', 'UsersController@edit')->name('users.edit');
@@ -31,15 +34,3 @@ Route::prefix('users')->group(function () {
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
-
-
-// ▼▼▼ テスト用（動作確認用の仮ログイン）コミット前に削除すること ▼▼▼
-Route::get('/test-login/{id}', function ($id) {
-    \Illuminate\Support\Facades\Auth::loginUsingId($id);
-    return redirect('/');
-});
-Route::get('/test-logout', function () {
-    \Illuminate\Support\Facades\Auth::logout();
-    return redirect('/');
-});
-// ▲▲▲ テスト用ここまで ▲▲▲
