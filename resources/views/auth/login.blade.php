04e33ec57fc5013698196979d92761918c722d2f
takuya nishikawa <gerbera404@gmail.com>
2026-07-11 15:54:46 +0900

    ログイン・ログアウト機能(nishikawa)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,8 @@
     </div>
     <div class="row mt-5 mb-5">
         <div class="col-sm-6 offset-sm-3">
-            <form method="" action="">
+            <form method="" action="{{ route('login.post') }}">
+                @csrf
                 <div class="form-group">
                     <label for="email">メールアドレス</label>
                     <input id="email" type="text" class="form-control" name="" value="">
