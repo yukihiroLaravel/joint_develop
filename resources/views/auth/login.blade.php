369b1c460263205f4b1118348d1a96dcbeadfe72
takuya nishikawa <gerbera404@gmail.com>
2026-07-20 16:45:30 +0900

    バリデーション機能＋ログインエラー表示

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,6 +11,15 @@
     </div>
     <div class="row mt-5 mb-5">
         <div class="col-sm-6 offset-sm-3">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul class="mb-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <form method="POST" action="{{ route('login.post') }}">
                 @csrf
                 <div class="form-group">
