419e10607fecce492c08efcae166e185d7c4858a
takuya nishikawa <gerbera404@gmail.com>
2026-07-27 04:15:27 +0900

    フラッシュメッセージ機能v2修正

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -52,10 +52,13 @@ class UsersController extends Controller
     // ユーザー退会
     public function destroy($userId)
     {
-        $user = User::findOrFail($userId);
-        if (\Auth::id() === $user->id) {
-            $user->delete();
+        if (Auth::id() !== (int)$userId) {
+            abort(403, 'このユーザは編集権限がありません。');
         }
+
+        $user = User::findOrFail($userId);
+        $user->delete();
+
         return redirect()->route('posts')->with('success', '退会しました！');
     }
 
