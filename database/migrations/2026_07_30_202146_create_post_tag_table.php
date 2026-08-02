2707309608432ccdda1a5a879987a2df1e195d13
daishukeyoshida <tengtianzhenghe379@gmail.com>
2026-08-02 11:04:24 +0900

    タグ付け投稿 作業途中

---
+++ database/migrations/2026_07_30_202146_create_post_tag_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('post_id');
+            $table->unsignedInteger('tag_id');
+            $table->timestamps();
+            // 外部キー制約
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->unique(['post_id', 'tag_id', 'type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
