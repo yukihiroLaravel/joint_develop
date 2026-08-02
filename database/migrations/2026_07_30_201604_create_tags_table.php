35d79b342784bd58bc9b3213d2b78d7448d07b58
daishukeyoshida <tengtianzhenghe379@gmail.com>
2026-08-02 12:49:35 +0900

    タグ付け投稿機能　tinker動作確認完

--- database/migrations/2026_07_30_201604_create_tags_table.php
+++ database/migrations/2026_07_30_201604_create_tags_table.php
@@ -15,7 +15,7 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('type');
             $table->timestamps();
         });
     }
