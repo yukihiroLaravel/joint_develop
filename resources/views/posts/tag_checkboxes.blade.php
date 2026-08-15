4b6d9bc24d17b0a2ad7fa07b7989272b32495989
daishukeyoshida <tengtianzhenghe379@gmail.com>
2026-08-15 23:48:16 +0900

    タグの投稿編集(吉田)

--- resources/views/posts/tag_checkboxes.blade.php
+++ resources/views/posts/tag_checkboxes.blade.php
@@ -1,6 +1,18 @@
 @isset($allTags)
 @foreach ($allTags as $tag)
-<input type="checkbox" class="du-tag-checkbox js-tag-checkbox-limit" name="tags[]" value="{{ $tag->id }}" id="{{ $tagIdPrefix }}{{ $tag->id }}">
+@php
+    $isChecked = isset($post) && $post->tags->contains('id', $tag->id);
+@endphp
+<input
+    type="checkbox"
+    class="du-tag-checkbox js-tag-checkbox-limit"
+    name="tags[]"
+    value="{{ $tag->id }}"
+    id="{{ $tagIdPrefix }}{{ $tag->id }}"
+    @if ($isChecked)
+        checked
+    @endif
+>
 <label class="du-tag-select-pill" for="{{ $tagIdPrefix }}{{ $tag->id }}">#{{ $tag->type }}</label>
 @endforeach
 @endisset
