1654bcbb2144ea40f162fac6b6b24abe704c9a8b
shinichi ushioda <kouya0315.riri0717@gmail.com>
2026-09-15 08:50:22 +0900

    #94 userテーブルのマイグレーション・シーダーの修正

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,19 +18,19 @@ class UsersTableSeeder extends Seeder
             'password' => bcrypt('test1')
         ]);
 
-         DB::table('users')->insert([
+        DB::table('users')->insert([
             'name' => 'test2',
             'email' => 'test2@test.com',
             'password' => bcrypt('test2')
         ]);
 
-         DB::table('users')->insert([
+        DB::table('users')->insert([
             'name' => 'test3',
             'email' => 'test3@test.com',
             'password' => bcrypt('test3')
         ]);
 
-         DB::table('users')->insert([
+        DB::table('users')->insert([
             'name' => 'test4',
             'email' => 'test4@test.com',
             'password' => bcrypt('test4')
