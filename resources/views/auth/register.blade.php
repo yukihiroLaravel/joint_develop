562289de70f60ea7351634e26456c0f45b31d8d3
shinichi ushioda <kouya0315.riri0717@gmail.com>
2026-09-23 14:32:02 +0900

    ユーザ新規登録修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,7 +20,7 @@
                 </div>
                 <div class="form-group">
                     <label for="email">メールアドレス</label>
-                    <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}">
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
                 </div>
                 <div class="form-group">
                     <label for="password">パスワード</label>
